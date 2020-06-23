03c516145fb1b6285545e4c0c2e00008ca96c4d0
Patrick Schulz <pschulz@posteo.de>
2020-06-23 11:12:11 +0200

    Improved virtuoso lua interface

--- interface/virtuoso.lua
+++ interface/virtuoso.lua
@@ -1,18 +1,27 @@
 local M = {}
 
+-- private variables
 local generate_filename = function()
     return os.tmpname()
 end
+local layermap 
 
 function M.register_filename_generation_func(func)
     generate_filename = func
 end
 
+function M.set_layermap(lm)
+
+end
+
 local function _write_shape(file, shape)
     local sep = sep or "\n"
-    file:write(string.format("%s %s\n", shape.layer, shape.purpose))
-    for i, pt in ipairs(shape.pts) do
-        file:write(string.format("%.1f %.1f%s", pt.x, pt.y, sep))
+    for pts in shape:iter() do
+        file:write(string.format("%s %s\n", shape.layer, shape.purpose))
+        for i, pt in ipairs(pts) do
+            file:write(string.format("%.1f %.1f%s", pt.x, pt.y, sep))
+        end
+        file:write("\n")
     end
 end
 
@@ -23,7 +32,6 @@ function M.print_object(object)
     local file = io.open(filename, "w")
     for shape in object:iter() do
         _write_shape(file, shape)
-        file:write("\n")
     end
     file:close()
 end
