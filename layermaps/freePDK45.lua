9f1033d5e6ad675c3ae3ff4e2d1a6388d8e4c331
Patrick Schulz <pschulz@posteo.de>
2020-07-09 08:39:46 +0200

    Moved layermaps to subdirectory

---
+++ layermaps/freePDK45.lua
@@ -0,0 +1,38 @@
+return {
+    nwell       = "nwell",
+    pwell       = "pwell",
+    active      = "active",
+    pimpl       = "pimplant",
+    nimpl       = "nimplant",
+    gate        = "poly",
+    wellcont    = "contact",
+    gatecont    = "contact",
+    firstmetal  = "metal1",
+    lastmetal   = "metal9",
+    outermetal  = "metal10",
+    M1          = "metal1",
+    viaM1M2     = "via1",
+    M2          = "metal2",
+    viaM2M3     = "via2",
+    M3          = "metal3",
+    viaM3M4     = "via3",
+    M4          = "metal4",
+    viaM4M5     = "via4",
+    M5          = "metal5",
+    viaM5M6     = "via5",
+    M6          = "metal6",
+    viaM6M7     = "via6",
+    M7          = "metal7",
+    viaM7M8     = "via7",
+    M8          = "metal8",
+    viaM8M9     = "via8",
+    M9          = "metal9",
+    viaM9M10    = "via9",
+    M10         = "metal10",
+}
+
+--sblock 	- 	Salicide block 	0,0,255 #0000FF 	(1) (2)
+--vthl 	- 	Low threshold implant 		(3)
+--vthg 	- 	General use threshold implant 		(3)
+--vthh 	- 	High threshold implant 		(3)
+--thkox 	- 	Thick oxide 		(3)
