32fb23f3e2c022a0a9f95d12abc4bd5aa3d0fdf4
Patrick Schulz <pschulz@posteo.de>
2020-08-04 20:15:05 +0200

    Started fixing virtuoso interface

--- interface/virtuoso/init.lua
+++ interface/virtuoso/init.lua
@@ -9,13 +9,11 @@ local gridfmt = "%.3f"
 
 local function _write_shape(file, shape)
     local sep = sep or "\n"
-    if not shape.layer then return end -- empty layers are not written
+    if not shape.lpp then return end -- empty layers are not written
     local fmt = string.format("%s %s%s", gridfmt, gridfmt, sep)
-    for pts in shape:iter() do
-        file:write(string.format('    dbCreatePolygon(cv list("%s" "%s") ', shape.layer.name, shape.purpose.name))
-        file:write(string.format("list(%s)", pts:concat(function(pt) return string.format(gridfmt .. ":" .. gridfmt, pt.x, pt.y) end)))
-        file:write(")\n")
-    end
+    file:write(string.format('    dbCreatePolygon(cv list("%s" "%s") ', shape.lpp.layer.name, shape.lpp.purpose.name))
+    file:write(string.format("list(%s)", shape.points:concat(function(pt) return string.format(gridfmt .. ":" .. gridfmt, pt.x, pt.y) end)))
+    file:write(")\n")
 end
 
 function M.print_object(file, object)
