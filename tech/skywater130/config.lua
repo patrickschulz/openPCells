2adee1cc65a741bbace826583ddeb8cd1fa536fb
Patrick Schulz <pschulz@posteo.de>
2020-08-06 08:11:05 +0200

    Further improved skywater tech files

--- tech/skywater130/config.lua
+++ tech/skywater130/config.lua
@@ -1,3 +1,4 @@
 return {
-    grid = 0.005
+    grid = 0.005,
+    metals = 6
 }
--- tech/skywater130/viarules.lua
+++ tech/skywater130/viarules.lua
@@ -16,19 +16,101 @@ return {
     gatecont = {
         layers = {
             {
-                layer = { name = "npc", number = 95 },
-                purpose = { name = "drawing", number = 20 }
+                lpp = {
+                    layer   = { name = "poly",    number = 66 },
+                    purpose = { name = "drawing", number = 20 },
+                },
+                enlarge = 0.1
             },
             {
-                layer = { number = 66, name = "licon1" }, 
-                purpose = { number = 44, name = "drawing" }
+                lpp = {
+                    layer   = { name = "npc",     number = 95 },
+                    purpose = { name = "drawing", number = 20 },
+                },
+                enlarge = 0.1
+            },
+            {
+                lpp = {
+                    layer   = { name = "licon1",  number = 66 },
+                    purpose = { name = "drawing", number = 44 },
+                },
             },
         },
-        width = 0.040,
-        height = 0.040, 
-        xspace = 0.094, 
-        yspace = 0.094, 
-        xencl = 0.01, 
-        yencl = 0.02
-    }
+        width = 0.17,
+        height = 0.17, 
+        xspace = 0.25, 
+        yspace = 0.17, 
+        xencl = 0.04, 
+        yencl = 0.08
+    },
+    viaM1M2 = {
+        layers = {
+            lpp = {
+                layer   = { name = "mcon",    number = 67 },
+                purpose = { name = "drawing", number = 44 },
+            },
+        },
+        width = 0.17,
+        height = 0.17, 
+        xspace = 0.25, 
+        yspace = 0.17, 
+        xencl = 0.04, 
+        yencl = 0.08
+    },
+    viaM2M3 = {
+        layers = {
+            lpp = {
+                layer   = { name = "via",     number = 68 },
+                purpose = { name = "drawing", number = 44 },
+            },
+        },
+        width = 0.17,
+        height = 0.17, 
+        xspace = 0.25, 
+        yspace = 0.17, 
+        xencl = 0.04, 
+        yencl = 0.08
+    },
+    viaM3M4 = {
+        layers = {
+            lpp = {
+                layer   = { name = "via2",    number = 69 },
+                purpose = { name = "drawing", number = 44 },
+            },
+        },
+        width = 0.17,
+        height = 0.17, 
+        xspace = 0.25, 
+        yspace = 0.17, 
+        xencl = 0.04, 
+        yencl = 0.08
+    },
+    viaM4M5 = {
+        layers = {
+            lpp = {
+                layer   = { name = "via3",    number = 70 },
+                purpose = { name = "drawing", number = 44 },
+            },
+        },
+        width = 0.17,
+        height = 0.17, 
+        xspace = 0.25, 
+        yspace = 0.17, 
+        xencl = 0.04, 
+        yencl = 0.08
+    },
+    viaM5M6 = {
+        layers = {
+            lpp = {
+                layer   = { name = "via4",    number = 71 },
+                purpose = { name = "drawing", number = 44 },
+            },
+        },
+        width = 0.17,
+        height = 0.17, 
+        xspace = 0.25, 
+        yspace = 0.17, 
+        xencl = 0.04, 
+        yencl = 0.08
+    },
 }
