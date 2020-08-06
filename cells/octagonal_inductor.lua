42fd0e27653f4eca67a32b85de21ce7af9cd6a36
Patrick Schulz <pschulz@posteo.de>
2020-08-06 08:11:34 +0200

    Fixed some issues with octagonal_inductor, added metal as parameter

--- cells/octagonal_inductor.lua
+++ cells/octagonal_inductor.lua
@@ -1,4 +1,3 @@
-
 return function(args)
     pcell.setup(args)
 
@@ -8,17 +7,23 @@ return function(args)
     local width      = pcell.process_args("width",      6.0)
     local extension  = pcell.process_args("extension", 10.0)
     local extsep     = pcell.process_args("extsep",     6.0)
+    local metalnum   = pcell.process_args("metalnum",  -1, "integer")
 
     pcell.check_args()
 
     local inductor = object.create()
 
     local tanpi8 = math.tan(math.pi / 8)
+    local pitch = separation + width
+
+    local mainmetal = string.format("M%d", metalnum)
+    local auxmetal = string.format("M%d", metalnum - 1)
+    local via = string.format("%s->%s", auxmetal, mainmetal)
 
     -- draw left and right segments
     local sign = (turns % 2 == 0) and 1 or -1
     for i = 1, turns do
-        local radius = initradius + (i - 1) * (separation + width)
+        local radius = initradius + (i - 1) * pitch
         local r = radius * tanpi8
         sign = -sign
 
@@ -34,25 +39,25 @@ return function(args)
         -- draw underpass
         if i < turns then
             -- create connection to underpass
-            pathpts:prepend(point.create(-0.5 * (initradius * tanpi8 + 0.5 * (separation + width)), sign * radius))
-            pathpts:append(point.create(-0.5 * (initradius * tanpi8 + 0.5 * (separation + width)), -sign * radius))
+            pathpts:prepend(point.create(-0.5 * (initradius * tanpi8 + 0.5 * pitch),  sign * radius))
+            pathpts:append( point.create(-0.5 * (initradius * tanpi8 + 0.5 * pitch), -sign * radius))
             -- create underpass
             local uppts = pointarray.create()
-            uppts:append(point.create(-0.5 * (initradius * tanpi8 + 0.5 * (separation + width)), -sign * radius))
-            uppts:append(point.create(-0.5 * (separation + width) - 0.5 * tanpi8 * width, -sign * radius))
-            uppts:append(point.create(-0.5 * (separation + width), -sign * radius))
-            uppts:append(point.create( 0.5 * (separation + width), -sign * (radius + separation + width)))
-            uppts:append(point.create( 0.5 * (separation + width) + 0.5 * tanpi8 * width, -sign * (radius + separation + width)))
-            uppts:append(point.create( 0.5 * (initradius * tanpi8 + 0.5 * (separation + width)), -sign * (radius + separation + width)))
-            inductor:merge_into(layout.path("lastmetal", uppts, width, true))
-            inductor:merge_into(layout.path("M9", uppts:xmirror(), width, true))
+            uppts:append(point.create(-0.5 * (initradius * tanpi8 + 0.5 * pitch), -sign * radius))
+            uppts:append(point.create(-0.5 * pitch - 0.5 * tanpi8 * width, -sign * radius))
+            uppts:append(point.create(-0.5 * pitch, -sign * radius))
+            uppts:append(point.create( 0.5 * pitch, -sign * (radius + pitch)))
+            uppts:append(point.create( 0.5 * pitch + 0.5 * tanpi8 * width, -sign * (radius + pitch)))
+            uppts:append(point.create( 0.5 * (initradius * tanpi8 + 0.5 * pitch), -sign * (radius + pitch)))
+            inductor:merge_into(layout.path(mainmetal, uppts, width, true))
+            inductor:merge_into(layout.path(auxmetal, uppts:xmirror(), width, true))
             -- place vias
-            inductor:merge_into(layout.via("M9->M10", width, width):translate(
-                -0.5 * (initradius * tanpi8 + 0.5 * (separation + width)), 
-                -sign * (radius + separation + width)
+            inductor:merge_into(layout.via(via, width, width):translate(
+                -0.5 * (initradius * tanpi8 + 0.5 * pitch), 
+                -sign * (radius + pitch)
             ))
-            inductor:merge_into(layout.via("M9->M10", width, width):translate(
-                0.5 * (initradius * tanpi8 + 0.5 * (separation + width)), 
+            inductor:merge_into(layout.via(via, width, width):translate(
+                0.5 * (initradius * tanpi8 + 0.5 * pitch), 
                 -sign * radius
             ))
         end
@@ -65,7 +70,7 @@ return function(args)
         -- draw connector
         if i == turns then
             -- create connection to underpass
-            pathpts:prepend(point.create(-0.5 * (initradius * tanpi8 + 0.5 * (separation + width)), sign * radius))
+            pathpts:prepend(point.create(-0.5 * (initradius * tanpi8 + 0.5 * pitch), sign * radius))
             if 0.5 * extsep + width > r + 0.5 * width * tanpi8 then
                 pathpts:append(point.create(-0.5 * (extsep + width), -r - radius + 0.5 * (extsep + width)))
                 pathpts:append(point.create(-0.5 * (extsep + width), -r - radius + 0.5 * (extsep + width) - extension))
@@ -75,8 +80,8 @@ return function(args)
             end
         end
 
-        inductor:merge_into(layout.path("lastmetal", pathpts, width, true))
-        inductor:merge_into(layout.path("lastmetal", pathpts:xmirror(), width, true))
+        inductor:merge_into(layout.path(mainmetal, pathpts, width, true))
+        inductor:merge_into(layout.path(mainmetal, pathpts:xmirror(), width, true))
     end
 
     return inductor
