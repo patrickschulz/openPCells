9d2e53298cfdb9e4422ccd8e0a0af78cb70b3bb7
Patrick Schulz <pschulz@posteo.de>
2020-08-13 22:11:47 +0200

    Improved skywater130 viarules

--- tech/skywater130/viarules.lua
+++ tech/skywater130/viarules.lua
@@ -52,10 +52,12 @@ return {
     },
     viaM1M2 = {
         layers = {
-            lpp = {
-                gds      = { layer = 67,     purpose = 44 },
-                virtuoso = { layer = "mcon", purpose = "drawing", },
-                magic    = { layer = "mcon", purpose = "drawing", },
+            {
+                lpp = {
+                    gds      = { layer = 67,     purpose = 44 },
+                    virtuoso = { layer = "mcon", purpose = "drawing", },
+                    magic    = { layer = "mcon", purpose = "drawing", },
+                },
             },
         },
         width = 0.17,
@@ -67,62 +69,70 @@ return {
     },
     viaM2M3 = {
         layers = {
-            lpp = {
-                gds      = { layer = 68,      purpose = 44 },
-                virtuoso = { layer = "via", purpose = "drawing", },
-                magic    = { layer = "via", purpose = "drawing", },
+            {
+                lpp = {
+                    gds      = { layer = 68,      purpose = 44 },
+                    virtuoso = { layer = "via", purpose = "drawing", },
+                    magic    = { layer = "via", purpose = "drawing", },
+                },
             },
         },
-        width = 0.17,
-        height = 0.17, 
-        xspace = 0.25, 
+        width = 0.15,
+        height = 0.15, 
+        xspace = 0.17, 
         yspace = 0.17, 
-        xencl = 0.04, 
-        yencl = 0.08
+        xencl = 0.055, 
+        yencl = 0.055
     },
     viaM3M4 = {
         layers = {
-            lpp = {
-                gds      = { layer = 69,     purpose = 44 },
-                virtuoso = { layer = "via2", purpose = "drawing", },
-                magic    = { layer = "via2", purpose = "drawing", },
+            {
+                lpp = {
+                    gds      = { layer = 69,     purpose = 44 },
+                    virtuoso = { layer = "via2", purpose = "drawing", },
+                    magic    = { layer = "via2", purpose = "drawing", },
+                },
             },
         },
-        width = 0.17,
-        height = 0.17, 
-        xspace = 0.25, 
-        yspace = 0.17, 
+        width = 0.2,
+        height = 0.2, 
+        xspace = 0.2, 
+        yspace = 0.2, 
         xencl = 0.04, 
-        yencl = 0.08
+        yencl = 0.04
     },
     viaM4M5 = {
         layers = {
-            lpp = {
-                gds      = { layer = 70,     purpose = 44 },
-                virtuoso = { layer = "via3", purpose = "drawing", },
-                magic    = { layer = "via3", purpose = "drawing", },
+            {
+                lpp = {
+                    gds      = { layer = 70,     purpose = 44 },
+                    virtuoso = { layer = "via3", purpose = "drawing", },
+                    magic    = { layer = "via3", purpose = "drawing", },
+                },
             },
         },
-        width = 0.17,
-        height = 0.17, 
-        xspace = 0.25, 
-        yspace = 0.17, 
-        xencl = 0.04, 
-        yencl = 0.08
+        width = 0.2,
+        height = 0.2, 
+        xspace = 0.2, 
+        yspace = 0.2, 
+        xencl = 0.06, 
+        yencl = 0.06
     },
     viaM5M6 = {
         layers = {
-            lpp = {
-                gds      = { layer = 71,     purpose = 44 },
-                virtuoso = { layer = "via4", purpose = "drawing" },
-                magic    = { layer = "via4", purpose = "drawing" },
+            {
+                lpp = {
+                    gds      = { layer = 71,     purpose = 44 },
+                    virtuoso = { layer = "via4", purpose = "drawing" },
+                    magic    = { layer = "via4", purpose = "drawing" },
+                },
             },
         },
-        width = 0.17,
-        height = 0.17, 
-        xspace = 0.25, 
-        yspace = 0.17, 
-        xencl = 0.04, 
-        yencl = 0.08
+        width = 0.8,
+        height = 0.8, 
+        xspace = 0.8, 
+        yspace = 0.8, 
+        xencl = 0.19, 
+        yencl = 0.19
     },
 }
