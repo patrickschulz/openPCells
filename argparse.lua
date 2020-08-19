d70db7e2b07c9d6932a48d02c07f6b8856322eee
Patrick Schulz <pschulz@posteo.de>
2020-08-19 09:59:23 +0200

    Improved argparse.lua

--- argparse.lua
+++ argparse.lua
@@ -1,28 +1,59 @@
 local M = {}
 
+local function _advance(state, num)
+    local num = num or 1
+    state.i = state.i + num
+end
+
+local function _next_arg(args, state)
+    _advance(state)
+    return args[state.i]
+end
+
+local function _consume_until_hyphen(args, state)
+    local t = {}
+    while true do
+        local arg = args[state.i + 1]
+        if not arg or string.match(arg, "^-") then break end
+        table.insert(t, arg)
+        _advance(state)
+    end
+    return table.concat(t, " ")
+end
+
+local actions = {
+    ["-P"] = function(res)
+        res.params = true 
+    end,
+    ["-T"] = function(res, state, args) 
+        res.technology = _next_arg(args, state)
+    end,
+    ["-I"] = function(res, state, args) 
+        res.interface = _next_arg(args, state)
+    end,
+    ["-C"] = function(res, state, args) 
+        res.cell = _next_arg(args, state)
+    end,
+    ["-f"] = function(res, state, args) 
+        res.filename = _next_arg(args, state)
+    end,
+    ["--iopt"] = function(res, state, args)
+        res.interface_options = _consume_until_hyphen(args, state)
+    end,
+}
+
 function M.parse(args)
-    local i = 1
+    local state = { i = 1 }
     local res = {cellargs = {}}
-    while i <= #args do
-        local arg = args[i]
-        if string.match(arg, "^-P") then
-            res.params = true
-        elseif string.match(arg, "^-T") then
-            res.technology = args[i + 1]
-            i = i + 1
-        elseif string.match(arg, "^-I") then
-            res.interface = args[i + 1]
-            i = i + 1
-        elseif string.match(arg, "^-C") then
-            res.cell = args[i + 1]
-            i = i + 1
-        elseif string.match(arg, "^-f") then
-            res.filename = args[i + 1]
-            i = i + 1
-        else
+    while state.i <= #args do
+        local arg = args[state.i]
+        local action = actions[arg]
+        if not action then
             table.insert(res.cellargs, arg)
+        else
+            action(res, state, args)
         end
-        i = i + 1
+        _advance(state)
     end
     return res
 end
