a0fb721ea2d0ef9579c3b46ff17df20352e7c093
Patrick Schulz <pschulz@posteo.de>
2020-08-25 09:06:24 +0200

    Added more information in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,17 @@
 
 mainfile=opc
 
+printf "* generating '%s' *\n" $mainfile
+
 # write main content
-echo   '#! /usr/bin/lua\n'        > $mainfile
-echo   'function _get_opc_home()' >> $mainfile
+echo   '#! /usr/bin/lua\n'         > $mainfile
+echo   "-- This is a generated file, don't edit it directly. Edit main.lua\n" >> $mainfile
+echo   "function _get_opc_home()" >> $mainfile
 printf '    return "%s"\n' $(pwd) >> $mainfile
-echo   'end\n'                    >> $mainfile
+echo   "end\n"                    >> $mainfile
 cat main.lua                      >> $mainfile
 
 # make executable
 chmod 750 $mainfile
+
+echo 'See doc/userguide/userguide.pdf for a quick start and general documentation.'
