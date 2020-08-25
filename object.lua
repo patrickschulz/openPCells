b21524998aec4c88ecab4b39d7d76805c9e860cb
Patrick Schulz <pschulz@posteo.de>
2020-08-25 09:06:57 +0200

    Added object anchors

--- object.lua
+++ object.lua
@@ -17,7 +17,7 @@ meta.__tostring = function(self)
 end
 
 function M.create()
-    local self = { shapes = {} }
+    local self = { shapes = {}, anchors = {} }
     setmetatable(self, meta)
     return self
 end
@@ -26,6 +26,9 @@ function meta.copy(self)
     local new = M.create()
     for i, shape in ipairs(self.shapes) do
         new.shapes[i] = shape:copy()
+    end
+    for name, pt in pairs(self.anchors) do
+        new.anchors[name] = pt:copy()
     end
     return new
 end
@@ -37,14 +40,13 @@ function M.make_from_shape(shape)
 end
 
 function meta.merge_into(self, other)
-    --for _, shape in ipairs(other.shapes) do
     for shape in other:iter() do
         self:add_shape(shape)
     end
 end
 
 function meta.add_shape(self, shape)
-    table.insert(self.shapes, shape)
+    table.insert(self.shapes, shape:copy())
 end
 
 function meta.add_shapes(self, shapes)
@@ -66,6 +68,25 @@ function meta.translate(self, dx, dy)
     for _, shape in ipairs(self.shapes) do
         shape:translate(dx, dy)
     end
+    for _, anchor in pairs(self.anchors) do
+        anchor:translate(dx, dy)
+    end
+    return self
+end
+
+function meta.add_anchor(self, name, where)
+    local where = where or point.create(0, 0)
+    self.anchors[name] = where
+end
+
+function meta.get_anchor(self, name)
+    return self.anchors[name]
+end
+
+function meta.move_anchor(self, name, where)
+    local where = where or point.create(0, 0)
+    local pt = self.anchors[name]
+    self:translate(where.x - pt.x, where.y - pt.y)
     return self
 end
 
