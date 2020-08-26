3c00e168031eb957c09674b68af0f54bef2d0645
Patrick Schulz <pschulz@posteo.de>
2020-08-26 13:12:43 +0200

    Started improving handling of positional parameters

--- argparse.lua
+++ argparse.lua
@@ -30,6 +30,14 @@ local function _consume_until_hyphen(args, state)
     return table.concat(t, " ")
 end
 
+local function _parse_key_value_pairs(str)
+    local t = {}
+    for k, v in string.gmatch(str, "(%w+)%s*=%s*(%S+)") do
+        t[k] = v
+    end
+    return t
+end
+
 local function _store_func(name)
     return function(res, state, args)
         res[name] = _next_arg(args, state)
@@ -42,9 +50,15 @@ local function _switch_func(name)
     end
 end
 
-local function _consumer_func(name)
+local function _consumer_string_func(name)
     return function(res, state, args)
         res[name] = _consume_until_hyphen(args, state)
+    end
+end
+
+local function _consumer_table_func(name)
+    return function(res, state, args)
+        res[name] = _parse_key_value_pairs(_consume_until_hyphen(args, state))
     end
 end
 
@@ -59,17 +73,22 @@ local actions = {
     ["--cell"]       = _store_func("cell"),
     ["-f"]           = _store_func("filename"),
     ["--filename"]   = _store_func("filename"),
-    ["--iopt"]       = _consumer_func("interface_options"),
+    ["--iopt"]       = _consumer_table_func("interface_options"),
 }
+
+--local positional = _consumer_table_func("cellargs")
+local positional = function(res, state, args)
+    table.insert(res["cellargs"], args[state.i])
+end
 
 function M.parse(args)
     local state = { i = 1 }
-    local res = {cellargs = {}}
+    local res = { cellargs = {} }
     while state.i <= #args do
         local arg = args[state.i]
         local action = actions[arg]
         if not action then
-            table.insert(res.cellargs, arg)
+            positional(res, state, args)
         else
             action(res, state, args)
         end
