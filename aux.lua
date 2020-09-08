08f88113ff2960f7d3aaa9c6ee3ebb69b2c92f95
Patrick Schulz <pschulz@posteo.de>
2020-09-08 08:23:50 +0200

    Added helper functions for transistor_array

--- aux.lua
+++ aux.lua
@@ -29,4 +29,48 @@ function M.concat(data, sep, pre, post, newline)
     return tabstr
 end
 
+function M.find(t, crit)
+    for i, v in ipairs(t) do
+        if crit(v) then
+            return i, v
+        end
+    end
+end
+
+function M.shuffle(t)
+  for i = #t, 2, -1 do
+    local j = math.random(i)
+    t[i], t[j] = t[j], t[i]
+  end
+  return t
+end
+
+function M.gcd(a, b)
+	if b ~= 0 then
+		return gcd(b, a % b)
+	else
+		return a
+	end
+end
+
+function M.tabgcd(t)
+    local gcd = t[1]
+    for i = 1, #t do
+        for j = 1, #t do
+            if i ~= j then
+                gcd = math.min(gcd, gcd(t[i], t[j]))
+            end
+        end
+    end
+    return gcd
+end
+
+function M.sum(t)
+    local res = 0
+    for _, e in ipairs(t) do
+        res = res + e
+    end
+    return res
+end
+
 return M
