7000e8fbb16068b51e01bed070c403007f039336
Patrick Kurth <p.kurth@posteo.de>
2020-10-17 00:32:18 +0200

    Improved error handling in load

--- load.lua
+++ load.lua
@@ -12,27 +12,27 @@ end
 local function _generic_load(filename, chunkname)
     local reader, msg = _get_reader(filename)
     if not reader then
-        print(msg)
-        return
+        error(msg)
     end
 
     local func, msg = load(reader, chunkname)
 
     if not func then
-        print(msg)
-        return nil
+        error(msg)
     end
 
     local status, chunk = pcall(func)
     if not status then
-        print(chunk)
-        return nil
+        error(chunk)
     end
 
     return chunk
 end
 
 function _load_module(modname)
+    if not modname then
+        error("no module name given")
+    end
     local filename = string.format("%s/%s.lua", _get_opc_home(), modname)
     local chunkname = string.format("@%s", modname)
 
