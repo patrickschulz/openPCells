7334f7fde28c120ef10a20fccd136362b2991add
Patrick Kurth <p.kurth@posteo.de>
2020-11-02 08:39:49 +0100

    Simplified config module

--- config.lua
+++ config.lua
@@ -1,24 +1,13 @@
 local M = {}
 
 function M.get_user_config()
-    local file = io.open(string.format("%s/.opcconfig.lua", os.getenv("HOME")))
-    if not file then
-        return
-    end
-    local content = file:read("*a")
+    local filename = string.format("%s/.opcconfig.lua", os.getenv("HOME"))
+    local chunkname = "@userconfig"
+
     local env = {
         addcellpath = pcell.add_cellpath
     }
-    local chunkname = string.format("=config '%s'", cellname)
-    local chunk, msg = load(content, chunkname, "t", env)
-    if not chunk then
-        error(string.format("error while loading user configuration: %s", msg), 0)
-    end
-    local status, msg = pcall(chunk)
-    if not status then
-        error(string.format("error while loading user configuration: %s", msg), 0)
-    end
-    return msg
+    return _generic_load(filename, chunkname, "error while loading user configuration", "error while loading user configuration", env)
 end
 
 return M
