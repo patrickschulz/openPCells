ab3c80aa195b72ef88e0faf459d3e5cbb376e41b
Patrick Kurth <p.kurth@posteo.de>
2020-11-03 12:31:30 +0100

    Parameters are now inherited in order

--- pcell.lua
+++ pcell.lua
@@ -314,7 +314,11 @@ end
 
 function inherit_all_parameters(cellname, othercell)
     local inherited = loadedcells[othercell]
-    for name, param in pairs(inherited.parameters) do 
+    local parameters = {}
+    for k in pairs(inherited.parameters) do
+        parameters[inherited.indices[k]] = k
+    end
+    for _, name in ipairs(parameters) do
         inherit_parameter(cellname, othercell, name)
     end
 end
