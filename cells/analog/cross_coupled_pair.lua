fd3f3dfe026783091aa89b181e7d1dcc5a73a87c
Patrick Schulz <pschulz@posteo.de>
2020-11-04 11:54:22 +0100

    Continued working on cross coupled pair

--- cells/analog/cross_coupled_pair.lua
+++ cells/analog/cross_coupled_pair.lua
@@ -1,16 +1,47 @@
 function parameters()
-    pcell.add_parameters({ "fingers(Number of Fingers)", 4 })
-    pcell.inherit_and_bind_all_parameters("basic/transistor")
+    --pcell.inherit_and_bind_all_parameters("basic/transistor")
+    pcell.inherit_all_parameters("basic/transistor")
+    pcell.add_parameters(
+        { "connmetal", 4 },
+        { "connwidth", 60 },
+        { "centerdummies", 2 }
+    )
 end
 
-function layout(oscillator, _P)
-    --local gatepitch = _P.gatelength + _P.gatespace
-    --local numtransistors = #_P.fingers
-    --local numfingers = aux.sum(_P.fingers)
-    --local gatestrspace = 0.2
+function layout(ccp, _P)
+    pcell.push_overwrites("basic/transistor", {
+        fingers = 4,
+        gatelength = _P.gatelength,
+        gatespace = _P.gatespace,
+        sdwidth = _P.sdwidth,
+        sourcesize = 500,
+        sourcealign = "bottom",
+        drainsize = 500,
+        drawtopgate = true,
+        topgatestrwidth = _P.connwidth,
+        topgatemetal = 4,
+        drawdrainvia = true,
+        conndrainmetal = 4,
+        connectsource = true
+    })
 
-    oscillator:merge_into(geometry.multiple(
-        pcell.create_layout("basic/transistor"),
-        1, 1, 0, 0)
+    -- create center dummy
+    local dummy = pcell.create_layout("basic/transistor", { fingers = _P.centerdummies, drawtopgate = false, drawdrainvia = false })
+    ccp:merge_into(dummy)
+
+    local left = pcell.create_layout("basic/transistor")
+    left:move_anchor("rightdrainsource", dummy:get_anchor("leftdrainsource"))
+    local right = pcell.create_layout("basic/transistor")
+    right:move_anchor("leftdrainsource", dummy:get_anchor("rightdrainsource"))
+    ccp:merge_into(left)
+    ccp:merge_into(right)
+
+    pcell.pop_overwrites("basic/transistor")
+
+    local leftgate = left:get_anchor("topgatestrapright")
+    local rightgate = right:get_anchor("topgatestrapleft"):copy():translate(0, -200)
+    ccp:merge_into(geometry.crossing(
+        generics.metal(_P.connmetal), generics.metal(_P.connmetal - 1), 
+        _P.connwidth, leftgate, rightgate, "rectangular-separated", 200)
     )
 end
