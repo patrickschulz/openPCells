99a04b2aa672b9d2540d0811218f7836226f5787
Patrick Kurth <p.kurth@posteo.de>
2021-01-13 21:52:16 +0100

    Improved profiler data display

--- profiler.lua
+++ profiler.lua
@@ -8,33 +8,25 @@ function M.start()
     local logcall = function(event)
         local funcinfo = debug.getinfo(2)
         if funcinfo.what ~= "Lua" then return end
-        local name = funcinfo.name
-        if not name then return end
+        if not funcinfo.name then return end
+        local key = funcinfo.name .. funcinfo.source
         if event == "call" then
-            lastcalled[name] = os.clock()
-        elseif event == "return" then
-            if lastcalled[name] then
-                local time = os.clock() - lastcalled[name]
-                if not data[name] then data[name] = { time = 0, count = 0 } end
-                data[name].time = data[name].time + time
-                data[name].count = data[name].count + 1
-            end
-        end
-        --[[
-        local name = funcinfo.name
-        local namewhat = funcinfo.namewhat
-        if name then
-            if not data[name] then 
-                data[name] = 0 
-            end
-            if not info[name] then
-                info[name] = {
-                    source = funcinfo.source
+            if not info[key] then
+                info[key] = { 
+                    name = funcinfo.name,
+                    source = funcinfo.source,
+                    line = funcinfo.linedefined
                 }
             end
-            data[name] = data[name] + 1
+            lastcalled[key] = os.clock()
+        elseif event == "return" then
+            if lastcalled[key] then
+                local time = os.clock() - lastcalled[key]
+                if not data[key] then data[key] = { time = 0, calls = 0 } end
+                data[key].time = data[key].time + time
+                data[key].calls = data[key].calls + 1
+            end
         end
-        --]]
     end
     debug.sethook(logcall, "cr")
 end
@@ -45,12 +37,34 @@ end
 
 function M.display()
     local sorted = {}
+    local widths = {
+        name = 0,
+        source = 0,
+        line = 4, -- at least four characters for the line number (to fit "Line")
+        calls = 5, -- see above comment (fit "Calls")
+    }
     for k, v in pairs(data) do
-        table.insert(sorted, { name = k, data = v })
+        table.insert(sorted, { key = k, data = v })
+        widths.calls = math.max(widths.calls, math.floor(math.log(v.calls, 10)) + 1)
+    end
+    for _, entry in pairs(info) do
+        widths.name = math.max(widths.name, string.len(entry.name))
+        widths.source = math.max(widths.source, string.len(entry.source))
+        widths.line = math.max(widths.line, math.floor(math.log(entry.line, 10)) + 1)
     end
     table.sort(sorted, function(lhs, rhs) return lhs.data.time < rhs.data.time end)
+    local entryfmt = string.format(" %%-%ds %%-%ds %%%dd %%10f %%%dd", widths.name, widths.source, widths.line, widths.calls)
+    local headerfmt = string.format(" %%-%ds %%-%ds %%%ds %%10s %%%ds", widths.name, widths.source, widths.line, widths.calls)
+    local header = string.format(headerfmt, "Function", "Module", "Line", "Time", "Calls")
+    print(header)
+    print(string.rep("-", string.len(header)))
     for _, entry in ipairs(sorted) do
-        print(string.format("%30s: %10f (%d)", entry.name, entry.data.time, entry.data.count))
+        print(string.format(entryfmt,
+            info[entry.key].name, 
+            info[entry.key].source, info[entry.key].line, 
+            entry.data.time, 
+            entry.data.calls
+        ))
     end
 end
 
