427d544c6bb62aabaee6ca94f47e157f6a463684
Patrick Kurth <p.kurth@posteo.de>
2021-01-19 15:05:28 +0100

    Fixed transistor_array

--- cells/basic/transistor_array.lua
+++ cells/basic/transistor_array.lua
@@ -1,10 +1,12 @@
 function parameters()
     pcell.add_parameters({ "fingers(Number of Fingers)", { 4, 4 }, argtype = "numtable" })
-    pcell.inherit_and_bind_all_parameters("basic/transistor")
+    pcell.inherit_all_parameters("basic/transistor")
 end
 
 function layout(array, _P)
-    local gatepitch = _P.gatelength + _P.gatespace
+    local bp = pcell.get_parameters("basic/transistor")
+
+    local gatepitch = bp.gatelength + bp.gatespace
     local numtransistors = #_P.fingers
     local numfingers = aux.sum(_P.fingers)
     local gatestrspace = 200
@@ -13,8 +15,8 @@ function layout(array, _P)
     local indices = {}
     for i, f in ipairs(_P.fingers) do
         ttypes[i] = {
-            topgatestrspace = i * gatestrspace + (i - 1) * _P.topgatestrwidth,
-            botgatestrspace = i * gatestrspace + (i - 1) * _P.botgatestrwidth,
+            topgatestrspace = i * gatestrspace + (i - 1) * bp.topgatestrwidth,
+            botgatestrspace = i * gatestrspace + (i - 1) * bp.botgatestrwidth,
         }
         for _ = 1, f / 2 do
             table.insert(indices, i)
@@ -25,8 +27,8 @@ function layout(array, _P)
         fingers = 1,
         drawtopgate = true,
         drawbotgate = true,
-        gtopext = numtransistors * (gatestrspace + _P.topgatestrwidth),
-        gbotext = numtransistors * (gatestrspace + _P.botgatestrwidth),
+        gtopext = numtransistors * (gatestrspace + bp.topgatestrwidth),
+        gbotext = numtransistors * (gatestrspace + bp.botgatestrwidth),
     })
     for i = 1, #indices do
         local offset = (i - 1) - (2 * #indices - 1) / 2
@@ -45,12 +47,12 @@ function layout(array, _P)
     -- gate connections
     for i = 1, #ttypes do
         array:merge_into(geometry.rectangle(
-            generics.metal(1), numfingers * gatepitch, _P.topgatestrwidth
-            ):translate(0, (_P.fwidth + _P.topgatestrwidth) / 2 + i * gatestrspace + (i - 1) * _P.topgatestrwidth)
+            generics.metal(1), numfingers * gatepitch, bp.topgatestrwidth
+            ):translate(0, (bp.fwidth + bp.topgatestrwidth) / 2 + i * gatestrspace + (i - 1) * bp.topgatestrwidth)
         )
         array:merge_into(geometry.rectangle(
-            generics.metal(1), numfingers * gatepitch, _P.topgatestrwidth
-            ):translate(0, -(_P.fwidth + _P.botgatestrwidth) / 2 - i * gatestrspace - (i - 1) * _P.botgatestrwidth)
+            generics.metal(1), numfingers * gatepitch, bp.topgatestrwidth
+            ):translate(0, -(bp.fwidth + bp.botgatestrwidth) / 2 - i * gatestrspace - (i - 1) * bp.botgatestrwidth)
         )
     end
 end
