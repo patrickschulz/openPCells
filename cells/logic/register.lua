e2d58f17f8c15ce8973f6b701b8bc3f6e3e6eeac
Patrick Kurth <p.kurth@posteo.de>
2021-01-29 12:28:47 +0100

    Started with simple register

---
+++ cells/logic/register.lua
@@ -0,0 +1,13 @@
+function parameters()
+    pcell.add_parameter("bitwidth", 8)
+end
+
+function layout(register, _P)
+    local anchor = point.create(0, 0)
+    for i = 1, _P.bitwidth do
+        local dff = pcell.create_layout("logic/dff")
+        dff:move_anchor("VDD", anchor)
+        register:merge_into(dff)
+        anchor = dff:get_anchor("VSS")
+    end
+end
