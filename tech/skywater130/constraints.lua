1260673aecaf5180ffe8dce20ab1123282af174c
Patrick Kurth <p.kurth@posteo.de>
2021-01-29 19:27:26 +0100

    Added constraint file for skywater130

---
+++ tech/skywater130/constraints.lua
@@ -0,0 +1,8 @@
+return {
+    ["Minimum Gate Extension"] = 120,
+    ["Minimum Gate Length"] = 150,
+    ["Minimum Gate Width"]  = 300,
+    ["Minimum Gate Space"]  = 200,
+    ["Minimum M1 Width"]    = 100,
+    ["Minimum M1 Space"]    = 100,
+}
