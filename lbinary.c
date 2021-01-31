7d280b8eb16d45355d3914966011ded2077b5279
Patrick Kurth <p.kurth@posteo.de>
2021-01-31 22:39:07 +0100

    Fixed byte order

--- lbinary.c
+++ lbinary.c
@@ -14,29 +14,40 @@ static int lbinary_split_in_bytes(lua_State* L)
     int num = lua_tointeger(L, 1);
     unsigned int bytes = lua_tointeger(L, 2);
     lua_createtable(L, bytes, 0);
-    if(num < 0)
+    int sign = num < 0;
+    for(unsigned int i = bytes; i > 0; --i)
     {
-        for(unsigned int i = bytes; i > 0; --i)
-        {
-            int byte = num >> (bits * (i - 1));
-            lua_pushinteger(L, byte);
-            lua_rawseti(L, -2, i);
-            num = num - (byte << (bits * (i - 1)));
-        }
-        lua_rawgeti(L, -1, 4);
+        int byte = num >> (bits * (i - 1));
+        lua_pushinteger(L, byte);
+        lua_rawseti(L, -2, bytes - i + 1);
+        num = num - (byte << (bits * (i - 1)));
+    }
+    if(sign)
+    {
+        lua_rawgeti(L, -1, 1);
         lua_pushinteger(L, lua_tointeger(L, -1) + 256);
-        lua_rawseti(L, -3, 4);
+        lua_rawseti(L, -3, 1);
         lua_pop(L, 1);
     }
-    else
+    return 1;
+}
+
+static int lbinary_assemble(lua_State* L)
+{
+    /*
+    local args = { ... }
+    local t = {}
+    for _, data in ipairs(args) do
+        for _, datum in ipairs(data) do
+            table.insert(t, string.char(datum))
+        end
+    end
+    return table.concat(t)
+    */
+    int nargs = lua_gettop(L);
+    for(int i = 1; i <= nargs; ++i)
     {
-        for(unsigned int i = bytes; i > 0; --i)
-        {
-            int byte = num >> (bits * (i - 1));
-            lua_pushinteger(L, byte);
-            lua_rawseti(L, -2, i);
-            num = num - (byte << (bits * (i - 1)));
-        }
+
     }
     return 1;
 }
@@ -45,8 +56,9 @@ int open_lbinary_lib(lua_State* L)
 {
     static const luaL_Reg modfuncs[] =
     {
-        { "split_in_bytes",        lbinary_split_in_bytes       },
-        { NULL,            NULL                }
+        { "split_in_bytes", lbinary_split_in_bytes },
+        { "assemble",       lbinary_assemble       },
+        { NULL,             NULL                   }
     };
     lua_newtable(L);
     luaL_setfuncs(L, modfuncs, 0);
