56911a20c496ac974c78205b1cbe5b8f6f749aba
Patrick Kurth <p.kurth@posteo.de>
2021-04-22 09:51:45 +0200

    Fixed another LVS bug in DFF

--- cells/logic/dff.lua
+++ cells/logic/dff.lua
@@ -105,7 +105,12 @@ function layout(gate, _P)
     gate:merge_into(isogate:copy())
 
     pcell.push_overwrites("logic/base", { connectoutput = true })
-    local fbcinv2 = pcell.create_layout("logic/cinv", { inputpos = "lower", swapinputs = false, swapoutputs = true, shiftoutput = xpitch * 5 / 2 }):move_anchor("left", isogate:get_anchor("right"))
+    local fbcinv2 = pcell.create_layout("logic/cinv", { 
+        inputpos = _P.clockpolarity == "positive" and "upper" or "lower",
+        swapinputs = false, 
+        swapoutputs = true, 
+        shiftoutput = xpitch * 5 / 2 
+    }):move_anchor("left", isogate:get_anchor("right"))
     fbcinv2:flipx()
     gate:merge_into(fbcinv2)
     pcell.pop_overwrites("logic/base")
@@ -121,7 +126,10 @@ function layout(gate, _P)
     if _P.enableQ and _P.enableQN then
         outbuf = pcell.create_layout("logic/buf"):move_anchor("left", fbcinv2:get_anchor("right"))
     else
-        outbuf = pcell.create_layout("logic/not_gate", { inputpos = "lower", shiftoutput = xpitch / 2 }):move_anchor("left", fbcinv2:get_anchor("right"))
+        outbuf = pcell.create_layout("logic/not_gate", { 
+            inputpos = _P.clockpolarity == "positive" and "upper" or "lower",
+            shiftoutput = xpitch / 2 
+        }):move_anchor("left", fbcinv2:get_anchor("right"))
     end
     pcell.pop_overwrites("logic/base")
     gate:merge_into(outbuf)
