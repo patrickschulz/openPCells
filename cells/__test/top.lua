d8a662be5b17d06cb67c3f83dd3d13ed709378aa
Patrick Kurth <p.kurth@posteo.de>
2021-05-02 19:24:10 +0200

    Improved test cells

--- cells/__test/top.lua
+++ cells/__test/top.lua
@@ -4,6 +4,8 @@ end
 
 function layout(cell, _P)
     local sub = pcell.create_layout("__test/sub")
-    cell:merge_into(sub:copy():translate(0, 100))
-    cell:merge_into(sub:translate(0, -100))
+    cell:merge_into(geometry.rectangle(generics.metal(1), 200, 200))
+    cell:add_child(sub:copy():translate(0, 500))
+    cell:add_child(sub:translate(0, -500))
+    cell:merge_into(geometry.path(generics.metal(3), { point.create(-200, 0), point.create(200, 0) }, 50))
 end
