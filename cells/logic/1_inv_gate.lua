2b9f8350da8b0b157a15f87836366bf455e26314
Patrick Kurth <p.kurth@posteo.de>
2021-06-09 12:24:28 +0200

    Added isogate in 1_inv_gate

--- cells/logic/1_inv_gate.lua
+++ cells/logic/1_inv_gate.lua
@@ -9,17 +9,22 @@ end
 
 function layout(gate, _P)
     local bp = pcell.get_parameters("logic/base")
+    local xpitch = bp.glength + bp.gspace
 
     pcell.push_overwrites("logic/base", { rightdummies = 0 })
     local subgateref = pcell.create_layout(string.format("logic/%s", _P.subgate), { fingers = _P.subgatefingers })
     pcell.pop_overwrites("logic/base")
     local subgate = gate:add_child(subgateref, _P.subgate)
 
+    local isogateref = pcell.create_layout("logic/isogate")
+    local isogate = gate:add_child(isogateref, "isogate")
+    isogate:move_anchor("left", subgate:get_anchor("right"))
+
     pcell.push_overwrites("logic/base", { leftdummies = 0 })
-    local invref = pcell.create_layout("logic/not_gate", { fingers = _P.notfingers })
+    local invref = pcell.create_layout("logic/not_gate", { fingers = _P.notfingers, shiftoutput = xpitch / 2 })
     pcell.pop_overwrites("logic/base")
     local inv = gate:add_child(invref, "not_gate")
-    inv:move_anchor("left", subgate:get_anchor("right"))
+    inv:move_anchor("left", isogate:get_anchor("right"))
 
     -- draw connection
     gate:merge_into_shallow(geometry.path(generics.metal(1), {
