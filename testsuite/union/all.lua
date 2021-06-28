370ea00069246afb3a4a7841deaea0abe98a4ad3
Patrick Kurth <p.kurth@posteo.de>
2021-06-28 10:25:32 +0200

    Improved testsuite/union

--- testsuite/union/all.lua
+++ testsuite/union/all.lua
@@ -1,17 +1,32 @@
 -- luacheck: globals report
 do
     local rectangles = {
-        { bl = point.create(-10, 0), tr = point.create(10, 10) },
-        { bl = point.create(-5, 0), tr = point.create(5, 10) }
+        {
+            name = "halfopenright",
+            first = { bl = point.create(-10, 0), tr = point.create(10, 10) },
+            second = { bl = point.create(-10, 0), tr = point.create(15, 10) },
+            result = { bl = point.create(-10, 0), tr = point.create(15, 10) }
+        },
+        {
+            name = "inner",
+            first = { bl = point.create(-10, 0), tr = point.create(10, 10) },
+            second = { bl = point.create(-5, 0), tr = point.create(5, 10) },
+            result = { bl = point.create(-10, 0), tr = point.create(10, 10) }
+        },
     }
-    local status, msg = pcall(union.rectangle_all, rectangles)
-    if status then
-        local result = rectangles[1]
-        local blx, bly = result.bl:unwrap()
-        local trx, try = result.tr:unwrap()
-        local s = blx == -10 and bly == 0 and trx == 10 and try == 10
-        report("all", s, string.format("merged result is wrong ((%d, %d) and (%d, %d))", blx, bly, trx, try))
-    else
-        report("all", nil, msg)
+    for _, entry in ipairs(rectangles) do
+        local rects = { entry.first, entry.second }
+        local status, msg = pcall(union.rectangle_all, rects)
+        if status then
+            local result = rects[1]
+            local blx, bly = result.bl:unwrap()
+            local trx, try = result.tr:unwrap()
+            local blxref, blyref = result.bl:unwrap()
+            local trxref, tryref = result.tr:unwrap()
+            local s = blx == blxref and bly == blyref and trx == trxref and try == tryref
+            report(entry.name, s, string.format("merged result is wrong ((%d, %d) and (%d, %d))", blx, bly, trx, try))
+        else
+            report("all", nil, msg)
+        end
     end
 end
