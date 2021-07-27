088844c38712ffdca9998be99551ca3d79cd2101
Patrick Kurth <p.kurth@posteo.de>
2021-07-27 21:40:15 +0200

    Continue to work on polyres

--- cells/basic/polyresistor.lua
+++ cells/basic/polyresistor.lua
@@ -5,15 +5,20 @@ function parameters()
         { "space", 120 },
         { "extension", 400 },
         { "contactheight", 200 },
-        { "nfingers", 4 }
+        { "nfingers", 4 },
+        { "dummies", 2 }
     )
 end
 
 function layout(res, _P)
     -- poly strips
-    res:merge_into_shallow(geometry.multiple_x(geometry.rectangle(generics.other("gate"), _P.width, _P.length + 2 * _P.extension + 2 * _P.contactheight), _P.nfingers, _P.width + _P.space))
+    res:merge_into_shallow(geometry.multiple_x(geometry.rectangle(generics.other("gate"), _P.width, _P.length + 2 * _P.extension + 2 * _P.contactheight), _P.nfingers + _P.dummies, _P.width + _P.space))
     -- contacts
     res:merge_into_shallow(geometry.multiple_xy(geometry.rectangle(generics.contact("gate"), _P.width, _P.contactheight), _P.nfingers, 2, _P.width + _P.space, _P.length + 2 * _P.extension + _P.contactheight))
     -- poly marker layer
-    res:merge_into_shallow(geometry.rectangle(generics.other("polyres"), _P.nfingers * (_P.width + _P.space), _P.length))
+    res:merge_into_shallow(geometry.rectangle(generics.other("polyres"), (_P.nfingers + _P.dummies) * (_P.width + _P.space), _P.length))
+    -- implant
+    res:merge_into_shallow(geometry.other(generics.other("nimpl"), (_P.nfingers + _P.dummies) * (_P.width + _P.space), _P.length + 2 * _P.extension + 2 * _P.contactheight))
+    -- well
+    res:merge_into_shallow(geometry.other(generics.other("nwell"), (_P.nfingers + _P.dummies) * (_P.width + _P.space), _P.length + 2 * _P.extension + 2 * _P.contactheight))
 end
