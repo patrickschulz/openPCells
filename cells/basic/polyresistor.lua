0ad4f68f0d077fdb526699a88473c937ebd42010
Patrick Kurth <p.kurth@posteo.de>
2021-07-27 21:35:55 +0200

    Updated polyres

--- cells/basic/polyresistor.lua
+++ cells/basic/polyresistor.lua
@@ -11,7 +11,7 @@ end
 
 function layout(res, _P)
     -- poly strips
-    res:merge_into_shallow(geometry.multiple_x(geometry.rectangle(generics.other("gate"), _P.width, _P.length + 2 * _P.extension), _P.nfingers, _P.width + _P.space))
+    res:merge_into_shallow(geometry.multiple_x(geometry.rectangle(generics.other("gate"), _P.width, _P.length + 2 * _P.extension + 2 * _P.contactheight), _P.nfingers, _P.width + _P.space))
     -- contacts
     res:merge_into_shallow(geometry.multiple_xy(geometry.rectangle(generics.contact("gate"), _P.width, _P.contactheight), _P.nfingers, 2, _P.width + _P.space, _P.length + _P.extension + _P.contactheight))
     -- poly marker layer
