a0571bc3661061c85436fb62a219bcc0d341f2c1
Patrick Schulz <pschulz@posteo.de>
2021-07-28 09:54:42 +0200

    Updated pads

---
+++ cells/auxiliary/pad.lua
@@ -0,0 +1,33 @@
+function parameters()
+    pcell.add_parameters(
+        { "padwidth(Width of Pad)",                   60000 },
+        { "padheight(Height of Pad)",                 80000 },
+        { "padopeningwidth(Width of Pad Opening)",    50000 },
+        { "padopeningheight(Height of Pad Opening)",  70000 },
+        { "orientation(Pad Orientation)",         "horizontal", posvals = set("horizontal", "vertical") },
+        { "alignment(Pad Alignment)",                 "center", posvals = set("center", "top/left", "bottom/right") }
+    )
+end
+
+function layout(pad, _P)
+    local metal, marker
+    local xshift, yshift = 0, 0
+    if _P.alignment == "top/left" then
+        xshift = (_P.orientation == "vertical") and _P.padheight / 2 or 0
+        yshift = (_P.orientation == "horizontal") and -_P.padheight / 2 or 0
+    elseif _P.alignment == "bottom/right" then
+        xshift = (_P.orientation == "vertical") and -_P.padheight / 2 or 0
+        yshift = (_P.orientation == "horizontal") and _P.padheight / 2 or 0
+    end
+    if _P.orientation == "horizontal" then
+        metal = geometry.rectangle(generics.metal(-1), _P.padwidth, _P.padheight)
+        marker = geometry.rectangle(generics.other("padopening"), _P.padopeningwidth, _P.padopeningheight)
+    else -- vertical
+        metal = geometry.rectangle(generics.metal(-1), _P.padheight, _P.padwidth)
+        marker = geometry.rectangle(generics.other("padopening"), _P.padopeningheight, _P.padopeningwidth)
+    end
+    metal:translate(xshift, yshift)
+    marker:translate(xshift, yshift)
+    pad:merge_into_shallow(metal)
+    pad:merge_into_shallow(marker)
+end
--- cells/auxiliary/pads.lua
+++ cells/auxiliary/pads.lua
@@ -1,33 +1,78 @@
 function parameters()
+    pcell.reference_cell("auxiliary/pad")
     pcell.add_parameters(
-        { "numpads(Number of Pads)",                      3 },
-        { "padwidth(Width of Pad)",                   60000 },
-        { "padheight(Height of Pad)",                 80000 },
-        { "padopeningwidth(Width of Pad Opening)",    50000 },
-        { "padopeningheight(Height of Pad Opening)",  70000 },
-        { "padpitch(Pitch between Pads)",            100000 },
-        { "orientation(Pad Orientation)",      "horizontal" }
+        { "padconfig(Pad Configuration; G, S or P)",      { "P", "P", "P" }, argtype = "strtable" },
+        { "Spadwidth(Width of S-Pad)",                   50000 },
+        { "Spadheight(Height of S-Pad)",                 54000 },
+        { "Spadopeningwidth(Width of S-Pad Opening)",    40000 },
+        { "Spadopeningheight(Height of S-Pad Opening)",  44000 },
+        { "Gpadwidth(Width of G-Pad)",                   60000 },
+        { "Gpadheight(Height of G-Pad)",                 80000 },
+        { "Gpadopeningwidth(Width of G-Pad Opening)",    60000 },
+        { "Gpadopeningheight(Height of G-Pad Opening)",  70000 },
+        { "Ppadwidth(Width of P-Pad)",                   60000 },
+        { "Ppadheight(Height of P-Pad)",                 80000 },
+        { "Ppadopeningwidth(Width of P-Pad Opening)",    50000 },
+        { "Ppadopeningheight(Height of P-Pad Opening)",  70000 },
+        { "padpitch(Pitch between Pads)",               100000 },
+        { "orientation(Pad Orientation)",         "horizontal", posvals = set("horizontal", "vertical") },
+        { "alignment(Pad Alignment)",                 "center", posvals = set("center", "top/left", "bottom/right") }
     )
 end
 
 function layout(pads, _P)
-    if _P.orientation == "horizontal" then
-        pads:merge_into_shallow(geometry.multiple_x(
-            geometry.rectangle(generics.metal(-1), _P.padwidth, _P.padheight),
-            _P.numpads, _P.padpitch
-        ))
-        pads:merge_into_shallow(geometry.multiple_x(
-            geometry.rectangle(generics.other("padopening"), _P.padopeningwidth, _P.padopeningheight),
-            _P.numpads, _P.padpitch
-        ))
-    else -- vertical
-        pads:merge_into_shallow(geometry.multiple_y(
-            geometry.rectangle(generics.metal(-1), _P.padheight, _P.padwidth),
-            _P.numpads, _P.padpitch
-        ))
-        pads:merge_into_shallow(geometry.multiple_y(
-            geometry.rectangle(generics.other("padopening"), _P.padopeningheight, _P.padopeningwidth),
-            _P.numpads, _P.padpitch
-        ))
+    pcell.push_overwrites("auxiliary/pad", { 
+        orientation = _P.orientation,
+        alignment = _P.alignment
+    })
+    pcell.push_overwrites("auxiliary/pad", { 
+        padwidth = _P.Spadwidth, 
+        padheight = _P.Spadheight, 
+        padopeningwidth = _P.Spadopeningwidth, 
+        padopeningheight = _P.Spadopeningheight,
+    })
+    local Spad = pcell.add_cell_reference(pcell.create_layout("auxiliary/pad"), "Spad")
+    pcell.pop_overwrites("auxiliary/pad")
+
+    pcell.push_overwrites("auxiliary/pad", { 
+        padwidth = _P.Gpadwidth, 
+        padheight = _P.Gpadheight, 
+        padopeningwidth = _P.Gpadopeningwidth, 
+        padopeningheight = _P.Gpadopeningheight,
+    })
+    local Gpad = pcell.add_cell_reference(pcell.create_layout("auxiliary/pad"), "Gpad")
+    pcell.pop_overwrites("auxiliary/pad")
+
+    pcell.push_overwrites("auxiliary/pad", { 
+        padwidth = _P.Ppadwidth, 
+        padheight = _P.Ppadheight, 
+        padopeningwidth = _P.Ppadopeningwidth, 
+        padopeningheight = _P.Ppadopeningheight,
+    })
+    local Ppad = pcell.add_cell_reference(pcell.create_layout("auxiliary/pad"), "Ppad")
+    pcell.pop_overwrites("auxiliary/pad")
+    pcell.pop_overwrites("auxiliary/pad")
+
+    local numpads = #_P.padconfig
+    for i, padtype in ipairs(_P.padconfig) do
+        local pad
+        if padtype == "S" then
+            pad = pads:add_child(Spad)
+        elseif padtype == "G" then
+            pad = pads:add_child(Gpad)
+        else
+            pad = pads:add_child(Ppad)
+        end
+        if _P.orientation == "horizontal" then
+            if _P.alignment == "top/left" then
+                pad:translate((i - 1) * _P.padpitch - (numpads - 1) * _P.padpitch / 2, 0)
+            elseif _P.alignment == "bottom/right" then
+                pad:translate((i - 1) * _P.padpitch - (numpads - 1) * _P.padpitch / 2, 0)
+            else -- center
+                pad:translate((i - 1) * _P.padpitch - (numpads - 1) * _P.padpitch / 2, 0)
+            end
+        else -- vertical
+            pad:translate(0, (i - 1) * _P.padpitch - (numpads - 1) * _P.padpitch / 2)
+        end
     end
 end
