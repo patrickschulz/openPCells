3469947632d9808d8b4d32e9617efd7280acd56b
Patrick Kurth <p.kurth@posteo.de>
2021-08-24 10:47:50 +0200

    Fixed padring

--- cells/auxiliary/padring.lua
+++ cells/auxiliary/padring.lua
@@ -8,8 +8,8 @@ end
 
 function layout(padring, _P)
     local padconfig = util.fill_all_with(_P.padsperside, "P")
-    padring:merge_into_shallow(pcell.create_layout("auxiliary/pads", { padpitch = _P.padpitch, padconfig = padconfig, orientation = "vertical" }):translate( (_P.padsperside - 1) * _P.padpitch / 2 + _P.sidedistance, 0))
-    padring:merge_into_shallow(pcell.create_layout("auxiliary/pads", { padpitch = _P.padpitch, padconfig = padconfig, orientation = "vertical" }):translate(-(_P.padsperside - 1) * _P.padpitch / 2 - _P.sidedistance, 0))
-    padring:merge_into_shallow(pcell.create_layout("auxiliary/pads", { padpitch = _P.padpitch, padconfig = padconfig, orientation = "horizontal" }):translate(0,  (_P.padsperside - 1) * _P.padpitch / 2 + _P.sidedistance))
-    padring:merge_into_shallow(pcell.create_layout("auxiliary/pads", { padpitch = _P.padpitch, padconfig = padconfig, orientation = "horizontal" }):translate(0, -(_P.padsperside - 1) * _P.padpitch / 2 - _P.sidedistance))
+    padring:merge_into_shallow(pcell.create_layout("auxiliary/pads", { padpitch = _P.padpitch, padconfig = padconfig, orientation = "vertical" }):translate( (_P.padsperside - 1) * _P.padpitch / 2 + _P.sidedistance, 0):flatten())
+    padring:merge_into_shallow(pcell.create_layout("auxiliary/pads", { padpitch = _P.padpitch, padconfig = padconfig, orientation = "vertical" }):translate(-(_P.padsperside - 1) * _P.padpitch / 2 - _P.sidedistance, 0):flatten())
+    padring:merge_into_shallow(pcell.create_layout("auxiliary/pads", { padpitch = _P.padpitch, padconfig = padconfig, orientation = "horizontal" }):translate(0,  (_P.padsperside - 1) * _P.padpitch / 2 + _P.sidedistance):flatten())
+    padring:merge_into_shallow(pcell.create_layout("auxiliary/pads", { padpitch = _P.padpitch, padconfig = padconfig, orientation = "horizontal" }):translate(0, -(_P.padsperside - 1) * _P.padpitch / 2 - _P.sidedistance):flatten())
 end
