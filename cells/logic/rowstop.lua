6966edc942b5c1c77c2afa3a9f51f86cdae29c4e
Patrick Kurth <p.kurth@posteo.de>
2021-08-26 21:15:15 +0200

    Added tuckgatemarker and gatecut

--- cells/logic/rowstop.lua
+++ cells/logic/rowstop.lua
@@ -20,13 +20,32 @@ function layout(gate, _P)
     gate:merge_into_shallow(harness)
     gate:inherit_alignment_box(harness)
 
-    local height = bp.pwidth + bp.nwidth + bp.numinnerroutes * bp.gstwidth + (bp.numinnerroutes + 1) * bp.gstspace + 2 * bp.powerspace + bp.powerwidth + 2 * math.max(tp.cutheight / 2 + bp.gateext, bp.dummycontheight / 2)
+    local separation = bp.numinnerroutes * bp.gstwidth + (bp.numinnerroutes + 1) * bp.gstspace
+    local height = bp.pwidth + bp.nwidth + separation + 2 * bp.powerspace + bp.powerwidth + 2 * math.max(tp.cutheight / 2 + bp.gateext, bp.dummycontheight / 2)
     local numgates = #_P.glengths
     local startx = gate:get_anchor("right"):getx()
     for i = 1, numgates do
         local gl = _P.glengths[i]
         local gs = _P.gspaces[i]
+        -- gates
         gate:merge_into_shallow(geometry.rectangle(generics.other("gate"), gl, height)
             :translate(startx - (bp.glength + bp.gspace) / 2 + (gl + gs) * -(i - 1), 0))
+        -- tuck gate marking
+        if i == 1 then
+            gate:merge_into_shallow(geometry.rectangle(generics.other("tuckgatemarker"), gl, height / 2 - tp.cutheight / 2)
+                :translate(startx - (bp.glength + bp.gspace) / 2 + (gl + gs) * -(i - 1), (height / 2 + tp.cutheight / 2) / 2))
+            gate:merge_into_shallow(geometry.rectangle(generics.other("tuckgatemarker"), gl, height / 2 - tp.cutheight / 2)
+                :translate(startx - (bp.glength + bp.gspace) / 2 + (gl + gs) * -(i - 1), -(height / 2 + tp.cutheight / 2) / 2))
+        end
     end
+
+    -- implants
+    gate:merge_into_shallow(geometry.rectanglebltr(generics.other("pimpl"), 
+        gate:get_anchor("left"), gate:get_anchor("topright")))
+    gate:merge_into_shallow(geometry.rectanglebltr(generics.other("nimpl"), 
+        gate:get_anchor("bottomleft"), gate:get_anchor("right")))
+
+    -- gate cuts
+    gate:merge_into_shallow(geometry.rectangle(generics.other("gatecut"), bp.glength + bp.gspace, tp.cutheight)
+        :translate(gate:get_anchor("right"):translate(-(bp.glength + bp.gspace) / 2, 0)))
 end
