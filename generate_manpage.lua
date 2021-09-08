36687ae00ab300f7c3c1c96335d35c2b2b378dc3
Patrick Kurth <p.kurth@posteo.de>
2021-09-09 00:01:04 +0200

    improved manpage generator

--- generate_manpage.lua
+++ generate_manpage.lua
@@ -1,11 +1,13 @@
 local entries = {}
-local entryfunc = function(t) table.insert(entries, t) end
-store_multiple = entryfunc
-store_multiple_string = entryfunc
-store = entryfunc
-switch = entryfunc
-section = entryfunc
-consumer_string = entryfunc
+local entryfunc = function(what) 
+    return function(t) table.insert(entries, { short = t.short, long = t.long, help = t.help, what = what }) end
+end
+store_multiple = entryfunc()
+store_multiple_string = entryfunc()
+store = entryfunc("arg")
+switch = entryfunc()
+section = function(s) table.insert(entries, s) end
+consumer_string = entryfunc()
 
 local cmdoptions = require "cmdoptions"
 
@@ -23,17 +25,22 @@ is a technology-independent layout generator for integrated circuits with suppor
 -- print options
 for _, entry in ipairs(entries) do
     if type(entry) == "string" then
-        print(entry)
+        print(string.format('.SS "%s"', entry))
     else
-        if entry.short then
-            if entry.long then
-                print(string.format('.IP "%s,%s"', entry.short, entry.long))
-            else
-                print(string.format('.IP "%s"', entry.short))
-            end
-        else
-            print(string.format('.IP "%s"', entry.long))
+        local fmt = '.IP "\\fB\\%s\\fR %s" 4'
+        local entryfmt = ""
+        if entry.short and entry.long then
+            entryfmt = string.format("%s,%s", entry.short, entry.long)
+        elseif entry.short then
+            entryfmt = string.format("%s", entry.short)
+        elseif entry.long then
+            entryfmt = string.format("%s", entry.long)
         end
+        local argfmt = ""
+        if entry.what then
+            argfmt = string.format("\\fI%s\\fR", entry.what)
+        end
+        print(string.format(fmt, entryfmt, argfmt))
         print(entry.help)
     end
     print()
