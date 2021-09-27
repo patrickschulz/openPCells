ca2e36d6b445efebe4e4d0645162a1970ae778c5
Patrick Kurth <p.kurth@posteo.de>
2021-09-27 13:01:15 +0200

    Improved placement.digital

--- placement.lua
+++ placement.lua
@@ -1,8 +1,9 @@
 local M = {}
 
-function M.digital(parent, cellnames, rows, columns)
+function M.digital(parent, cellnames, startanchor, growdirection)
     local last = object.create_omni()
-    local lastleft = object.create_omni()
+    local lastleft
+    growdirection = growdirection or "upright"
     local cells = {}
     for r, row in ipairs(cellnames) do
         cells[r] = {}
@@ -12,10 +13,24 @@ function M.digital(parent, cellnames, rows, columns)
                 cell:flipy()
             end
             if c == 1 then
-                cell:move_anchor("bottom", lastleft:get_anchor("top"))
+                if r == 1 then
+                    if startanchor then
+                        cell:move_anchor(startanchor)
+                    end
+                else
+                    if string.match(growdirection, "up") then
+                        cell:move_anchor("bottom", lastleft:get_anchor("top"))
+                    else
+                        cell:move_anchor("top", lastleft:get_anchor("bottom"))
+                    end
+                end
                 lastleft = cell
             else
-                cell:move_anchor("left", last:get_anchor("right"))
+                if string.match(growdirection, "left") then
+                    cell:move_anchor("right", last:get_anchor("left"))
+                else
+                    cell:move_anchor("left", last:get_anchor("right"))
+                end
             end
             last = cell
             cells[r][c] = cell
