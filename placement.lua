7156d5d70d32ef3397d0c4b4f3f5c2721daf7cd6
Patrick Kurth <p.kurth@posteo.de>
2021-09-28 08:24:34 +0200

    Improved digital cell placement

--- placement.lua
+++ placement.lua
@@ -6,15 +6,36 @@ function M.digital(parent, cellnames, startanchor, startpt, growdirection)
     growdirection = growdirection or "upright"
     startpt = startpt or point.create(0, 0)
     local cells = {}
-    for r, row in ipairs(cellnames) do
-        cells[r] = {}
-        for c, cellname in ipairs(row) do
+    for row, entries in ipairs(cellnames) do
+        cells[row] = {}
+        for column, entry in ipairs(entries) do
+            local cellname
+            if type(entry) == "string" then
+                cellname = entry
+            else
+                cellname = entry.cell
+            end
+
+            -- add child to parent
             local cell = parent:add_child(cellname)
-            if r % 2 == 0 then
+
+            -- process extra arguments
+            if type(entry) == "table" then
+                for _, extra in ipairs(entry) do
+                    if extra == "flipx" then
+                        cell:flipx()
+                    end
+                end
+            end
+
+            -- flip every second row
+            if row % 2 == 0 then
                 cell:flipy()
             end
-            if c == 1 then
-                if r == 1 then
+
+            -- place cell relative the previous cells
+            if column == 1 then
+                if row == 1 then
                     if startanchor then
                         cell:move_anchor(startanchor, startpt)
                     end
@@ -33,8 +54,10 @@ function M.digital(parent, cellnames, startanchor, startpt, growdirection)
                     cell:move_anchor("left", last:get_anchor("right"))
                 end
             end
+
+            -- store cells for later use
             last = cell
-            cells[r][c] = cell
+            cells[row][column] = cell
         end
     end
     return cells
