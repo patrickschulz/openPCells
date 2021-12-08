592039aaf34dd96e198f8970bd8ed46733e6842e
Patrick Kurth <p.kurth@posteo.de>
2021-12-08 17:05:51 +0100

    Improved and cleaned up placement.lua

--- src/placement.lua
+++ src/placement.lua
@@ -1,72 +1,5 @@
 local M = {}
 
-local function _fix_to_grid(num, grid)
-    return grid * math.floor(num / grid)
-end
-
-function M.digital(parent, cellnames, noflipeven, startanchor, startpt, growdirection)
-    local last = object.create_omni()
-    local lastleft
-    growdirection = growdirection or "upright"
-    startpt = startpt or point.create(0, 0)
-    local cells = {}
-    for row, entries in ipairs(cellnames) do
-        cells[row] = {}
-        for column, entry in ipairs(entries) do
-            local cellname
-            if type(entry) == "string" then
-                cellname = entry
-            else
-                cellname = entry.cell
-            end
-
-            -- add child to parent
-            local cell = parent:add_child(cellname, string.format("I_%d_%d", row, column))
-
-            -- process extra arguments
-            if type(entry) == "table" then
-                for _, extra in ipairs(entry) do
-                    if extra == "flipx" then
-                        cell:flipx()
-                    end
-                end
-            end
-
-            -- if necessary, flip every second row
-            if not noflipeven and (row % 2 == 0) then
-                cell:flipy()
-            end
-
-            -- place cell relative the previous cells
-            if column == 1 then
-                if row == 1 then
-                    if startanchor then
-                        cell:move_anchor(startanchor, startpt)
-                    end
-                else
-                    if string.match(growdirection, "up") then
-                        cell:move_anchor("bottom", lastleft:get_anchor("top"))
-                    else
-                        cell:move_anchor("top", lastleft:get_anchor("bottom"))
-                    end
-                end
-                lastleft = cell
-            else
-                if string.match(growdirection, "left") then
-                    cell:move_anchor("right", last:get_anchor("left"))
-                else
-                    cell:move_anchor("left", last:get_anchor("right"))
-                end
-            end
-
-            -- store cells for later use
-            last = cell
-            cells[row][column] = cell
-        end
-    end
-    return cells
-end
-
 local warningissued = false
 local function _add_fillers(parent, fillers, numfill, anchor)
     local inserted = {}
@@ -80,10 +13,9 @@ local function _add_fillers(parent, fillers, numfill, anchor)
                 anchor = fill:get_anchor("right")
                 numfill = search
                 search = 0
-            else
+            elseif search < numfill then
                 search = search + 1
-            end
-            if search >= numfill then
+            else
                 if not warningissued then
                     print("Can't find solution to insert fillers.")
                     print("This happens, when the fill cells can not be combined to form any arbitrary integer")
@@ -97,48 +29,44 @@ local function _add_fillers(parent, fillers, numfill, anchor)
     return inserted
 end
 
-function M.digital_auto(parent, pitch, width, cellnames, fillers, utilization, noflipeven, startanchor, startpt, growdirection)
+function M.digital(parent, pitch, width, cellnames, fillers, utilization, noflipeven, startanchor, startpt, growdirection)
     check_number_range(utilization, { lower = 0, lowerinclusive = false, upper = 1, upperinclusive = true })
-    local last = object.create_omni()
+    local last
     local lastleft
     startanchor = startanchor or "left"
     growdirection = growdirection or "upright"
     startpt = startpt or point.create(0, 0)
-    local cells = { {} } -- at least one row
-    local rowwidths = { 0 }
-    local row = 1
-    local column = 0
-    for _, cellname in ipairs(cellnames) do
-        local cell = parent:add_child(cellname, string.format("I_%d_%d", row, column + 1))
-        local w = cell:width_height_alignmentbox()
-        if not lastleft then -- first cell
-            cell:move_anchor(startanchor, startpt)
-            lastleft = cell
-        else
-            if rowwidths[row] + w > width * utilization then -- new row
-                cell:move_anchor("bottomleft", lastleft:get_anchor("topleft"))
+    local cells = {}
+    local rowwidths = {}
+
+    for row, entries in ipairs(cellnames) do
+        cells[row] = {}
+        rowwidths[row] = 0
+        for column, cellname in ipairs(entries) do
+            local cell = parent:add_child(cellname, string.format("I_%d_%d", row, column + 1))
+            local w = cell:width_height_alignmentbox()
+            if column == 1 then
+                if row == 1 then -- first cell
+                    cell:move_anchor(startanchor, startpt)
+                    lastleft = cell
+                else
+                    cell:move_anchor("bottomleft", lastleft:get_anchor("topleft"))
+                end
                 lastleft = cell
-                row = row + 1
-                column = 0
-                cells[row] = {}
-                rowwidths[row] = 0
-            else -- current row
+            else
                 cell:move_anchor("left", last:get_anchor("right"))
             end
+
+            last = cell
+            rowwidths[row] = rowwidths[row] + w
+
+            cells[row][column] = cell
         end
-
-        last = cell
-        rowwidths[row] = rowwidths[row] + w
-
-        column = column + 1
-        cells[row][column] = cell
     end
 
     -- equalize cells and insert fillers
     for row, cs in ipairs(cells) do
-        local widthdiff = width
-        for _, c in ipairs(cs) do widthdiff = widthdiff - c:width_height_alignmentbox() end
-        local diff = widthdiff // pitch
+        local diff = (width - rowwidths[row]) // pitch
         if #cs > 1 then
             -- calculate width of holes
             local delta = diff // (#cs - 1) -- distributed correction (equal for all holes)
@@ -192,17 +120,6 @@ function M.digital_auto(parent, pitch, width, cellnames, fillers, utilization, n
     end
 
     return cells
-end
-
-function M.digital_regular(parent, cellname, rows, columns, noflipeven, startanchor, startpt, growdirection)
-    local cellnames = {}
-    for row = 1, rows do
-        cellnames[row] = {}
-        for column = 1, columns do
-            cellnames[row][column] = cellname
-        end
-    end
-    return M.digital(parent, cellnames, noflipeven, startanchor, startpt, growdirection)
 end
 
 return M
