0129c895055ee577c18e72967a3979872e462371
Patrick Kurth <p.kurth@posteo.de>
2022-01-10 12:13:57 +0100

    updated logic/counter

--- cells/logic/counter.lua
+++ cells/logic/counter.lua
@@ -27,32 +27,18 @@ function parameters()
 end
 
 function layout(counter, _P)
-    -- generate cell layouts
-    --pcell.push_overwrites("stdcells/base", { leftdummies = 1, rightdummies = 1 })
-    --local dffpref = pcell.create_layout("stdcells/dff", { clockpolarity = "positive", enable_Q = true, enable_QN = false })
-    --local dffnref = pcell.create_layout("stdcells/dff", { clockpolarity = "negative", enable_Q = true, enable_QN = false })
-    --local xorref  = pcell.create_layout("stdcells/xor_gate")
-    --local orref   = pcell.create_layout("stdcells/or_gate")
-    --pcell.pop_overwrites("stdcells/base")
-
-    ---- create references
-    --local dffpname = pcell.add_cell_reference(dffpref, "dffp")
-    --local dffnname = pcell.add_cell_reference(dffnref, "dffn")
-    --local xorname  = pcell.add_cell_reference(xorref, "xor")
-    --local orname   = pcell.add_cell_reference(orref, "or")
-
-    local rows = {}
+    local cellnames = {}
     for i = 1, _P.numrows do
         local row = {}
         for j = 1, _P.numcolumns do
-            table.insert(row, { instance = string.format("dffp_%d_%d", i, j), reference = "dff" })
-            table.insert(row, { instance = string.format("xor_%d_%d", i, j), reference = "xor_gate" })
-            table.insert(row, { instance = string.format("or_%d_%d", i, j), reference = "or_gate" })
-            table.insert(row, { instance = string.format("dffn_%d_%d", i, j), reference = "dff" })
+            table.insert(row, "dffp")
+            table.insert(row, "xor_gate")
+            table.insert(row, "or_gate")
+            table.insert(row, "dffn")
         end
-        table.insert(rows, row)
+        table.insert(cellnames, row)
     end
-    table.insert(rows, { "dff" })
-    local cells = placement.digital(counter, 100, rows)
+    local rows = placement.create_reference_rows(cellnames)
+    local cells = placement.digital(counter, rows, 80)
 end
 
