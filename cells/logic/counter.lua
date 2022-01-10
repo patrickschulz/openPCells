e3f8e0ec1225e59a5a601b0c81a34df3c5466d73
Patrick Kurth <p.kurth@posteo.de>
2022-01-10 01:25:53 +0100

    Updated counter to use placement.digital

--- cells/logic/counter.lua
+++ cells/logic/counter.lua
@@ -17,6 +17,7 @@ could go with a different approach.
          vss,carry[0:N-1] --*---------OR
 
 --]]
+
 function parameters()
     pcell.reference_cell("stdcells/base")
     pcell.add_parameters(
@@ -27,60 +28,31 @@ end
 
 function layout(counter, _P)
     -- generate cell layouts
-    pcell.push_overwrites("stdcells/base", { leftdummies = 1, rightdummies = 1 })
-    local dffpref = pcell.create_layout("stdcells/dff", { clockpolarity = "positive", enable_Q = true, enable_QN = false })
-    local dffnref = pcell.create_layout("stdcells/dff", { clockpolarity = "negative", enable_Q = true, enable_QN = false })
-    local xorref  = pcell.create_layout("stdcells/xor_gate")
-    local orref   = pcell.create_layout("stdcells/or_gate")
-    pcell.pop_overwrites("stdcells/base")
+    --pcell.push_overwrites("stdcells/base", { leftdummies = 1, rightdummies = 1 })
+    --local dffpref = pcell.create_layout("stdcells/dff", { clockpolarity = "positive", enable_Q = true, enable_QN = false })
+    --local dffnref = pcell.create_layout("stdcells/dff", { clockpolarity = "negative", enable_Q = true, enable_QN = false })
+    --local xorref  = pcell.create_layout("stdcells/xor_gate")
+    --local orref   = pcell.create_layout("stdcells/or_gate")
+    --pcell.pop_overwrites("stdcells/base")
 
-    -- create references
-    local dffpname = pcell.add_cell_reference(dffpref, "dffp")
-    local dffnname = pcell.add_cell_reference(dffnref, "dffn")
-    local xorname  = pcell.add_cell_reference(xorref, "xor")
-    local orname   = pcell.add_cell_reference(orref, "or")
+    ---- create references
+    --local dffpname = pcell.add_cell_reference(dffpref, "dffp")
+    --local dffnname = pcell.add_cell_reference(dffnref, "dffn")
+    --local xorname  = pcell.add_cell_reference(xorref, "xor")
+    --local orname   = pcell.add_cell_reference(orref, "or")
 
-    -- place cells
     local rows = {}
     for i = 1, _P.numrows do
+        local row = {}
         for j = 1, _P.numcolumns do
-            local column = {}
-
-            -- positive flip flop
-            column.dffp = counter:add_child(dffpname)
-
-            if i > 1 then
-                column.dffp:move_anchor("bottom", rows[i - 1][1].dffp:get_anchor("top"))
-            end
-            if j > 1 then
-                column.dffp:move_anchor("left", rows[i][j - 1].dffn:get_anchor("right"))
-            end
-
-            -- xor gate
-            column.xorgate = counter:add_child(xorname)
-            column.xorgate:move_anchor("left", column.dffp:get_anchor("right"))
-
-            -- or gate
-            column.orgate = counter:add_child(orname)
-            column.orgate:move_anchor("left", column.xorgate:get_anchor("right"))
-
-            -- positive flip flop
-            column.dffn = counter:add_child(dffnname)
-            column.dffn:move_anchor("left", column.orgate:get_anchor("right"))
-
-            -- save row
-            if j == 1 then
-                rows[i] = {}
-            end
-            rows[i][j] = column
+            table.insert(row, { instance = string.format("dffp_%d_%d", i, j), reference = "dff" })
+            table.insert(row, { instance = string.format("xor_%d_%d", i, j), reference = "xor_gate" })
+            table.insert(row, { instance = string.format("or_%d_%d", i, j), reference = "or_gate" })
+            table.insert(row, { instance = string.format("dffn_%d_%d", i, j), reference = "dff" })
         end
-        -- flip every second row
-        if i % 2 == 0 then
-            for _, column in ipairs(rows[i]) do
-                for _, gate in pairs(column) do
-                    gate:flipy()
-                end
-            end
-        end
+        table.insert(rows, row)
     end
+    table.insert(rows, { "dff" })
+    local cells = placement.digital(counter, 100, rows)
 end
+
