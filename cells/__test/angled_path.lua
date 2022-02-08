b986f8b50ddad69033d5381c2db15dc79c39cec9
Patrick Kurth <p.kurth@posteo.de>
2022-02-08 09:03:12 +0100

    Edited format of angled_path

--- cells/__test/angled_path.lua
+++ cells/__test/angled_path.lua
@@ -3,5 +3,9 @@ function parameters()
 end
 
 function layout(cell)
-    cell:merge_into_shallow(geometry.any_angle_path(generics.metal(1), { point.create(0, 0), point.create(10000, 100000) }, 100, 100, true, true))
+    cell:merge_into_shallow(geometry.any_angle_path(generics.metal(1), {
+        point.create(0, 0),
+        point.create(10000, 100000),
+        point.create(15000, 100000),
+    }, 5000, 100, true, true))
 end
