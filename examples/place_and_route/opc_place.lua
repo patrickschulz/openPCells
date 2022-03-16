148604ecf4a96d7d6c197e869b82b26720de6aea
philippnickel <philipp.nickel@campus.tu-berlin.de>
2022-03-16 12:37:30 +0100

    fixed pnr script

--- examples/place_and_route/opc_place.lua
+++ examples/place_and_route/opc_place.lua
@@ -2,7 +2,7 @@ local module = "counter"
 local exporttype = "gds"
 
 local content = generator.from_verilog(
-    string.format("%s.v", module), -- verilog file to be read
+    string.format("examples/place_and_route/%s.v", module), -- verilog file to be read
     0.5, -- utilization
     1, -- aspect ratio
     { "clk", "_mem.clk", "vdd", "vss", "in", "out", }, -- excluded nets
@@ -10,7 +10,7 @@ local content = generator.from_verilog(
 )
 generator.write_from_verilog(content, "verilogimport", "verilogimport")
 
-technology.add_techpath(string.format("%s/tech", _get_opc_home()))
+--technology.add_techpath(string.format("%s/tech", _get_opc_home()))
 technology.load("GF22FDSOI")
 pcell.append_cellpath(string.format("%s/%s", _get_opc_home(), "cells"))
 pcell.append_cellpath("verilogimport")
