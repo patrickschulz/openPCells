a97d8da664757838c74fe6e03b568f0a17cedd41
Patrick Kurth <p.kurth@posteo.de>
2022-03-20 12:45:16 +0100

    Fixed segfault in rectangle union

--- src/union.c
+++ src/union.c
@@ -35,107 +35,112 @@ static order_t rect_order(coordinate_t bl1, coordinate_t tr1, coordinate_t bl2, 
     return NOINTERSECTION;
 }
 
-bool rectangle_union(
-    coordinate_t bl1x, coordinate_t bl1y, coordinate_t tr1x, coordinate_t tr1y, 
-    coordinate_t bl2x, coordinate_t bl2y, coordinate_t tr2x, coordinate_t tr2y, 
-    coordinate_t* blx, coordinate_t* bly, coordinate_t* trx, coordinate_t* try
-)
+shape_t* rectangle_union(shape_t* rect1, shape_t* rect2)
 {
+    coordinate_t bl1x = rect1->points[0]->x;
+    coordinate_t bl1y = rect1->points[0]->y;
+    coordinate_t tr1x = rect1->points[1]->x;
+    coordinate_t tr1y = rect1->points[1]->y;
+    coordinate_t bl2x = rect2->points[0]->x;
+    coordinate_t bl2y = rect2->points[0]->y;
+    coordinate_t tr2x = rect2->points[1]->x;
+    coordinate_t tr2y = rect2->points[1]->y;
     order_t xorder = rect_order(bl1x, tr1x, bl2x, tr2x);
     order_t yorder = rect_order(bl1y, tr1y, bl2y, tr2y);
     if(xorder == NOINTERSECTION || yorder == NOINTERSECTION)
     {
-        return false;
+        return NULL;
     }
     if(xorder != EQUAL && yorder != EQUAL) // polygon union, one order has to be EQUAL for rectangle union
     {
-        return false;
+        return NULL;
     }
+    coordinate_t blx, bly, trx, try;
     switch(xorder)
     {
         case HALFEQUALLEFTREGULAR:
-            *blx = bl1x;
-            *trx = tr2x;
+            blx = bl1x;
+            trx = tr2x;
             break;
         case HALFEQUALLEFTINVERSE:
-            *blx = bl1x;
-            *trx = tr1x;
+            blx = bl1x;
+            trx = tr1x;
             break;
         case HALFEQUALRIGHTREGULAR:
-            *blx = bl1x;
-            *trx = tr1x;
+            blx = bl1x;
+            trx = tr1x;
             break;
         case HALFEQUALRIGHTINVERSE:
-            *blx = bl2x;
-            *trx = tr1x;
+            blx = bl2x;
+            trx = tr1x;
             break;
         case EQUAL:
-            *blx = bl1x;
-            *trx = tr1x;
+            blx = bl1x;
+            trx = tr1x;
             break;
         case OUTER:
-            *blx = bl1x;
-            *trx = tr1x;
+            blx = bl1x;
+            trx = tr1x;
             break;
         case INNER:
-            *blx = bl2x;
-            *trx = tr2x;
+            blx = bl2x;
+            trx = tr2x;
             break;
         case REGULAR:
-            *blx = bl1x;
-            *trx = tr2x;
+            blx = bl1x;
+            trx = tr2x;
             break;
         case INVERSE:
-            *blx = bl2x;
-            *trx = tr1x;
+            blx = bl2x;
+            trx = tr1x;
             break;
-        case NOINTERSECTION:
-            return false;
+        default: // silence warning about not handling NOINTERSECTION, which is handled earlier
             break;
     }
     switch(yorder)
     {
         case HALFEQUALLEFTREGULAR:
-            *bly = bl1y;
-            *try = tr2y;
+            bly = bl1y;
+            try = tr2y;
             break;
         case HALFEQUALLEFTINVERSE:
-            *bly = bl1y;
-            *try = tr1y;
+            bly = bl1y;
+            try = tr1y;
             break;
         case HALFEQUALRIGHTREGULAR:
-            *bly = bl1y;
-            *try = tr1y;
+            bly = bl1y;
+            try = tr1y;
             break;
         case HALFEQUALRIGHTINVERSE:
-            *bly = bl2y;
-            *try = tr1y;
+            bly = bl2y;
+            try = tr1y;
             break;
         case EQUAL:
-            *bly = bl1y;
-            *try = tr1y;
+            bly = bl1y;
+            try = tr1y;
             break;
         case OUTER:
-            *bly = bl1y;
-            *try = tr1y;
+            bly = bl1y;
+            try = tr1y;
             break;
         case INNER:
-            *bly = bl2y;
-            *try = tr2y;
+            bly = bl2y;
+            try = tr2y;
             break;
         case REGULAR:
-            *bly = bl1y;
-            *try = tr2y;
+            bly = bl1y;
+            try = tr2y;
             break;
         case INVERSE:
-            *bly = bl2y;
-            *try = tr1y;
+            bly = bl2y;
+            try = tr1y;
             break;
-        case NOINTERSECTION:
-            return false;
+        default: // silence warning about not handling NOINTERSECTION, which is handled earlier
             break;
     }
-    return true;
+    shape_t* new = shape_create_rectangle(blx, bly, trx, try);
+    new->layer = rect1->layer;
+    return new;
 }
 
 size_t union_rectangle_all(struct vector* rectangles)
@@ -144,26 +149,14 @@ size_t union_rectangle_all(struct vector* rectangles)
     int j = 1;
     while(1)
     {
-        if(i == vector_size(rectangles) - 1 && j == vector_size(rectangles)) break;
+        //if(i == vector_size(rectangles) - 1 && j == vector_size(rectangles)) break;
+        if(i >= vector_size(rectangles) - 1) break;
         shape_t* rect1 = vector_get(rectangles, i);
         shape_t* rect2 = vector_get(rectangles, j);
-        coordinate_t blx, bly, trx, try;
-        int result = rectangle_union(
-            rect1->points[0]->x,
-            rect1->points[0]->y,
-            rect1->points[1]->x,
-            rect1->points[1]->y,
-            rect2->points[0]->x,
-            rect2->points[0]->y,
-            rect2->points[1]->x,
-            rect2->points[1]->y,
-            &blx, &bly, &trx, &try
-        );
+        shape_t* result = rectangle_union(rect1, rect2);
         if(result)
         {
-            shape_t* new = shape_create_rectangle(blx, bly, trx, try);
-            new->layer = rect1->layer;
-            vector_set(rectangles, i, new);
+            vector_set(rectangles, i, result);
             shape_destroy(rect1);
             vector_remove(rectangles, j, shape_destroy);
             // restart iteration
@@ -174,7 +167,7 @@ size_t union_rectangle_all(struct vector* rectangles)
         {
             ++j;
         }
-        if(j > vector_size(rectangles))
+        if(j > vector_size(rectangles) - 1)
         {
             ++i;
             j = i + 1;
