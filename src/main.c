aa0cc72e0b14048a33382532ef3a90500647bae2
Patrick Kurth <p.kurth@posteo.de>
2022-04-02 13:59:41 +0200

    Added support for options for --read-gds

--- src/main.c
+++ src/main.c
@@ -287,7 +287,6 @@ int main(int argc, const char* const * argv)
     // read gds
     if(cmdoptions_was_provided_long(cmdoptions, "read-gds"))
     {
-        const char* arg = cmdoptions_get_argument_long(cmdoptions, "read-gds");
         lua_State* L = util_create_basic_lua_state();
         open_gdsparser_lib(L);
         open_lfilesystem_lib(L);
@@ -295,8 +294,82 @@ int main(int argc, const char* const * argv)
         _load_module(L, "envlib");
         _load_module(L, "import");
         lua_newtable(L);
-        lua_pushstring(L, arg);
+
+        const char* readgds = cmdoptions_get_argument_long(cmdoptions, "read-gds");
+        lua_pushstring(L, readgds);
         lua_setfield(L, -2, "readgds");
+
+        const char* gdslayermap = cmdoptions_get_argument_long(cmdoptions, "gds-layermap");
+        if(gdslayermap)
+        {
+            lua_pushstring(L, gdslayermap);
+            lua_setfield(L, -2, "gdslayermap");
+        }
+
+        const char* gdsalignmentboxlayer = cmdoptions_get_argument_long(cmdoptions, "gds-alignmentbox-layer");
+        if(gdsalignmentboxlayer)
+        {
+            lua_pushstring(L, gdsalignmentboxlayer);
+            lua_setfield(L, -2, "gdsalignmentboxlayer");
+        }
+
+        const char* gdsalignmentboxpurpose = cmdoptions_get_argument_long(cmdoptions, "gds-alignmentbox-purpose");
+        if(gdsalignmentboxpurpose)
+        {
+            lua_pushstring(L, gdsalignmentboxpurpose);
+            lua_setfield(L, -2, "gdsalignmentboxpurpose");
+        }
+
+        int gdsusestreamlibname = cmdoptions_was_provided_long(cmdoptions, "gds-use-libname");
+        lua_pushboolean(L, gdsusestreamlibname);
+        lua_setfield(L, -2, "gdsusestreamlibname");
+
+        int importoverwrite = cmdoptions_was_provided_long(cmdoptions, "import-overwrite");
+        lua_pushboolean(L, importoverwrite);
+        lua_setfield(L, -2, "importoverwrite");
+
+        const char* importlibname = cmdoptions_get_argument_long(cmdoptions, "gds-alignmentbox-purpose");
+        if(importlibname)
+        {
+            lua_pushstring(L, importlibname);
+            lua_setfield(L, -2, "importlibname");
+        }
+
+        const char* importnamepattern = cmdoptions_get_argument_long(cmdoptions, "import-name-pattern");
+        if(importnamepattern)
+        {
+            lua_pushstring(L, importnamepattern);
+            lua_setfield(L, -2, "importnamepattern");
+        }
+
+        const char* importprefix = cmdoptions_get_argument_long(cmdoptions, "import-prefix");
+        if(importprefix)
+        {
+            lua_pushstring(L, importprefix);
+            lua_setfield(L, -2, "importprefix");
+        }
+
+        const char* importflatpattern = cmdoptions_get_argument_long(cmdoptions, "import-flatten-cell-pattern");
+        if(importflatpattern)
+        {
+            lua_pushstring(L, importflatpattern);
+            lua_setfield(L, -2, "importflatpattern");
+        }
+
+        const char* const * gdsignorelpp = cmdoptions_get_argument_long(cmdoptions, "gds-ignore-lpp");
+        if(gdsignorelpp)
+        {
+            lua_newtable(L);
+            const char* const * ptr = gdsignorelpp;
+            while(*ptr)
+            {
+                lua_pushstring(L, *ptr);
+                lua_rawseti(L, -2, ptr - gdsignorelpp + 1);
+                ++ptr;
+            }
+            lua_setfield(L, -2, "gdsignorelpp");
+        }
+
         lua_setglobal(L, "args");
         call_main_program(L, OPC_HOME "/src/scripts/read_gds.lua");
         lua_close(L);
