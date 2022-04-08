50d75355833e703819f03db12213a23e3d977b6b
Patrick Kurth <p.kurth@posteo.de>
2022-04-08 19:28:48 +0200

    Added 'point' movement to routing

--- src/routing.lua
+++ src/routing.lua
@@ -30,22 +30,31 @@ function M.legalize(nets, rows)
         end
         print()
     end
-    -- call router here
+    -- FIXME: call router here
     return routes
 end
 
 function M.route(cell, routes, cells, width)
     for r, route in ipairs(routes) do
-        if route[1].type ~= "anchor" then
-            moderror(string.format("routing.route: route #%d: first movement needs to be of type 'anchor'", r))
+        if not (route[1].type == "anchor" or route[1].type == "point") then
+            moderror(string.format("routing.route: route #%d: first movement needs to be of type 'anchor' or 'point'", r))
         end
-        local startpt = cells[route[1].name]:get_anchor(route[1].anchor)
+        local startpt
+        if route[1].type == "anchor" then
+            startpt = cells[route[1].name]:get_anchor(route[1].anchor)
+        else
+            startpt = route[1].where
+        end
         local pts = {}
         local currmetal = 1
         local x, y = startpt:unwrap()
         for i = 2, #route do
             local movement = route[i]
-            if movement.type == "anchor" then
+            if movement.type == "point" then
+                local pt = movement.where
+                x, y = pt:unwrap()
+                table.insert(pts, pt)
+            elseif movement.type == "anchor" then
                 local pt = cells[movement.name]:get_anchor(movement.anchor)
                 x, y = pt:unwrap()
                 table.insert(pts, pt)
