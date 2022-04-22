032116caaeb378ce223fb2ca320b77ab224de91a
Patrick Kurth <p.kurth@posteo.de>
2022-04-22 09:55:35 +0200

    Improved re-use of common code in lplacer

--- src/lplacer_common.c
+++ src/lplacer_common.c
@@ -1,3 +1,8 @@
+#include "lplacer_common.h"
+
+#include <stdint.h>
+#include <stdlib.h>
+
 uint64_t factorial(uint64_t num)
 {
     if(num == 0)
@@ -64,5 +69,102 @@ int next_permutation(unsigned int* array, size_t len)
         }
     }
     return 1;
+}
+
+void placer_initialize_base_cell(lua_State* L, struct basic_cell* base, size_t index, struct keyvaluearray* netmap)
+{
+    // instance
+    base->instance = index;
+
+    // width
+    lua_getfield(L, -1, "width");
+    base->width = lua_tointeger(L, -1);
+    lua_pop(L, 1);
+
+    // nets
+    lua_getfield(L, -1, "nets");
+    lua_len(L, -1);
+    size_t num_conns = lua_tointeger(L, -1);
+    lua_pop(L, 1);
+    base->nets = calloc(num_conns, sizeof(*base->nets));
+    base->pinoffset = calloc(num_conns, sizeof(*base->pinoffset));
+    base->num_conns = num_conns;
+    for(size_t j = 1; j <= num_conns; ++j)
+    {
+        lua_geti(L, -1, j); // get net
+
+        lua_getfield(L, -1, "name");
+        const char* name = lua_tostring(L, -1);
+        base->nets[j - 1] = keyvaluearray_get(netmap, name);
+        lua_pop(L, 1); // pop name
+
+        lua_getfield(L, -3, "pinoffsets");
+        lua_getfield(L, -2, "port");
+        lua_gettable(L, -2);
+        int pinoffset = lua_tointeger(L, -1);
+        base->pinoffset[j - 1] = pinoffset;
+        lua_pop(L, 2); // pop pinoffset + pinoffsets table
+                       //
+        lua_pop(L, 1); // pop net
+    }
+}
+
+void placer_destroy_base_cell_contents(struct basic_cell* base)
+{
+    free(base->nets);
+    free(base->pinoffset);
+}
+
+struct floorplan* placer_create_floorplan(lua_State* L)
+{
+    lua_getfield(L, 3, "floorplan_width");
+    unsigned int floorplan_width = lua_tointeger(L, -1);
+    lua_pop(L, 1);
+    lua_getfield(L, 3, "floorplan_height");
+    unsigned int floorplan_height = lua_tointeger(L, -1);
+    lua_pop(L, 1);
+    lua_getfield(L, 3, "desired_row_width");
+    unsigned int desired_row_width = lua_tointeger(L, -1);
+    lua_pop(L, 1);
+
+    struct floorplan* floorplan = malloc(sizeof(struct floorplan));
+    floorplan->floorplan_width = floorplan_width;
+    floorplan->floorplan_height = floorplan_height;
+    floorplan->desired_row_width = desired_row_width;
+
+    return floorplan;
+}
+
+void placer_destroy_floorplan(struct floorplan* floorplan)
+{
+    free(floorplan);
+}
+
+void placer_create_lua_result(lua_State* L, void* block, row_access_func_t row_access_func, struct floorplan* floorplan)
+{
+    lua_newtable(L);
+    for(unsigned int cur_row = 0; cur_row < floorplan->floorplan_height; cur_row++)
+    {
+        int* row_instance_indices = row_access_func(block, cur_row);
+        lua_newtable(L);
+        int i = 1;
+        for(int* idx = row_instance_indices; *idx; ++idx)
+        {
+            lua_rawgeti(L, 1, *idx);
+
+            lua_newtable(L);
+            lua_pushstring(L, "reference");
+            lua_getfield(L, -3, "reference");
+            lua_settable(L, -3);
+            lua_pushstring(L, "instance");
+            lua_getfield(L, 1, "instance");
+            lua_settable(L, -3);
+            lua_pop(L, 1);
+            lua_seti(L, -2, i);
+            ++i;
+        }
+        lua_seti(L, -2, cur_row + 1);
+        free(row_instance_indices);
+    }
 }
 
