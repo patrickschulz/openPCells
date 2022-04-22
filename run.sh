0af9d2e5e491ef532b7455447fac7d51bf6914f5
Philipp Nickel <philipp.nickel@campus.tu-berlin.de>
2022-04-22 19:42:58 +0200

    fixed run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-opc --import-verilog examples/place_and_route/place_route_generate.lua
-opc --technology opc --export gds --cell verilogimport/counter --cellpath verilogimport
+./opc --import-verilog examples/place_and_route/place_route_generate.lua
+./opc --technology opc --export gds --cell verilogimport/counter --cellpath verilogimport
