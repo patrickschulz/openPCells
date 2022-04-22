e601affc7e298a2c8afff6ccb5053a1a4fd90112
Patrick Kurth <p.kurth@posteo.de>
2022-04-22 15:29:16 +0200

    Added flexible routing width

--- src/modules/generator.lua
+++ src/modules/generator.lua
@@ -2,7 +2,10 @@ local M = {}
 
 local function _write_module(rows, routes)
     local lines = {}
-    table.insert(lines, "function layout(toplevel)")
+    table.insert(lines, 'function parameters()')
+    table.insert(lines, '    pcell.reference_cell("stdcells/base")')
+    table.insert(lines, 'end')
+    table.insert(lines, 'function layout(toplevel)')
 
     -- placement
     if rows then
@@ -39,7 +42,8 @@ local function _write_module(rows, routes)
           table.insert(lines, '        },')
         end
         table.insert(lines, '    }')
-        table.insert(lines, string.format('    local width = %d', 100))
+        table.insert(lines, '    local bp = pcell.get_parameters("stdcells/base")')
+        table.insert(lines, '    local width = bp.gstwidth')
         table.insert(lines, string.format('    routing.route(toplevel, routes, cells, width)'))
     else
         print("no routing information found")
