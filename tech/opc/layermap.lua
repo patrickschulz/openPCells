ed75c1f6f806f53ea121de5f3afce01c46756189
Patrick Kurth <p.kurth@posteo.de>
2022-04-25 12:09:57 +0200

    Improved opc technology

--- tech/opc/layermap.lua
+++ tech/opc/layermap.lua
@@ -1,207 +1,219 @@
 return {
-    special = { 
+    special = {
         name = "_special",
         layer = {
-            gds = { layer = 0, purpose = 0 }, 
-            SKILL = { layer = "_special", purpose = "drawing" }
+            gds = { layer = 0, purpose = 0 },
+            SKILL = { layer = "_special", purpose = "drawing" },
         }
     },
-    active = { 
+    active = {
         name = "active",
         layer = {
-            gds = { layer = 1, purpose = 0 }, 
-            SKILL = { layer = "active", purpose = "drawing" }
+            gds = { layer = 1, purpose = 0 },
+            SKILL = { layer = "active", purpose = "drawing" },
+            tikz = { color = "green" },
         }
     },
-    pwell = { 
+    pwell = {
         name = "pwell",
         layer = {
-            gds = { layer = 2, purpose = 0 }, 
-            SKILL = { layer = "pwell", purpose = "drawing" }
+            gds = { layer = 2, purpose = 0 },
+            SKILL = { layer = "pwell", purpose = "drawing" },
+            tikz = { color = "black", nofill = true },
         }
     },
-    nwell = { 
+    nwell = {
         name = "nwell",
         layer = {
-            gds = { layer = 3, purpose = 0 }, 
-            SKILL = { layer = "nwell", purpose = "drawing" }
+            gds = { layer = 3, purpose = 0 },
+            SKILL = { layer = "nwell", purpose = "drawing" },
+            tikz = { color = "black", nofill = true },
         }
     },
-    pimplant = { 
+    pimplant = {
         name = "pimplant",
         layer = {
-            gds = { layer = 4, purpose = 0 }, 
-            SKILL = { layer = "pimplant", purpose = "drawing" }
+            gds = { layer = 4, purpose = 0 },
+            SKILL = { layer = "pimplant", purpose = "drawing" },
+            tikz = { color = "black", nofill = true },
         }
     },
-    nimplant = { 
+    nimplant = {
         name = "nimplant",
         layer = {
-            gds = { layer = 5, purpose = 0 }, 
-            SKILL = { layer = "nimplant", purpose = "drawing" }
+            gds = { layer = 5, purpose = 0 },
+            SKILL = { layer = "nimplant", purpose = "drawing" },
+            tikz = { color = "black", nofill = true },
         }
     },
-    gate = { 
+    gate = {
         name = "poly",
         layer = {
-            gds = { layer = 6, purpose = 0 }, 
-            SKILL = { layer = "poly", purpose = "drawing" }
+            gds = { layer = 6, purpose = 0 },
+            SKILL = { layer = "poly", purpose = "drawing" },
+            tikz = { color = "red" },
         }
     },
-    contactactive = { 
+    contactactive = {
         name = "contactactive",
         layer = {
-            gds = { layer = 7, purpose = 0 }, 
-            SKILL = { layer = "contactactive", purpose = "drawing" }
+            gds = { layer = 7, purpose = 0 },
+            SKILL = { layer = "contactactive", purpose = "drawing" },
+            tikz = { color = "yellow" },
         }
     },
-    contactsourcedrain = { 
+    contactsourcedrain = {
         name = "contactsourcedrain",
         layer = {
-            gds = { layer = 7, purpose = 0 }, 
-            SKILL = { layer = "contactsourcedrain", purpose = "drawing" }
+            gds = { layer = 7, purpose = 0 },
+            SKILL = { layer = "contactsourcedrain", purpose = "drawing" },
+            tikz = { color = "yellow" },
         }
     },
-    contactgate = { 
+    contactgate = {
         name = "contactgate",
         layer = {
-            gds = { layer = 7, purpose = 0 }, 
-            SKILL = { layer = "contactgate", purpose = "drawing" }
+            gds = { layer = 7, purpose = 0 },
+            SKILL = { layer = "contactgate", purpose = "drawing" },
+            tikz = { color = "yellow" },
         }
     },
-    M1 = { 
+    M1 = {
         name = "metal1",
         layer = {
-            gds = { layer = 8, purpose = 0 }, 
+            gds = { layer = 8, purpose = 0 },
             SKILL = { layer = "metal1", purpose = "drawing" },
-            svg = { color = "blue" }
+            tikz = { color = "blue" },
         }
     },
-    viacutM1M2 = { 
+    viacutM1M2 = {
         name = "via1",
         layer = {
-            gds = { layer = 9, purpose = 0 }, 
+            gds = { layer = 9, purpose = 0 },
             SKILL = { layer = "via1", purpose = "drawing" },
-            svg = { color = "yellow" }
+            tikz = { color = "yellow" },
         }
     },
-    M2 = { 
+    M2 = {
         name = "metal2",
         layer = {
-            gds = { layer = 10, purpose = 0 }, 
+            gds = { layer = 10, purpose = 0 },
             SKILL = { layer = "metal2", purpose = "drawing" },
-            svg = { color = "pink" }
+            tikz = { color = "pink" },
         }
     },
-    viacutM2M3 = { 
+    viacutM2M3 = {
         name = "via2",
         layer = {
-            gds = { layer = 11, purpose = 0 }, 
-            SKILL = { layer = "via2", purpose = "drawing" }
+            gds = { layer = 11, purpose = 0 },
+            SKILL = { layer = "via2", purpose = "drawing" },
+            tikz = { color = "yellow" },
         }
     },
-    M3 = { 
+    M3 = {
         name = "metal3",
         layer = {
-            gds = { layer = 12, purpose = 0 }, 
-            SKILL = { layer = "metal3", purpose = "drawing" }
+            gds = { layer = 12, purpose = 0 },
+            SKILL = { layer = "metal3", purpose = "drawing" },
+            tikz = { color = "orange" },
         }
     },
-    viacutM3M4 = { 
+    viacutM3M4 = {
         name = "via3",
         layer = {
-            gds = { layer = 13, purpose = 0 }, 
-            SKILL = { layer = "via3", purpose = "drawing" }
+            gds = { layer = 13, purpose = 0 },
+            SKILL = { layer = "via3", purpose = "drawing" },
+            tikz = { color = "white" },
         }
     },
-    M4 = { 
+    M4 = {
         name = "metal4",
         layer = {
-            gds = { layer = 14, purpose = 0 }, 
+            gds = { layer = 14, purpose = 0 },
             SKILL = { layer = "metal4", purpose = "drawing" }
         }
     },
-    viacutM4M5 = { 
+    viacutM4M5 = {
         name = "via4",
         layer = {
-            gds = { layer = 15, purpose = 0 }, 
+            gds = { layer = 15, purpose = 0 },
             SKILL = { layer = "via4", purpose = "drawing" }
         }
     },
-    M5 = { 
+    M5 = {
         name = "metal5",
         layer = {
-            gds = { layer = 16, purpose = 0 }, 
+            gds = { layer = 16, purpose = 0 },
             SKILL = { layer = "metal5", purpose = "drawing" }
         }
     },
-    viacutM5M6 = { 
+    viacutM5M6 = {
         name = "via5",
         layer = {
-            gds = { layer = 17, purpose = 0 }, 
+            gds = { layer = 17, purpose = 0 },
             SKILL = { layer = "via5", purpose = "drawing" }
         }
     },
-    M6 = { 
+    M6 = {
         name = "metal6",
         layer = {
-            gds = { layer = 18, purpose = 0 }, 
+            gds = { layer = 18, purpose = 0 },
             SKILL = { layer = "metal6", purpose = "drawing" }
         }
     },
-    viacutM6M7 = { 
+    viacutM6M7 = {
         name = "via6",
         layer = {
-            gds = { layer = 19, purpose = 0 }, 
+            gds = { layer = 19, purpose = 0 },
             SKILL = { layer = "via6", purpose = "drawing" }
         }
     },
-    M7 = { 
+    M7 = {
         name = "metal7",
         layer = {
-            gds = { layer = 20, purpose = 0 }, 
+            gds = { layer = 20, purpose = 0 },
             SKILL = { layer = "metal7", purpose = "drawing" }
         }
     },
-    viacutM7M8 = { 
+    viacutM7M8 = {
         name = "via7",
         layer = {
-            gds = { layer = 21, purpose = 0 }, 
+            gds = { layer = 21, purpose = 0 },
             SKILL = { layer = "via7", purpose = "drawing" }
         }
     },
-    M8 = { 
+    M8 = {
         name = "metal8",
         layer = {
-            gds = { layer = 22, purpose = 0 }, 
+            gds = { layer = 22, purpose = 0 },
             SKILL = { layer = "metal8", purpose = "drawing" }
         }
     },
-    viacutM8M9 = { 
+    viacutM8M9 = {
         name = "via8",
         layer = {
-            gds = { layer = 23, purpose = 0 }, 
+            gds = { layer = 23, purpose = 0 },
             SKILL = { layer = "via8", purpose = "drawing" }
         }
     },
-    M9 = { 
+    M9 = {
         name = "metal9",
         layer = {
-            gds = { layer = 24, purpose = 0 }, 
+            gds = { layer = 24, purpose = 0 },
             SKILL = { layer = "metal9", purpose = "drawing" }
         }
     },
-    viacutM9M10 = { 
+    viacutM9M10 = {
         name = "via9",
         layer = {
-            gds = { layer = 25, purpose = 0 }, 
+            gds = { layer = 25, purpose = 0 },
             SKILL = { layer = "via9", purpose = "drawing" }
         }
     },
-    M10 = { 
+    M10 = {
         name = "metal10",
         layer = {
-            gds = { layer = 26, purpose = 0 }, 
+            gds = { layer = 26, purpose = 0 },
             SKILL = { layer = "metal10", purpose = "drawing" }
         }
     },
