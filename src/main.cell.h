2772a91d3ae20c1a807f2307f584ebede80c3d40
Patrick Kurth <p.kurth@posteo.de>
2022-04-26 07:41:40 +0200

    Added '-' as possible filename

--- src/main.cell.h
+++ src/main.cell.h
@@ -5,6 +5,6 @@
 #include "cmdoptions.h"
 
 void main_list_cell_parameters(struct cmdoptions* cmdoptions, struct keyvaluearray* config);
-void main_create_and_export_cell(struct cmdoptions* cmdoptions, struct keyvaluearray* config, int iscellscript);
+int main_create_and_export_cell(struct cmdoptions* cmdoptions, struct keyvaluearray* config, int iscellscript);
 
 #endif // OPC_MAIN_CELL_H
