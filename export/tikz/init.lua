ee09c46533042785278289d95090a0af0840be88
Patrick Kurth <p.kurth@posteo.de>
2022-04-26 07:49:10 +0200

    updated tikz export

--- export/tikz/init.lua
+++ export/tikz/init.lua
@@ -2,22 +2,27 @@ local M = {}
 
 local baseunit = 100
 
-local __standalone
-
 function M.get_extension()
     return "tikz"
 end
 
+local __standalone
+local __drawpatterns = true
+
 function M.set_options(opt)
     for i = 1, #opt do
         local arg = opt[i]
-        if arg == "-s" or arg == "--standalone" then
+        if arg == "-S" or arg == "--standalone" then
             __standalone = true
+        end
+        if arg == "-s" or arg == "--solid" then
+            __drawpatterns = false
         end
     end
 end
 
 local __before = {}
+local __options = {}
 local __after = {}
 local __content = {}
 
@@ -26,14 +31,25 @@ function M.finalize()
     for _, entry in ipairs(__before) do
         table.insert(t, entry)
     end
+    if #__options > 0 then
+        table.insert(t, "[")
+        for i, entry in ipairs(__options) do
+            if i ~= #__options then
+                table.insert(t, string.format("    %s,", entry))
+            else
+                table.insert(t, string.format("    %s", entry))
+            end
+        end
+        table.insert(t, "]")
+    end
     table.sort(__content, function(lhs, rhs) return lhs.order < rhs.order end)
     for _, entry in ipairs(__content) do
-        table.insert(t, entry.content)
+        table.insert(t, string.format("    %s", entry.content))
     end
     for _, entry in ipairs(__after) do
         table.insert(t, entry)
     end
-    return table.concat(t, "\n")
+    return table.concat(t, "\n") .. "\n"
 end
 
 function M.at_begin()
@@ -43,7 +59,8 @@ function M.at_begin()
         table.insert(__before, '\\usetikzlibrary{patterns}')
         table.insert(__before, '\\begin{document}')
     end
-    table.insert(__before, '\\begin{tikzpicture}[x = 5, y = 5]')
+    table.insert(__before, '\\begin{tikzpicture}')
+    table.insert(__options, "x = 5, y = 5")
 end
 
 function M.at_end()
@@ -82,33 +99,46 @@ local function _format_point(pt, baseunit, sep)
     return string.format("%s%s%s", sx, sep, sy)
 end
 
-local function _format_layer(layer)
-    if false and layer.order then
+local function _get_layer_style(layer)
+    if layer.nofill then
+        return string.format("draw = %s", layer.color)
     else
-        if layer.nofill then
-            return string.format("\\path[draw = %s]", layer.color)
+        if layer.pattern then
+            return string.format("draw = %s, pattern = crosshatch, pattern color = %s", layer.color, layer.color)
         else
-            if layer.pattern then
-                return string.format("\\path[draw = %s, pattern = crosshatch, pattern color = %s]", layer.color, layer.color)
-            else
-                return string.format("\\path[fill = %s]", layer.color, layer.color)
-            end
+            return string.format("fill = %s", layer.color, layer.color)
         end
     end
 end
 
+local styles = {}
+local function _format_layer(layer)
+    if layer.style then
+        if not styles[layer.style] then
+            styles[layer.style] = true
+            table.insert(__options, string.format("%s/.style = { %s }", layer.style, _get_layer_style(layer)))
+        end
+        return string.format("\\path[%s]", layer.style)
+    end
+    return string.format("\\path[%s]", _get_layer_style(layer))
+end
+
 function M.write_rectangle(layer, bl, tr)
-    --_insert_appearance(layer)
-    table.insert(__content, { order = layer.order or 0, content = string.format("%s (%s) rectangle (%s);", _format_layer(layer), _format_point(bl, baseunit, ", "), _format_point(tr, baseunit, ", ")) })
+    table.insert(__content, {
+        order = layer.order or 0,
+        content = string.format("%s (%s) rectangle (%s);", _format_layer(layer), _format_point(bl, baseunit, ", "), _format_point(tr, baseunit, ", "))
+    })
 end
 
 function M.write_polygon(layer, pts)
-    --_insert_appearance(layer)
-    --local ptstream = {}
-    --for _, pt in ipairs(pts) do
-    --    table.insert(ptstream, string.format("(%s)", _format_point(pt, baseunit, ", ")))
-    --end
-    --table.insert(__content, table.concat(ptstream, " -- ") .. ";")
+    local ptstream = {}
+    for _, pt in ipairs(pts) do
+        table.insert(ptstream, string.format("(%s)", _format_point(pt, baseunit, ", ")))
+    end
+    table.insert(__content, {
+        order = layer.order or 0,
+        content = string.format("%s %s;", _format_layer(layer), table.concat(ptstream, " -- "))
+    })
 end
 
 return M
