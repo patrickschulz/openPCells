e30fa9cfc005094d605a929d29bcc4ca14d0cd33
Patrick Kurth <p.kurth@posteo.de>
2022-05-09 22:33:54 +0200

    Fixed vias in transmission line

--- cells/RF/transmission_line.lua
+++ cells/RF/transmission_line.lua
@@ -15,5 +15,5 @@ function layout(tline, _P)
     geometry.rectangle(tline, generics.metal(_P.metalnum), _P.length, _P.width)
     geometry.rectangle(tline, generics.metal(_P.metalnum), _P.length, _P.groundwidth, 0, 0, 1, 2, 0, _P.width + 2 * _P.groundspace + _P.groundwidth)
     geometry.rectangle(tline, generics.metal(_P.shieldmetal), _P.shieldwidth, _P.width + 2 * _P.groundspace + 2 * _P.groundwidth, 0, 0, _P.length // (_P.shieldwidth + _P.shieldspace), 1, _P.shieldwidth + _P.shieldspace, 0)
-    geometry.via(tline, _P.shieldmetal, _P.shieldmetal + 1, _P.length, _P.groundwidth, 0, 0, 1, 2, 0, _P.width + 2 * _P.groundwidth)
+    geometry.via(tline, _P.shieldmetal, _P.metalnum, _P.length, _P.groundwidth, 0, 0, 1, 2, 0, _P.width + 2 * _P.groundwidth)
 end
