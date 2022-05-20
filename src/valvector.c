a7f21be46c271e415ce567cd43267c373c5661ba
Patrick Kurth <p.kurth@posteo.de>
2022-05-20 11:00:23 +0200

    Improved code readability

--- src/valvector.c
+++ src/valvector.c
@@ -13,10 +13,22 @@
 #define VECTOR_CAPACITY_OFFSET 1
 #define VECTOR_SIZE_OFFSET 2
 #define VECTOR_BOOKKEEPING_SIZE 3
+
+#define _head(data) (data - VECTOR_BOOKKEEPING_SIZE * sizeof(size_t))
+#define _elem_size(data) ((size_t*) (data + (-VECTOR_BOOKKEEPING_SIZE + VECTOR_ELEMENT_SIZE_OFFSET) * sizeof(size_t)))
+#define _capacity(data) ((size_t*) (data + (-VECTOR_BOOKKEEPING_SIZE + VECTOR_CAPACITY_OFFSET) * sizeof(size_t)))
+#define _size(data) ((size_t*) (data + (-VECTOR_BOOKKEEPING_SIZE + VECTOR_SIZE_OFFSET) * sizeof(size_t)))
+
+void* _create(size_t capacity, size_t elem_size)
+{
+    void* data = malloc(VECTOR_BOOKKEEPING_SIZE * sizeof(size_t) + capacity * elem_size);
+    return data;
+}
+
 void* valvector_create(size_t elem_size);
 {
     size_t capacity = 0;
-    void* data = malloc(VECTOR_BOOKKEEPING_SIZE * sizeof(size_t) + capacity * elem_size);
+    void* data = _create(capacity, elem_size);
     memset(data, 0, VECTOR_BOOKKEEPING_SIZE * sizeof(size_t));
     size_t* elem_size_ptr = data + VECTOR_ELEMENT_SIZE_OFFSET * sizeof(size_t);
     *elem_size_ptr = elem_size;
@@ -30,17 +42,16 @@ void* valvector_create(size_t elem_size);
 void valvector_destroy(void* vp)
 {
     void* data = *(void**)vp;
-    void* v = data - VECTOR_BOOKKEEPING_SIZE * sizeof(size_t);
+    void* v = _head(data);
     free(v);
 }
 
 void valvector_append(void* vp, void* e)
 {
     void* data = *(void**)vp;
-    size_t* elem_size_ptr = data + (-VECTOR_BOOKKEEPING_SIZE + VECTOR_ELEMENT_SIZE_OFFSET) * sizeof(size_t);
-    size_t elem_size = *elem_size_ptr;
-    size_t* capacity_ptr = data + (-VECTOR_BOOKKEEPING_SIZE + VECTOR_CAPACITY_OFFSET) * sizeof(size_t);
-    size_t* size_ptr = data + (-VECTOR_BOOKKEEPING_SIZE + VECTOR_SIZE_OFFSET) * sizeof(size_t);
+    size_t elem_size = *_elem_size(data);
+    size_t* capacity_ptr = _capacity(data);
+    size_t* size_ptr = _size(data);
     if(*size_ptr == *capacity_ptr)
     {
         size_t capacity = *capacity_ptr > 0 ? (2 * *capacity_ptr) : 1;
