678ee8643566626c7798aecb60e6af56fe4474db
Patrick Kurth <p.kurth@posteo.de>
2022-06-16 15:53:09 +0200

    Updated excluded nets in place&route script

--- examples/place_and_route/place_route_generate.lua
+++ examples/place_and_route/place_route_generate.lua
@@ -1,23 +1,23 @@
-local module = "inverter_chain"
+local module = "register_cell"
 local exporttype = "gds"
 
 local netlist = verilog.read_parse_file(string.format("%s.v", module))
 
-verilog.filter_excluded_nets(netlist, { "clk", "_mem.clk", "vdd", "vss", "in", "out", })
+verilog.filter_excluded_nets(netlist, { "reset", "clk", "update" })
 
 local cellinfo = verilogprocessor.read_cellinfo_from_file("cellinfo.lua")
 local instances, nets = verilogprocessor.collect_nets_cells(netlist, cellinfo)
 
 local utilization = 0.8
-local numrows = 2
+local numrows = 3
 local floorplan = placement.create_floorplan_fixed_rows(instances, utilization, numrows)
-local rows = placement.optimize(instances, nets, floorplan)
---local plan = {
---    { "inv", "nand1", "dff_out" },
---    { "nand2", "dff_buf" },
---    { "nand3", "dff_in" },
---}
---local rows = placement.manual(instances, plan)
+--local rows = placement.optimize(instances, nets, floorplan)
+local plan = {
+    { "inv", "nand1", "dff_out" },
+    { "nand2", "dff_buf" },
+    { "nand3", "dff_in" },
+}
+local rows = placement.manual(instances, plan)
 placement.insert_filler_names(rows, floorplan.floorplan_width)
 
 -- this value can be (at least in theory) changed in the generated layout, but the router assumes this many tracks
