5831576dfed2e98d15369ad8bafd6146ad6432c8
Patrick Kurth <p.kurth@posteo.de>
2022-07-10 22:08:37 +0200

    Improved route()

--- src/lrouter_route.c
+++ src/lrouter_route.c
@@ -1,13 +1,11 @@
+#include "lrouter_route.h"
+
 #include "lrouter_net.h"
 #include "lrouter_queue.h"
 #include "lrouter_min_heap.h"
 #include "lrouter_field.h"
-#include "lrouter_route.h"
 
-#include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include <limits.h>
 
 #define EVEN(val) ((val % 2) == 0)
@@ -26,6 +24,38 @@ static int _next_field_position(int i, const struct rpoint* current, struct rpoi
     return 1;
 }
 
+static int _get_cost_increment(struct rpoint current, struct rpoint next, int step_cost, int wrong_dir_cost, int via_cost)
+{
+    if(next.z != current.z) /* via */
+    {
+        return via_cost;
+    }
+    else if(next.y != current.y && EVEN(current.z)) /* wrong y direction */
+    {
+        return wrong_dir_cost;
+    }
+    else if (next.x != current.x && !EVEN(current.z)) /* wrong x direction */
+    {
+        return wrong_dir_cost;
+    }
+    return step_cost;
+}
+
+static int _is_unvisited(int nextfield)
+{
+    return nextfield == UNVISITED;
+}
+
+static int _is_final_port(int nextfield, struct rpoint next, const struct position* endpos)
+{
+    return nextfield == PORT && next.x == endpos->x && next.y == endpos->y && next.z == endpos->z;
+}
+
+static int _is_smaller_score(int score, int score_incr, int nextfield)
+{
+    return (score + score_incr) < nextfield;
+}
+
 void route(struct net *net, struct field* field, int step_cost, int wrong_dir_cost, int via_cost)
 {
     const struct position* startpos = net_get_startpos(net);
@@ -35,16 +65,13 @@ void route(struct net *net, struct field* field, int step_cost, int wrong_dir_co
     struct minheap* min_heap = heap_init();
     heap_insert_point(min_heap, startpos->x, startpos->y, startpos->z, 0);
 
-    struct rpoint current = { .x = 0, .y = 0, .z = 0 };
+    struct rpoint current = { .x = startpos->x, .y = startpos->y, .z = startpos->z };
 
     field_set(field, startpos->x, startpos->y, startpos->z, 0);
 
-    /*
-     * do as long as there are points
-     * to be marked or
-     * endpoint is reached
-     */
-    do {
+    /* do as long as there are points to be marked or endpoint is reached */
+    while(!(current.x == endpos->x && current.y == endpos->y && current.z == endpos->z))
+    {
         /* get next point from heap */
         struct rpoint* point_ptr = heap_get_point(min_heap);
 
@@ -70,47 +97,17 @@ void route(struct net *net, struct field* field, int step_cost, int wrong_dir_co
                 continue;
             }
 
-            /* decide the val of the score incrementer */
-            int score_incr = step_cost;
-            if(next.z != current.z) /* via */
-            {
-                score_incr = via_cost;
-            }
-            else if(next.y != current.y && EVEN(current.z)) /* wrong y direction */
-            {
-                score_incr = wrong_dir_cost;
-            }
-            else if (next.x != current.x && !EVEN(current.z)) /* wrong x direction */
-            {
-                score_incr = wrong_dir_cost;
-            }
+            int score_incr = _get_cost_increment(current, next, step_cost, wrong_dir_cost, via_cost);
 
             /* check if point is visitable */
             int nextfield = field_get(field, next.x, next.y, next.z);
-            if((nextfield == PORT &&
-                next.x == endpos->x &&
-                next.y == endpos->y &&
-                next.z == endpos->z) ||
-               nextfield == UNVISITED ||
-               (score + score_incr < nextfield))
+            if(
+                _is_unvisited(nextfield) ||
+                _is_final_port(nextfield, next, endpos) ||
+                _is_smaller_score(score, score_incr, nextfield)
+            )
             {
-                if(next.x == endpos->x && next.y == endpos->y && next.z == endpos->z)
-                {
-                    /*
-                     * if next point is endpoint
-                     * put it into front of heap
-                     * so empty the heap (not nice way)
-                     */
-                    struct rpoint* pt;
-                    while((pt = heap_get_point(min_heap)))
-                    {
-                        free(pt);
-                    }
-                }
-
                 field_set(field, next.x, next.y, next.z, score + score_incr);
-
-                /* put the point in the to be visited queue */
                 heap_insert_point(min_heap, next.x, next.y, next.z, score + score_incr);
             }
         }
@@ -123,8 +120,7 @@ void route(struct net *net, struct field* field, int step_cost, int wrong_dir_co
             heap_destroy(min_heap);
             return;
         }
-    } while(!(current.x == endpos->x && current.y == endpos->y && current.z == endpos->z));
-
+    }
 
     heap_destroy(min_heap);
 
@@ -133,7 +129,8 @@ void route(struct net *net, struct field* field, int step_cost, int wrong_dir_co
     int zdiff = 0;
 
     /* backtrace */
-    do {
+    while(!(current.x == startpos->x && current.y == startpos->y && current.z == startpos->z))
+    {
         int score = field_get(field, current.x, current.y, current.z);
 
         struct rpoint nextpoint = { .x = INT_MAX, .y = INT_MAX, .z = INT_MAX, .score = INT_MAX };
@@ -158,10 +155,11 @@ void route(struct net *net, struct field* field, int step_cost, int wrong_dir_co
                 continue;
             }
 
+            int is_wrong_dir = (yincr[i] && EVEN(current.z)) || (xincr[i] && !EVEN(current.z));
             int is_reachable =
                 ((score - nextfield) == via_cost) ||
-                ((score - nextfield) == wrong_dir_cost) ||
-                ((score - nextfield) == step_cost);
+                (((score - nextfield) == wrong_dir_cost) && is_wrong_dir) ||
+                (((score - nextfield) == step_cost) && !is_wrong_dir);
 
             if(is_reachable && nextfield < score && nextfield < nextpoint.score)
             {
@@ -189,8 +187,7 @@ void route(struct net *net, struct field* field, int step_cost, int wrong_dir_co
         current.x = nextpoint.x;
         current.y = nextpoint.y;
         current.z = nextpoint.z;
-
-    } while (!(current.x == startpos->x && current.y == startpos->y && current.z == startpos->z));
+    }
 
     net_reverse_points(net);
 
