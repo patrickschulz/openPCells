fb6f711c801a6ac2209c0a57dc5f6ba92bed3426
philippnickel <philipp.nickel@campus.tu-berlin.de>
2022-08-10 11:57:02 +0200

    Worked on placer

--- examples/place_and_route/place_route_generate.lua
+++ examples/place_and_route/place_route_generate.lua
@@ -1,4 +1,4 @@
-local module = "register_cell"
+local module = "serial_ctrl"
 local exporttype = "gds"
 
 local netlist = verilog.read_parse_file(string.format("%s.v", module))
@@ -17,8 +17,8 @@ local ignorednets = {
 }
 local instances, nets = verilogprocessor.collect_nets_cells(netlist, cellinfo, ignorednets)
 
-local utilization = 0.8
-local numrows = 2
+local utilization = 0.5
+local numrows = 10
 local floorplan = placement.create_floorplan_fixed_rows(instances, utilization, numrows)
 local rows = placement.optimize(instances, nets, floorplan)
 --local plan = {
