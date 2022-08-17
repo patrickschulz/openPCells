38f3e980a2a0f0acb8df8aab80b10268c3056122
Patrick Kurth <p.kurth@posteo.de>
2022-08-17 16:43:25 +0200

    Fixed some issues in stdcells

--- cells/stdcells/and_gate.lua
+++ cells/stdcells/and_gate.lua
@@ -2,11 +2,19 @@ function parameters()
     pcell.reference_cell("stdcells/base")
     pcell.add_parameters(
         { "nandfingers", 1 },
-        { "notfingers", 1 }
+        { "notfingers", 1 },
+        { "pwidth", 2 * tech.get_dimension("Minimum Gate Width") },
+        { "nwidth", 2 * tech.get_dimension("Minimum Gate Width") }
     )
 end
 
 function layout(gate, _P)
-    local subgate = pcell.create_layout("stdcells/1_inv_gate", { subgate = "nand_gate", subgatefingers = _P.nandfingers, notfingers = _P.notfingers })
+    local subgate = pcell.create_layout("stdcells/1_inv_gate", {
+        subgate = "nand_gate",
+        subgatefingers = _P.nandfingers,
+        notfingers = _P.notfingers,
+        pwidth = _P.pwidth,
+        nwidth = _P.nwidth
+    })
     gate:exchange(subgate)
 end
