bd191a016e49b6127b673675fceae9cfe2f943ac
Patrick Kurth <p.kurth@posteo.de>
2022-08-17 23:52:06 +0200

    Added more dff varieties

---
+++ cells/stdcells/dffnrq.lua
@@ -0,0 +1,10 @@
+function layout(gate, _P)
+    local dff = pcell.create_layout("stdcells/dff", {
+        clockpolarity = "negative",
+        enable_Q = true,
+        enable_QN = false,
+        enable_set = false,
+        enable_reset = true,
+    })
+    gate:exchange(dff)
+end
