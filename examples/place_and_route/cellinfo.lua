9b6ebcc023e367ff09a514ec28ce6d91846a82a4
Philipp Nickel <philipp.nickel@campus.tu-berlin.de>
2022-08-18 15:54:49 +0200

    chose floor cellinfo

--- examples/place_and_route/cellinfo.lua
+++ examples/place_and_route/cellinfo.lua
@@ -1,122 +1,101 @@
 return {
+    dffpq = {
+        width = 21,
+        pinoffsets = {
+            CLK = { x = 0, y = -1 },
+            D = { x = 0, y = 1 },
+            Q = { x = 21, y = 0 },
+        },
+    },
+    dffnq = {
+        width = 21,
+        pinoffsets = {
+            CLK = { x = 0, y = -1 },
+            D = { x = 0, y = 1 },
+            Q = { x = 21, y = 0 },
+        },
+    },
+    dffprq = {
+        width = 24,
+        pinoffsets = {
+            CLK = { x = 0, y = -1 },
+            D = { x = 0, y = 1 },
+            Q = { x = 24, y = 0 },
+            RST = { x = 15, y = 0 },
+        },
+    },
+    dffnrq = {
+        width = 24,
+        pinoffsets = {
+            CLK = { x = 0, y = -1 },
+            D = { x = 0, y = 1 },
+            Q = { x = 24, y = 0 },
+            RST = { x = 15, y = 0 },
+        },
+    },
+    dffpsq = {
+        width = 25,
+        pinoffsets = {
+            CLK = { x = 0, y = -1 },
+            D = { x = 0, y = 1 },
+            Q = { x = 25, y = 0 },
+            SET = { x = 16, y = 0 },
+        },
+    },
+    dffnsq = {
+        width = 25,
+        pinoffsets = {
+            CLK = { x = 0, y = -1 },
+            D = { x = 0, y = 1 },
+            Q = { x = 25, y = 0 },
+            SET = { x = 16, y = 0 },
+        },
+    },
     not_gate = {
         width = 2,
         pinoffsets = {
             I = { x = 0, y = 0 },
-            O = { x = 1, y = 0 }
+            O = { x = 1, y = 0 },
         },
     },
     nand_gate = {
-        width = 2,
+        width = 3,
         pinoffsets = {
             A = { x = 0, y = -1 },
             B = { x = 1, y = 1 },
-            O = { x = 2, y = 0 }
+            O = { x = 2, y = 0 },
         },
     },
     nor_gate = {
-        width = 2,
+        width = 3,
         pinoffsets = {
-            A = { x = 0, y = 0 },
-            B = { x = 1, y = 0 },
-            O = { x = 2, y = 0 }
+            A = { x = 0, y = -1 },
+            B = { x = 1, y = 1 },
+            O = { x = 2, y = 0 },
+        },
+    },
+    or_gate = {
+        width = 5,
+        pinoffsets = {
+            A = { x = 0, y = -1 },
+            B = { x = 1, y = 1 },
+            O = { x = 4, y = 0 },
+        },
+    },
+    and_gate = {
+        width = 5,
+        pinoffsets = {
+            A = { x = 0, y = -1 },
+            B = { x = 1, y = 1 },
+            O = { x = 4, y = 0 },
         },
     },
     xor_gate = {
         width = 10,
         pinoffsets = {
-            A = { x = 0, y = 0 },
-            B = { x = 1, y = 0 },
-            O = { x = 10, y = 0 }
-        },
-    },
-    xnor_gate = {
-        width = 13,
-        pinoffsets = {
-            A = { x = 0, y = 0 },
-            B = { x = 1, y = 0 },
-            O = { x = 13, y = 0 }
-        },
-    },
-    dffpq = {
-        width = 22,
-        pinoffsets = {
-            CLK = { x = 0, y = -1 },
-            D = { x = 0, y = 1 },
-            Q = { x = 21, y = 0 },
-        },
-        blockages = {
-            {
-                { x = 0, y = 1, z = 2},
-                { x = 6, y = 1, z = 2},
-            },
-            {
-                { x = 3, y = 0, z = 2},
-                { x = 17, y = 0, z = 2},
-            },
-            {
-                { x = 1, y = -1, z = 2},
-                { x = 17, y = -1, z = 2},
-            },
-        }
-    },
-    dffnq = {
-        width = 22,
-        pinoffsets = {
-            CLK = { x = 0, y = -1 },
-            D = { x = 0, y = 1 },
-            Q = { x = 21, y = 0 },
-        },
-        blockages = {
-            {
-                { x = 0, y = 1, z = 2},
-                { x = 6, y = 1, z = 2},
-            },
-            {
-                { x = 3, y = 0, z = 2},
-                { x = 17, y = 0, z = 2},
-            },
-            {
-                { x = 1, y = -1, z = 2},
-                { x = 17, y = -1, z = 2},
-            },
-        }
-    },
-    dffprq = {
-        width = 25,
-        pinoffsets = {
-            CLK = { x = 0, y = -1 },
-            D = { x = 0, y = 1 },
-            Q = { x = 24, y = 0 },
-            RST = { x = 16, y = -2 }
-        },
-        blockages = {
-            {
-                { x = 0, y = 1, z = 2},
-                { x = 6, y = 1, z = 2},
-            },
-            {
-                { x = 2, y = -1, z = 2},
-                { x = 22, y = -1, z = 2},
-            },
-            {
-                { x = 3, y = 0, z = 2},
-                { x = 8, y = 0, z = 2},
-                { x = 8, y = 1, z = 2},
-                { x = 19, y = 1, z = 2},
-            },
-            {
-                { x = 10, y = 0, z = 2},
-                { x = 21, y = 0, z = 2},
-            },
-        }
-    },
-    dffprsq = {
-        width = 25,
-        pinoffsets = {
-            CLK = { x = 0, y = 0 },
-            D = { x = 0, y = 0 },
-            Q = { x = 20, y = 0 }
+            A = { x = 0, y = -1 },
+            B = { x = 0, y = 1 },
+            O = { x = 3, y = 0 },
         },
     },
 }
