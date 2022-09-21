63856ec780826d4a19ec0130ef7a63a563fa03af
Patrick Kurth <p.kurth@posteo.de>
2022-09-21 15:08:50 +0200

    updated opc tech constraints

--- tech/opc/constraints.lua
+++ tech/opc/constraints.lua
@@ -3,7 +3,7 @@ return {
     ["Minimum Gate Extension"] = 100,
     ["Minimum Gate Length"]    = 200,
     ["Minimum Gate Width"]     = 500,
-    ["Minimum Gate XSpace"]     = 300,
+    ["Minimum Gate XSpace"]    = 300,
     ["Minimum Gate YSpace"]    = 300,
     ["Minimum M1 Width"]       = 200,
     ["Minimum M1 Space"]       = 200,
