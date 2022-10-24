b839f07832afb97fade3da435626b0181f5b9095
Patrick Kurth <p.kurth@posteo.de>
2022-10-24 09:49:50 +0200

    removed old testsuite

--- src/testsuite/object/all.lua
+++
@@ -1,7 +0,0 @@
--- luacheck: globals report
-do
-    --local status, msg = check_points(pts, ref)
-    local status = true
-    local msg = "flipy is wrong"
-    report("flipy", status, msg)
-end
--- src/testsuite/point/basic.lua
+++
@@ -1,9 +0,0 @@
--- luacheck: globals report check_point
-do
-    local pt1 = point.create(10, 0)
-    local pt2 = point.create(10, 0)
-    local status, msg = check_point(pt1 + pt2, point.create(20, 0))
-    report("horizontal piece", status, msg)
-end
-
-return true
