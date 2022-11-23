0de7856483718d16cb336276b4a1f692a66a1bcf
Patrick Kurth <p.kurth@posteo.de>
2022-11-23 11:38:39 +0100

    Added aux.find and aux.find_predicate

--- src/modules/pcell.lua
+++ src/modules/pcell.lua
@@ -110,7 +110,7 @@ function paramlib.check_constraints(parameter, value)
     local name = parameter.name
     if posvals then
         if posvals.type == "set" then
-            local found = aux.find(posvals.values, function(v) return v == value end)
+            local found = aux.find_predicate(posvals.values, function(v) return v == value end)
             if not found then
                 moderror(string.format("parameter '%s' (%s) can only be %s", name, value, table.concat(posvals.values, " or ")))
             end
