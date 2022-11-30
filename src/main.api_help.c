5d1dab6a75f4f5baf69fba01e27ddf562f17eac0
Patrick Kurth <p.kurth@posteo.de>
2022-11-30 20:06:38 +0100

    Added more documentation

--- src/main.api_help.c
+++ src/main.api_help.c
@@ -1221,7 +1221,11 @@ struct vector* _initialize_api_entries(void)
         vector_append(entries, _make_api_entry(
             "insert_filler_names",
             MODULE_PLACEMENT,
-            "equalize placement rows by inserting fillers in every row. The method tries to equalize spacing between cells. This function is intended to be called in a place-and-route-script for --import-verilog",
+            // help text
+            "equalize placement rows by inserting fillers in every row."
+            "The method tries to equalize spacing between cells."
+            "This function is intended to be called in a place-and-route-script for --import-verilog",
+            // example
             "placement.insert_filler_names(rows, 200)",
             parameters,
             sizeof(parameters) / sizeof(parameters[0])
@@ -1236,9 +1240,13 @@ struct vector* _initialize_api_entries(void)
         vector_append(entries, _make_api_entry(
             "create_reference_rows",
             MODULE_PLACEMENT,
+            // help text
             "prepare a row placement table for further placement functions by parsing a definition given in 'cellnames'."
             "This table contains the individual rows of the placment, which every row consiting of individual cells."
-            "Cell entries can either be given by just the name of the standard cell (the 'reference') or the instance name ('instance') and the reference name ('reference')",
+            "Cell entries can either be given by just the name of the standard cell (the 'reference') or the instance name ('instance') and the reference name ('reference')"
+            "This function is meant to be used in pcell definitions"
+            ,
+            // example
             "-- un-named mode:\n"
             "local rows = placement.create_reference_rows({\n"
             "    { \"inv\", \"nand1\", \"dff_out\" },\n"
@@ -1311,44 +1319,64 @@ struct vector* _initialize_api_entries(void)
             sizeof(parameters) / sizeof(parameters[0])
         ));
     }
-    /* curve.lineto, */ // FIXME: lineto,
+    /* curve.lineto, */
     {
         struct parameter parameters[] = {
-
+            { "point", POINT, NULL, "destination point of the line segment" }
         };
         vector_append(entries, _make_api_entry(
-            "lineto,",
+            "lineto",
             MODULE_CURVE,
-            "",
-            "",
+            // help text
+            "create a line segment for a curve",
+            // example
+            "geometry.curve(cell, generics.metal(1), point.create(0, 0), {\n"
+            "	curve.lineto(point.create(1000, 1000)),\n"
+            "}, grid, allow45)\n"
+            ,
             parameters,
             sizeof(parameters) / sizeof(parameters[0])
         ));
     }
-    /* curve.arcto, */ // FIXME: arcto,
+    /* curve.arcto, */
     {
         struct parameter parameters[] = {
-
+            { "startangle", NUMBER,     NULL, "start angle of the line segment" },
+            { "endangle",   NUMBER,     NULL, "end angle of the line segment" },
+            { "radius",     INTEGER,    NULL, "radius of the line segment" },
+            { "clockwise",  BOOLEAN,    NULL, "flag if arc is drawn clock-wise or counter-clock-wise" }
         };
         vector_append(entries, _make_api_entry(
-            "arcto,",
+            "arcto",
             MODULE_CURVE,
-            "",
-            "",
+            // help text
+            "create an arc segment for a curve",
+            // example
+            "geometry.curve(cell, generics.metal(1), point.create(0, 0), {\n"
+            "	curve.arcto(180, 0, 1000, true),\n"
+            "}, grid, allow45)\n"
+            ,
             parameters,
             sizeof(parameters) / sizeof(parameters[0])
         ));
     }
-    /* curve.cubicto */ // FIXME: cubicto
+    /* curve.cubicto */
     {
         struct parameter parameters[] = {
-
+            { "ctp1",   POINT, NULL, "first control point" },
+            { "ctp2",   POINT, NULL, "second control point" },
+            { "endpt",  POINT, NULL, "destination point of the cubic bezier segment" }
         };
         vector_append(entries, _make_api_entry(
             "cubicto",
             MODULE_CURVE,
-            "",
-            "",
+            // help text
+            "create a cubic bezier segment for a curve",
+            // example
+            "geometry.curve(cell, generics.metal(1), point.create(0, 0), {\n"
+            "	curve.cubicto(point.create(0, 500), point.create(500, 500), point.create(500, 0)),\n"
+            "}, grid, allow45)\n"
+            ,
             parameters,
             sizeof(parameters) / sizeof(parameters[0])
         ));
