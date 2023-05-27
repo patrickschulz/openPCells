9a1612d17cf31e8daeb5717e96f6e75f7f56cb41
Patrick Kurth <p.kurth@posteo.de>
2023-05-27 21:18:53 +0200

    fixed indentation

--- src/ldebug.c
+++ src/ldebug.c
@@ -12,51 +12,51 @@ void ldebug_dump_stack(lua_State *L)
         int t = lua_type(L, i);
         switch(t)
         {
-        case LUA_TSTRING:
-            printf("%i (%i) = string: \"%s\"", i, i - (top + 1), lua_tostring(L, i));
-            break;
+            case LUA_TSTRING:
+                printf("%i (%i) = string: \"%s\"", i, i - (top + 1), lua_tostring(L, i));
+                break;
 
-        case LUA_TBOOLEAN:
-            printf("%i (%i) = %s", i, i - (top + 1), lua_toboolean(L, i) ? "true" : "false");
-            break;
+            case LUA_TBOOLEAN:
+                printf("%i (%i) = %s", i, i - (top + 1), lua_toboolean(L, i) ? "true" : "false");
+                break;
 
-        case LUA_TNUMBER:
-            printf("%i (%i) = %g", i, i - (top + 1), lua_tonumber(L, i));
-            break;
+            case LUA_TNUMBER:
+                printf("%i (%i) = %g", i, i - (top + 1), lua_tonumber(L, i));
+                break;
 
-        case LUA_TUSERDATA:
-            /* FIXME: broken, but I don't know why
-            if(lua_getmetatable(L, i))
-            {
-                lua_pushstring(L, "__tostring");
-                lua_gettable(L, -2);
-                if(lua_isnil(L, -1))
-                {
-                    lua_pop(L, 1); // pop nil
-                    lua_pushstring(L, "userdata");
-                }
-                else
-                {
-                    lua_pushvalue(L, i);
-                    lua_call(L, 1, 1);
-                }
-                lua_insert(L, -2);
-                lua_pop(L, 1); // pop meta table
-            }
-            else
-            {
-                lua_pushstring(L, "userdata");
-            }
-            const char* tag = lua_tostring(L, -1);
-            printf("%i (%i) = %s: %p", i, i - (top + 1), tag, lua_touserdata(L, i));
-            lua_pop(L, 1);
-            */
-            printf("%i (%i) = %s: %p", i, i - (top + 1), "userdata", lua_touserdata(L, i));
-            break;
+            case LUA_TUSERDATA:
+                /* FIXME: broken, but I don't know why
+                   if(lua_getmetatable(L, i))
+                   {
+                   lua_pushstring(L, "__tostring");
+                   lua_gettable(L, -2);
+                   if(lua_isnil(L, -1))
+                   {
+                   lua_pop(L, 1); // pop nil
+                   lua_pushstring(L, "userdata");
+                   }
+                   else
+                   {
+                   lua_pushvalue(L, i);
+                   lua_call(L, 1, 1);
+                   }
+                   lua_insert(L, -2);
+                   lua_pop(L, 1); // pop meta table
+                   }
+                   else
+                   {
+                   lua_pushstring(L, "userdata");
+                   }
+                   const char* tag = lua_tostring(L, -1);
+                   printf("%i (%i) = %s: %p", i, i - (top + 1), tag, lua_touserdata(L, i));
+                   lua_pop(L, 1);
+                   */
+                printf("%i (%i) = %s: %p", i, i - (top + 1), "userdata", lua_touserdata(L, i));
+                break;
 
-        default:
-            printf("%i (%i) = %s", i, i - (top + 1), lua_typename(L, t));
-            break;
+            default:
+                printf("%i (%i) = %s", i, i - (top + 1), lua_typename(L, t));
+                break;
         }
         putchar('\n');
     }
