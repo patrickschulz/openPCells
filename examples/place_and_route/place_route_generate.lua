f71157457d8d6258bc43ee53955e67473db57e08
Patrick Kurth <p.kurth@posteo.de>
2023-06-18 19:35:00 +0200

    updated place & route example

--- examples/place_and_route/place_route_generate.lua
+++ examples/place_and_route/place_route_generate.lua
@@ -2,8 +2,6 @@ if not args[1] then
     error("no target given")
 end
 
-print(args)
-
 local module = args[1]
 local exporttype = "gds"
 
@@ -12,21 +10,13 @@ local netlist = verilog.read_parse_file(string.format("%s.v", module))
 verilogprocessor.write_spice_netlist(string.format("%s_netlist.sp", module), netlist)
 
 local cellinfo = verilogprocessor.read_cellinfo_from_file("cellinfo.lua")
-local ignorednets = {
-    --"chain_out",
-    --"ff_in",
-    --"enable",
-    --"_00_",
-    --"_05_",
-    --"_06_",
-    --"_07_",
-}
-local instances, nets = verilogprocessor.collect_nets_cells(netlist, cellinfo, ignorednets)
+local ignorednets = {} -- insert names of nets that should be ignored
+local circuit = verilogprocessor.collect_nets_cells(netlist, cellinfo, ignorednets)
 
 local utilization = 0.6
-local numrows = 3
-local floorplan = placement.create_floorplan_fixed_rows(instances, utilization, numrows)
-local rows = placement.optimize(instances, nets, floorplan)
+local numrows = 2
+local floorplan = placement.create_floorplan_fixed_rows(circuit, utilization, numrows)
+local rows = placement.optimize(circuit, floorplan)
 --local plan = {
 --    { "inv", "nand1", "dff_out" },
 --    { "nand2", "dff_buf" },
@@ -36,11 +26,18 @@ local rows = placement.optimize(instances, nets, floorplan)
 --    { "inv1", },
 --    { "inv2" }
 --}
---local rows = placement.manual(instances, plan)
+--local rows = placement.manual(circuit, plan)
 placement.insert_filler_names(rows, floorplan.floorplan_width)
 
+-- hack: insert fill at the left side
+-- this is done to ensure that routing works if a dff is there, since their gates extend to the left
+-- this will be fixed and removed at some point
 for i in ipairs(rows) do
-    table.insert(rows[i], 1, {reference = "isogate", instance = string.format("hackfill_%i", i), width = 1})
+    table.insert(rows[i], 1, {
+        reference = "isogate",
+        instance = string.format("hackfill_%i", i),
+        width = 1
+    })
 end
 floorplan.floorplan_width = floorplan.floorplan_width + 1
 
@@ -50,7 +47,7 @@ local pnumtracks = 4
 local nnumtracks = 4
 local numinnertracks = 3
 
-local routes = routing.legalize(nets, rows, numinnertracks, pnumtracks, nnumtracks, floorplan, instances)
+local routes = routing.legalize(circuit, rows, numinnertracks, pnumtracks, nnumtracks, floorplan)
 
 local filename = generator.get_cell_filename("verilogimport", "verilogimport", module)
 print(string.format("writing to file '%s'", filename))
--- examples/place_and_route/run.sh
+++ examples/place_and_route/run.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-CELL=command_reg
+CELL=register_cell
 
 ../../opc --import-verilog place_route_generate.lua ${CELL}
 ../../opc --technology GF22FDSOI --export gds --cellscript generate_cell.lua --cellpath verilogimport ${CELL}
