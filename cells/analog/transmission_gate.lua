267f83419213ce43e2175a1344dac785a78f6a4d
Patrick Schulz <pschulz@posteo.de>
2023-06-19 11:51:44 +0200

    working on improving transmission gate

--- cells/analog/transmission_gate.lua
+++ cells/analog/transmission_gate.lua
@@ -1,13 +1,13 @@
 function parameters()
     pcell.add_parameters(
         { "glength", technology.get_dimension("Minimum Gate Length") },
-        { "gspace", technology.get_dimension("Minimum Gate Space") },
+        { "gspace", technology.get_dimension("Minimum Gate XSpace") },
         { "fingers", 2 },
         { "pwidth", 2 * technology.get_dimension("Minimum Gate Width") },
         { "nwidth", 2 * technology.get_dimension("Minimum Gate Width") },
-        { "gstwidth", technology.get_dimension("Minimum M1 Width") },
-        { "gstspace", technology.get_dimension("Minimum M1 Space") },
+        { "gatestrapwidth", technology.get_dimension("Minimum M1 Width") },
         { "sdwidth", technology.get_dimension("Minimum M1 Width") },
+        { "sdspace", technology.get_dimension("Minimum M1 Space") },
         { "powerspace", technology.get_dimension("Minimum M1 Space") },
         { "nmosflippedwell", false },
         { "pmosflippedwell", false },
@@ -18,31 +18,32 @@ function parameters()
 end
 
 function layout(tgate, _P)
-    local guardringxsep = 200
-    local guardringysep = 200
+    local guardringxsep = 300
+    local guardringysep = 400
     pcell.push_overwrites("basic/mosfet", {
         gatelength = _P.glength,
         gatespace = _P.gspace,
         fingers = _P.fingers,
         sdwidth = _P.sdwidth,
-        conndrainwidth = _P.sdwidth,
-        connsourcewidth = _P.sdwidth,
         connectsource = true,
+        connectsourcewidth = _P.sdwidth,
+        connectsourcespace = _P.sdspace,
         connectdrain = true,
-        conndrainspace = _P.sdwidth + _P.sdwidth + guardringysep + _P.guardringwidth + _P.powerspace,
-        conndrainmetal = 2,
+        connectdrainwidth = _P.sdwidth,
+        connectdrainspace = _P.sdspace,
+        connectdrainmetal = 2,
         drawdrainvia = true,
         drawguardring = true,
         guardringwidth = _P.guardringwidth,
         guardringxsep = guardringxsep,
         guardringysep = guardringysep,
-        guardringsegments = { "top", "bottom" },
+        guardringsegments = { "top", "bottom", "left", "right" },
         drawtopgate = true,
+        topgatestrspace = _P.sdwidth + 2 * _P.sdspace,
+        topgatestrwidth = _P.gatestrapwidth,
         drawbotgate = true,
-        topgatestrwidth = _P.sdwidth,
-        topgatestrspace = _P.sdwidth,
-        botgatestrwidth = _P.sdwidth,
-        botgatestrspace = _P.sdwidth,
+        botgatestrspace = _P.sdwidth + 2 * _P.sdspace,
+        botgatestrwidth = _P.gatestrapwidth,
     })
     local pmos = pcell.create_layout("basic/mosfet", "pmos", {
         channeltype = "pmos",
@@ -50,7 +51,6 @@ function layout(tgate, _P)
         fwidth = _P.pwidth,
         vthtype = _P.pmosvthtype,
         flippedwell = _P.pmosflippedwell,
-        botgatecompsd = false,
     })
     local nmos = pcell.create_layout("basic/mosfet", "nmos", {
         channeltype = "nmos",
@@ -58,61 +58,145 @@ function layout(tgate, _P)
         fwidth = _P.nwidth,
         vthtype = _P.nmosvthtype,
         flippedwell = _P.nmosflippedwell,
-        topgatecompsd = false,
     })
     pcell.pop_overwrites("basic/mosfet")
-    pmos:move_anchor_y("bottom")
-    pmos:translate(0, _P.sdwidth / 2 + _P.guardringwidth / 2 + _P.powerspace)
-    nmos:move_anchor_y("top")
-    nmos:translate(0, -_P.sdwidth / 2 - _P.guardringwidth / 2 - _P.powerspace)
+    pmos:abut_area_anchor_top("outerguardring", nmos, "outerguardring")
+    pmos:translate(0, _P.sdwidth + 2 * _P.powerspace)
     tgate:merge_into(pmos)
     tgate:merge_into(nmos)
 
     -- additional rails between guard rings
     geometry.rectanglebltr(tgate, generics.metal(1),
-        nmos:get_anchor("guardringtl") .. point.create(0, -_P.sdwidth / 2),
-        pmos:get_anchor("guardringbr") .. point.create(0,  _P.sdwidth / 2)
+        nmos:get_area_anchor("outerguardring").tl:translate(0, _P.powerspace),
+        pmos:get_area_anchor("outerguardring").br:translate(0, -_P.powerspace)
     )
     geometry.rectanglebltr(tgate, generics.metal(1),
-        pmos:get_anchor("guardringtl"):translate(0, _P.powerspace),
-        pmos:get_anchor("guardringtr"):translate(0, _P.powerspace + _P.sdwidth)
+        pmos:get_area_anchor("outerguardring").tl:translate(0, _P.powerspace),
+        pmos:get_area_anchor("outerguardring").tr:translate(0, _P.powerspace + _P.sdwidth)
     )
     geometry.rectanglebltr(tgate, generics.metal(1),
-        nmos:get_anchor("guardringbl"):translate(0, -_P.powerspace - _P.sdwidth),
-        nmos:get_anchor("guardringbr"):translate(0, -_P.powerspace)
+        nmos:get_area_anchor("outerguardring").bl:translate(0, -_P.powerspace - _P.sdwidth),
+        nmos:get_area_anchor("outerguardring").br:translate(0, -_P.powerspace)
+    )
+    tgate:add_area_anchor_bltr("lowerrail",
+        nmos:get_area_anchor("outerguardring").bl:translate(0, -_P.powerspace - _P.sdwidth),
+        nmos:get_area_anchor("outerguardring").br:translate(0, -_P.powerspace)
+    )
+    tgate:add_area_anchor_bltr("middlerail",
+        nmos:get_area_anchor("outerguardring").tl:translate(0, _P.powerspace),
+        pmos:get_area_anchor("outerguardring").br:translate(0, -_P.powerspace)
+    )
+    tgate:add_area_anchor_bltr("upperrail",
+        pmos:get_area_anchor("outerguardring").tl:translate(0, _P.powerspace),
+        pmos:get_area_anchor("outerguardring").tr:translate(0, _P.powerspace + _P.sdwidth)
     )
 
     -- connect top and bottom gate strap
-    geometry.cshape(tgate, generics.metal(1),
-        nmos:get_anchor("botgatestrapcr"),
-        nmos:get_anchor("topgatestrapcr"),
-        2 * (_P.glength + _P.gspace),
-        _P.sdwidth
-    )
-    geometry.cshape(tgate, generics.metal(1),
-        pmos:get_anchor("botgatestrapcr"),
-        pmos:get_anchor("topgatestrapcr"),
-        2 * (_P.glength + _P.gspace),
-        _P.sdwidth
-    )
+    local gateconnoffset = 200
+    geometry.polygon(tgate, generics.metal(1), {
+        nmos:get_area_anchor("botgatestrap").tl,
+        nmos:get_area_anchor("botgatestrap").tl:translate_x(-gateconnoffset),
+        nmos:get_area_anchor("topgatestrap").bl:translate_x(-gateconnoffset),
+        nmos:get_area_anchor("topgatestrap").bl,
+        nmos:get_area_anchor("topgatestrap").tl,
+        nmos:get_area_anchor("topgatestrap").tl:translate_x(-gateconnoffset - _P.gatestrapwidth),
+        nmos:get_area_anchor("botgatestrap").bl:translate_x(-gateconnoffset - _P.gatestrapwidth),
+        nmos:get_area_anchor("botgatestrap").bl
+    })
+    geometry.polygon(tgate, generics.metal(1), {
+        pmos:get_area_anchor("botgatestrap").tl,
+        pmos:get_area_anchor("botgatestrap").tl:translate_x(-gateconnoffset),
+        pmos:get_area_anchor("topgatestrap").bl:translate_x(-gateconnoffset),
+        pmos:get_area_anchor("topgatestrap").bl,
+        pmos:get_area_anchor("topgatestrap").tl,
+        pmos:get_area_anchor("topgatestrap").tl:translate_x(-gateconnoffset - _P.gatestrapwidth),
+        pmos:get_area_anchor("botgatestrap").bl:translate_x(-gateconnoffset - _P.gatestrapwidth),
+        pmos:get_area_anchor("botgatestrap").bl
+    })
+    --geometry.polygon(tgate, generics.metal(1), {
+    --    nmos:get_area_anchor("botgatestrap").br,
+    --    nmos:get_area_anchor("botgatestrap").br:translate_x(gateconnoffset + _P.gatestrapwidth),
+    --    nmos:get_area_anchor("topgatestrap").tr:translate_x(gateconnoffset + _P.gatestrapwidth),
+    --    nmos:get_area_anchor("topgatestrap").tr,
+    --    nmos:get_area_anchor("topgatestrap").br,
+    --    nmos:get_area_anchor("topgatestrap").br:translate_x(gateconnoffset),
+    --    nmos:get_area_anchor("botgatestrap").tr:translate_x(gateconnoffset),
+    --    nmos:get_area_anchor("botgatestrap").tl,
+    --})
+    --geometry.polygon(tgate, generics.metal(1), {
+    --    pmos:get_area_anchor("botgatestrap").br,
+    --    pmos:get_area_anchor("botgatestrap").br:translate_x(gateconnoffset + _P.gatestrapwidth),
+    --    pmos:get_area_anchor("topgatestrap").tr:translate_x(gateconnoffset + _P.gatestrapwidth),
+    --    pmos:get_area_anchor("topgatestrap").tr,
+    --    pmos:get_area_anchor("topgatestrap").br,
+    --    pmos:get_area_anchor("topgatestrap").br:translate_x(gateconnoffset),
+    --    pmos:get_area_anchor("botgatestrap").tr:translate_x(gateconnoffset),
+    --    pmos:get_area_anchor("botgatestrap").tl,
+    --})
 
     -- input connection
     local inputconnoffset = 500
-    geometry.cshape(tgate, generics.metal(1),
-        nmos:get_anchor("sourcestrapcl"),
-        pmos:get_anchor("sourcestrapcl"),
-        -inputconnoffset,
-        _P.sdwidth
+    geometry.polygon(tgate, generics.metal(3), {
+        nmos:get_area_anchor("sourcestrap").tl,
+        nmos:get_area_anchor("sourcestrap").tl:translate_x(-inputconnoffset),
+        pmos:get_area_anchor("sourcestrap").bl:translate_x(-inputconnoffset),
+        pmos:get_area_anchor("sourcestrap").bl,
+        pmos:get_area_anchor("sourcestrap").tl,
+        pmos:get_area_anchor("sourcestrap").tl:translate_x(-inputconnoffset - _P.sdwidth),
+        nmos:get_area_anchor("sourcestrap").bl:translate_x(-inputconnoffset - _P.sdwidth),
+        nmos:get_area_anchor("sourcestrap").bl,
+    })
+    geometry.viabltr(tgate, 1, 3,
+        nmos:get_area_anchor("sourcestrap").bl,
+        nmos:get_area_anchor("sourcestrap").tr
     )
+    geometry.viabltr(tgate, 1, 3,
+        pmos:get_area_anchor("sourcestrap").bl,
+        pmos:get_area_anchor("sourcestrap").tr
+    )
+
+    -- output connection
+    local outputconnoffset = 500
+    geometry.polygon(tgate, generics.metal(2), {
+        nmos:get_area_anchor("drainstrap").br,
+        nmos:get_area_anchor("drainstrap").br:translate_x(outputconnoffset + _P.sdwidth),
+        pmos:get_area_anchor("drainstrap").tr:translate_x(outputconnoffset + _P.sdwidth),
+        pmos:get_area_anchor("drainstrap").tr,
+        pmos:get_area_anchor("drainstrap").br,
+        pmos:get_area_anchor("drainstrap").br:translate_x(outputconnoffset),
+        nmos:get_area_anchor("drainstrap").tr:translate_x(outputconnoffset),
+        nmos:get_area_anchor("drainstrap").tr,
+    })
 
     -- alignmentbox
     tgate:set_alignment_box(
-        nmos:get_anchor("guardringbl"):translate(0, -_P.powerspace - _P.sdwidth / 2),
-        pmos:get_anchor("guardringtr"):translate(0, _P.powerspace + _P.sdwidth / 2)
+        nmos:get_area_anchor("outerguardring").bl:translate(0, -_P.powerspace - _P.sdwidth),
+        pmos:get_area_anchor("outerguardring").tr:translate(0, _P.powerspace + _P.sdwidth),
+        nmos:get_area_anchor("innerguardring").bl:translate(0, -_P.guardringwidth - _P.powerspace),
+        pmos:get_area_anchor("innerguardring").tr:translate(0, _P.guardringwidth + _P.powerspace)
     )
 
-    tgate:add_area_anchor_bltr("clkp", pmos:get_anchor("topgatestrapbl"), pmos:get_anchor("topgatestraptr"))
-    tgate:add_area_anchor_bltr("clkn", nmos:get_anchor("botgatestrapbl"), nmos:get_anchor("botgatestraptr"))
-    tgate:add_port("input", generics.metalport(1), (pmos:get_anchor("sourcestrapcl") + nmos:get_anchor("sourcestrapcl")):translate(-inputconnoffset, 0))
-    tgate:add_port("output", generics.metalport(1), (pmos:get_anchor("drainstrapcc") + nmos:get_anchor("drainstrapcc")))
+    --tgate:add_area_anchor_bltr("clkp", pmos:get_area_anchor("topgatestrap").bl, pmos:get_area_anchor("topgatestrap").tr)
+    --tgate:add_area_anchor_bltr("clkn", nmos:get_area_anchor("botgatestrap").bl, nmos:get_area_anchor("botgatestrap").tr)
+    --tgate:add_port("input", generics.metalport(1), (pmos:get_area_anchor("sourcestrap").cl + nmos:get_area_anchor("sourcestrap").cl):translate(-inputconnoffset, 0))
+    tgate:add_area_anchor_bltr("input",
+        point.combine(
+            pmos:get_area_anchor("sourcestrap").tr,
+            nmos:get_area_anchor("sourcestrap").br
+        ):translate(-inputconnoffset - _P.sdwidth, -_P.sdwidth / 2),
+        point.combine(
+            pmos:get_area_anchor("sourcestrap").tr,
+            nmos:get_area_anchor("sourcestrap").br
+        ):translate(-inputconnoffset, _P.sdwidth / 2)
+    )
+    tgate:add_area_anchor_bltr("output",
+        point.combine(
+            pmos:get_area_anchor("drainstrap").tr,
+            nmos:get_area_anchor("drainstrap").br
+        ):translate(outputconnoffset, -_P.sdwidth / 2),
+        point.combine(
+            pmos:get_area_anchor("drainstrap").tr,
+            nmos:get_area_anchor("drainstrap").br
+        ):translate(outputconnoffset + _P.sdwidth, _P.sdwidth / 2)
+    )
 end
