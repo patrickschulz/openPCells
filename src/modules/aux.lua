ad5a5bd2b437562bbb330af948597f1bf1ed4cb7
Patrick Kurth <p.kurth@posteo.de>
2023-07-23 20:42:42 +0200

    fixed formatting

--- src/modules/aux.lua
+++ src/modules/aux.lua
@@ -150,29 +150,29 @@ end
 --   doSomething(substr)
 -- end
 function aux.strgsplit(text, pattern, plain)
-  local splitStart, length = 1, #text
-  return function ()
-    if splitStart then
-      local sepStart, sepEnd = string.find(text, pattern, splitStart, plain)
-      local ret
-      if not sepStart then
-        ret = string.sub(text, splitStart)
-        splitStart = nil
-      elseif sepEnd < sepStart then
-        -- Empty separator!
-        ret = string.sub(text, splitStart, sepStart)
-        if sepStart < length then
-          splitStart = sepStart + 1
-        else
-          splitStart = nil
+    local splitStart, length = 1, #text
+    return function ()
+        if splitStart then
+            local sepStart, sepEnd = string.find(text, pattern, splitStart, plain)
+            local ret
+            if not sepStart then
+                ret = string.sub(text, splitStart)
+                splitStart = nil
+            elseif sepEnd < sepStart then
+                -- Empty separator!
+                ret = string.sub(text, splitStart, sepStart)
+                if sepStart < length then
+                    splitStart = sepStart + 1
+                else
+                    splitStart = nil
+                end
+            else
+                ret = sepStart > splitStart and string.sub(text, splitStart, sepStart - 1) or ''
+                splitStart = sepEnd + 1
+            end
+            return ret
         end
-      else
-        ret = sepStart > splitStart and string.sub(text, splitStart, sepStart - 1) or ''
-        splitStart = sepEnd + 1
-      end
-      return ret
     end
-  end
 end
 
 -- split: split a string into substrings separated by a pattern.
@@ -185,11 +185,11 @@ end
 -- 
 -- Returns: table (a sequence table containing the substrings)
 function aux.strsplit(text, pattern, plain)
-  local ret = {}
-  for match in aux.strgsplit(text, pattern, plain) do
-    table.insert(ret, match)
-  end
-  return ret
+    local ret = {}
+    for match in aux.strgsplit(text, pattern, plain) do
+        table.insert(ret, match)
+    end
+    return ret
 end
 
 function aux.tprint(tbl, indent)                                                   
