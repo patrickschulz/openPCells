f666c7c81d2a37659e2e5061688aee964cd37ddf
Patrick Kurth <p.kurth@posteo.de>
2023-07-25 22:43:09 +0200

    added rectanglev/hlines_width/height_space_settings

--- src/lgeometry.c
+++ src/lgeometry.c
@@ -234,54 +234,6 @@ static int lgeometry_path_manhatten(lua_State* L)
     return 0;
 }
 
-static int lgeometry_rectanglelines_vertical_settings(lua_State* L)
-{
-    lcheck_check_numargs(L, 4, "geometry.rectanglevlines_settings");
-    struct lpoint* pt1 = lpoint_checkpoint(L, 1);
-    struct lpoint* pt2 = lpoint_checkpoint(L, 2);
-    int numlines = lua_tointeger(L, 3);
-    double ratio = lua_tonumber(L, 4);
-
-    if(numlines <= 0)
-    {
-        lua_pushfstring(L, "geometry.rectanglevlines_settings: number of lines must be greater than zero (got %d)", numlines);
-        lua_error(L);
-    }
-
-    if(ratio <= 0)
-    {
-        lua_pushfstring(L, "geometry.rectanglevlines_settings: ratio must be greater than zero (got %f)", ratio);
-        lua_error(L);
-    }
-
-    const point_t* bl = lpoint_get(pt1);
-    const point_t* tr = lpoint_get(pt2);
-
-    ucoordinate_t totalwidth = point_xdifference(tr, bl);
-    // ensure that the width and the space of the lines is even
-    unsigned int correction = 0;
-    while(totalwidth % ((ucoordinate_t)(numlines * 4 * (ratio + 1))) != 0)
-    {
-        --totalwidth;
-        ++correction;
-    }
-
-    ucoordinate_t height = point_ydifference(tr, bl);
-    ucoordinate_t pitch = totalwidth / numlines;
-    ucoordinate_t space = pitch / (ratio + 1);
-    ucoordinate_t width = pitch - space;
-
-    coordinate_t offset = (correction + space) / 2;
-
-    lua_pushinteger(L, width);
-    lua_pushinteger(L, height);
-    lua_pushinteger(L, space);
-    lua_pushinteger(L, offset);
-    lua_pushinteger(L, numlines);
-
-    return 5;
-}
-
 static int lgeometry_rectanglelines_vertical(lua_State* L)
 {
     lcheck_check_numargs(L, 6, "geometry.rectanglevlines");
@@ -335,6 +287,80 @@ static int lgeometry_rectanglelines_vertical(lua_State* L)
     return 0;
 }
 
+static int lgeometry_rectanglelines_vertical_settings(lua_State* L)
+{
+    lcheck_check_numargs(L, 4, "geometry.rectanglevlines_settings");
+    struct lpoint* pt1 = lpoint_checkpoint(L, 1);
+    struct lpoint* pt2 = lpoint_checkpoint(L, 2);
+    int numlines = lua_tointeger(L, 3);
+    double ratio = lua_tonumber(L, 4);
+
+    if(numlines <= 0)
+    {
+        lua_pushfstring(L, "geometry.rectanglevlines_settings: number of lines must be greater than zero (got %d)", numlines);
+        lua_error(L);
+    }
+
+    if(ratio <= 0)
+    {
+        lua_pushfstring(L, "geometry.rectanglevlines_settings: ratio must be greater than zero (got %f)", ratio);
+        lua_error(L);
+    }
+
+    const point_t* bl = lpoint_get(pt1);
+    const point_t* tr = lpoint_get(pt2);
+
+    ucoordinate_t totalwidth = point_xdifference(tr, bl);
+    // ensure that the width and the space of the lines is even
+    unsigned int correction = 0;
+    while(totalwidth % ((ucoordinate_t)(numlines * 4 * (ratio + 1))) != 0)
+    {
+        --totalwidth;
+        ++correction;
+    }
+
+    ucoordinate_t height = point_ydifference(tr, bl);
+    ucoordinate_t pitch = totalwidth / numlines;
+    ucoordinate_t space = pitch / (ratio + 1);
+    ucoordinate_t width = pitch - space;
+
+    coordinate_t offset = (correction + space) / 2;
+
+    lua_pushinteger(L, width);
+    lua_pushinteger(L, height);
+    lua_pushinteger(L, space);
+    lua_pushinteger(L, offset);
+    lua_pushinteger(L, numlines);
+
+    return 5;
+}
+
+static int lgeometry_rectanglelines_vertical_height_space_settings(lua_State* L)
+{
+    lcheck_check_numargs(L, 4, "geometry.rectanglevlines_height_space_settings");
+    struct lpoint* pt1 = lpoint_checkpoint(L, 1);
+    struct lpoint* pt2 = lpoint_checkpoint(L, 2);
+    coordinate_t widthtarget = lua_tointeger(L, 3);
+    coordinate_t spacetarget = lua_tointeger(L, 4);
+
+    const point_t* bl = lpoint_get(pt1);
+    const point_t* tr = lpoint_get(pt2);
+
+    coordinate_t totalwidth = point_xdifference(tr, bl);
+    unsigned int numlines = totalwidth / (widthtarget + spacetarget);
+    coordinate_t width = widthtarget;
+    coordinate_t space = spacetarget;
+    coordinate_t height = point_ydifference(tr, bl);
+    coordinate_t offset = (totalwidth - numlines * (width + space) + space) / 2;
+
+    lua_pushinteger(L, width);
+    lua_pushinteger(L, height);
+    lua_pushinteger(L, space);
+    lua_pushinteger(L, offset);
+    lua_pushinteger(L, numlines);
+
+    return 5;
+}
 static int lgeometry_rectanglelines_horizontal(lua_State* L)
 {
     lcheck_check_numargs(L, 6, "geometry.rectanglehlines");
@@ -426,6 +452,33 @@ static int lgeometry_rectanglelines_horizontal_settings(lua_State* L)
     ucoordinate_t height = pitch - space;
 
     coordinate_t offset = (correction + space) / 2;
+
+    lua_pushinteger(L, width);
+    lua_pushinteger(L, height);
+    lua_pushinteger(L, space);
+    lua_pushinteger(L, offset);
+    lua_pushinteger(L, numlines);
+
+    return 5;
+}
+
+static int lgeometry_rectanglelines_horizontal_width_space_settings(lua_State* L)
+{
+    lcheck_check_numargs(L, 4, "geometry.rectanglehlines_width_space_settings");
+    struct lpoint* pt1 = lpoint_checkpoint(L, 1);
+    struct lpoint* pt2 = lpoint_checkpoint(L, 2);
+    coordinate_t heighttarget = lua_tointeger(L, 3);
+    coordinate_t spacetarget = lua_tointeger(L, 4);
+
+    const point_t* bl = lpoint_get(pt1);
+    const point_t* tr = lpoint_get(pt2);
+
+    coordinate_t totalheight = point_ydifference(tr, bl);
+    unsigned int numlines = totalheight / (heighttarget + spacetarget);
+    coordinate_t height = heighttarget;
+    coordinate_t space = spacetarget;
+    coordinate_t width = point_xdifference(tr, bl);
+    coordinate_t offset = (totalheight - numlines * (height + space) + space) / 2;
 
     lua_pushinteger(L, width);
     lua_pushinteger(L, height);
@@ -1234,37 +1287,39 @@ int open_lgeometry_lib(lua_State* L)
     lua_newtable(L);
     static const luaL_Reg modfuncs[] =
     {
-        { "rectanglebltr",                  lgeometry_rectanglebltr                      },
-        { "rectanglepoints",                lgeometry_rectanglepoints                    },
-        { "rectanglearray",                 lgeometry_rectanglearray                     },
-        { "rectanglepath",                  lgeometry_rectanglepath                      },
-        { "rectanglevlines",                lgeometry_rectanglelines_vertical            },
-        { "rectanglevlines_settings",       lgeometry_rectanglelines_vertical_settings   },
-        { "rectanglehlines",                lgeometry_rectanglelines_horizontal          },
-        { "rectanglehlines_settings",       lgeometry_rectanglelines_horizontal_settings },
-        { "rectangle_fill_in_boundary",     lgeometry_rectangle_fill_in_boundary         },
-        { "polygon",                        lgeometry_polygon                            },
-        { "path",                           lgeometry_path                               },
-        { "path_manhatten",                 lgeometry_path_manhatten                     },
-        { "path_2x",                        lgeometry_path_2x                            },
-        { "path_2y",                        lgeometry_path_2y                            },
-        { "path_cshape",                    lgeometry_path_cshape                        },
-        { "path_ushape",                    lgeometry_path_ushape                        },
-        { "path_polygon",                   lgeometry_path_polygon                       },
-        { "viabltr",                        lgeometry_viabltr                            },
-        { "viabarebltr",                    lgeometry_viabarebltr                        },
-        { "viabltr_xcontinuous",            lgeometry_viabltr_xcontinuous                },
-        { "viabltr_ycontinuous",            lgeometry_viabltr_ycontinuous                },
-        { "viabltr_continuous",             lgeometry_viabltr_continuous                 },
-        { "contactbltr",                    lgeometry_contactbltr                        },
-        { "contactbarebltr",                lgeometry_contactbarebltr                    },
-        { "cross",                          lgeometry_cross                              },
-        { "ring",                           lgeometry_ring                               },
-        { "unequal_ring",                   lgeometry_unequal_ring                       },
-        { "unequal_ring_pts",               lgeometry_unequal_ring_pts                   },
-        { "curve",                          lgeometry_curve                              },
-        { "curve_rasterized",               lgeometry_curve_rasterized                   },
-        { NULL,                             NULL                                         }
+        { "rectanglebltr",                              lgeometry_rectanglebltr                                     },
+        { "rectanglepoints",                            lgeometry_rectanglepoints                                   },
+        { "rectanglearray",                             lgeometry_rectanglearray                                    },
+        { "rectanglepath",                              lgeometry_rectanglepath                                     },
+        { "rectanglevlines",                            lgeometry_rectanglelines_vertical                           },
+        { "rectanglevlines_settings",                   lgeometry_rectanglelines_vertical_settings                  },
+        { "rectanglevlines_height_space_settings",      lgeometry_rectanglelines_vertical_height_space_settings     },
+        { "rectanglehlines",                            lgeometry_rectanglelines_horizontal                         },
+        { "rectanglehlines_settings",                   lgeometry_rectanglelines_horizontal_settings                },
+        { "rectanglehlines_width_space_settings",       lgeometry_rectanglelines_horizontal_width_space_settings    },
+        { "rectangle_fill_in_boundary",                 lgeometry_rectangle_fill_in_boundary                        },
+        { "polygon",                                    lgeometry_polygon                                           },
+        { "path",                                       lgeometry_path                                              },
+        { "path_manhatten",                             lgeometry_path_manhatten                                    },
+        { "path_2x",                                    lgeometry_path_2x                                           },
+        { "path_2y",                                    lgeometry_path_2y                                           },
+        { "path_cshape",                                lgeometry_path_cshape                                       },
+        { "path_ushape",                                lgeometry_path_ushape                                       },
+        { "path_polygon",                               lgeometry_path_polygon                                      },
+        { "viabltr",                                    lgeometry_viabltr                                           },
+        { "viabarebltr",                                lgeometry_viabarebltr                                       },
+        { "viabltr_xcontinuous",                        lgeometry_viabltr_xcontinuous                               },
+        { "viabltr_ycontinuous",                        lgeometry_viabltr_ycontinuous                               },
+        { "viabltr_continuous",                         lgeometry_viabltr_continuous                                },
+        { "contactbltr",                                lgeometry_contactbltr                                       },
+        { "contactbarebltr",                            lgeometry_contactbarebltr                                   },
+        { "cross",                                      lgeometry_cross                                             },
+        { "ring",                                       lgeometry_ring                                              },
+        { "unequal_ring",                               lgeometry_unequal_ring                                      },
+        { "unequal_ring_pts",                           lgeometry_unequal_ring_pts                                  },
+        { "curve",                                      lgeometry_curve                                             },
+        { "curve_rasterized",                           lgeometry_curve_rasterized                                  },
+        { NULL,                                         NULL                                                        }
     };
     luaL_setfuncs(L, modfuncs, 0);
 
