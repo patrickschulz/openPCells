6219be65e2182bbf17319658dcbc62c4e64ff711
Patrick Kurth <p.kurth@posteo.de>
2023-09-25 15:29:53 +0200

    reworked layouthelpers.fit_guardring to place_guardring

--- src/modules/layouthelpers.lua
+++ src/modules/layouthelpers.lua
@@ -1,14 +1,18 @@
 local M = {}
 
-function M.fit_guardring(bl, tr, xspace, yspace)
-    return {
-        alignanchor = "innerbottomleft",
-        aligntarget = bl,
-        holewidth = point.xdistance_abs(bl, tr) + 2 * xspace,
-        holeheight = point.ydistance_abs(bl, tr) + 2 * yspace,
-        xshift = -xspace,
-        yshift = -yspace,
-    }
+function M.place_guardring(cell, bl, tr, xspace, yspace, options)
+    local guardring = pcell.create_layout(
+        "auxiliary/guardring",
+        "_guardring",
+            util.add_options(options or {}, {
+            holewidth = point.xdistance_abs(bl, tr) + 2 * xspace,
+            holeheight = point.ydistance_abs(bl, tr) + 2 * yspace,
+        })
+    )
+    guardring:move_point(guardring:get_anchor("innerbottomleft"), bl)
+    guardring:translate(-xspace, -yspace)
+    cell:merge_into(guardring)
+    cell:inherit_alignment_box(guardring)
 end
 
 return M
