1588bf1604d22ef83ddb352594859da5dd6458ae
Patrick Kurth <p.kurth@posteo.de>
2023-09-26 08:45:20 +0200

    added layouthelpers.place_guardring_with_hole

--- src/modules/layouthelpers.lua
+++ src/modules/layouthelpers.lua
@@ -15,4 +15,26 @@ function M.place_guardring(cell, bl, tr, xspace, yspace, options)
     cell:inherit_alignment_box(guardring)
 end
 
+function M.place_guardring_with_hole(cell, bl, tr, holebl, holetr, xspace, yspace, options)
+    local welloffset = 100
+    local guardring = pcell.create_layout(
+        "auxiliary/guardring",
+        "_guardring",
+            util.add_options(options or {}, {
+            holewidth = point.xdistance_abs(bl, tr) + 2 * xspace,
+            holeheight = point.ydistance_abs(bl, tr) + 2 * yspace,
+            fillwell = true,
+            fillwelldrawhole = true,
+            fillwellholeoffsettop = yspace - point.ydistance(holetr, tr) - welloffset,
+            fillwellholeoffsetbottom = yspace - point.ydistance(bl, holebl) - welloffset,
+            fillwellholeoffsetleft = xspace - point.xdistance(bl, holebl) - welloffset,
+            fillwellholeoffsetright = xspace - point.xdistance(holetr, tr) - welloffset,
+        })
+    )
+    guardring:move_point(guardring:get_anchor("innerbottomleft"), bl)
+    guardring:translate(-xspace, -yspace)
+    cell:merge_into(guardring)
+    cell:inherit_alignment_box(guardring)
+end
+
 return M
