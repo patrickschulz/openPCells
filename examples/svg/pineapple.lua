ac791621ac3cbaf0e56a44de6747ad178adfa317
Patrick Kurth <p.kurth@posteo.de>
2023-09-27 23:32:42 +0200

    updated SVG example

--- examples/svg/pineapple.lua
+++ examples/svg/pineapple.lua
@@ -1,8 +1,9 @@
 local cell = object.create("pineapple")
 
 cell:merge_into(pcell.create_layout("auxiliary/svg2layout", "_svg", {
-    filename = "pineapple1.svg",
-    scale = 1000,
+    filename = "pineapple.svg",
+    scale = 1,
+    grid = 1,
     allow45 = true,
 }))
 
--- examples/svg/run.sh
+++ examples/svg/run.sh
@@ -1,3 +1,3 @@
 OPC=../../opc
 
-${OPC} --technology opc --export gds --cellscript pineapple.lua --enable-dprint
+${OPC} --technology opc --export gds --cellscript pineapple.lua --enable-dprint --triangulate-polygons --rasterize-curves
