236fb77e1efe957b5790ab8cb4a72d8e5b67ee59
Patrick Kurth <p.kurth@posteo.de>
2023-10-30 09:04:25 +0100

    added API documentation for layouthelpers.place_guardring (and ... with_hole)

--- src/main.api_help.c
+++ src/main.api_help.c
@@ -68,7 +68,8 @@ enum module {
     MODULE_UTIL,
     MODULE_PLACEMENT,
     MODULE_ROUTING,
-    MODULE_CURVE
+    MODULE_CURVE,
+    MODULE_LAYOUTHELPERS
 };
 
 struct api_entry {
@@ -105,6 +106,8 @@ static const char* _stringify_module(enum module module)
             return "point";
         case MODULE_UTIL:
             return "util";
+        case MODULE_LAYOUTHELPERS:
+            return "layouthelpers";
     }
     return NULL; // make the compiler happy
 }
@@ -1611,6 +1614,52 @@ static struct vector* _initialize_api_entries(void)
             "	curve.cubicto(point.create(0, 500), point.create(500, 500), point.create(500, 0)),\n"
             "}, grid, allow45)\n"
             ,
+            parameters,
+            sizeof(parameters) / sizeof(parameters[0])
+        ));
+    }
+
+    /* layouthelpers.place_guardring */
+    {
+        struct parameter parameters[] = {
+            { "cell",       OBJECT,     NULL, "cell to place guardring in" },
+            { "bl",         POINT,      NULL, "bottom-left boundary corner" },
+            { "tr",         POINT,      NULL, "top-right boundary corner" },
+            { "xspace",     INTEGER,    NULL, "space in x-direction between boundary and guardring" },
+            { "yspace",     INTEGER,    NULL, "space in y-direction between boundary and guardring" },
+            { "options",    TABLE,      NULL, "placement options" }
+        };
+        vector_append(entries, _make_api_entry(
+            "place_guardring",
+            MODULE_LAYOUTHELPERS,
+            // help text
+            "place a guardring in a cell with a defined boundary and spacing",
+            // example
+    "layouthelpers.place_guardring(cell,\n    nmos:get_area_anchor(\"active\").bl,\n    nmos:get_area_anchor(\"active\").tr,\n    200, 200,\n    {\n        contype = \"n\",\n        ringwidth = 100,\n        drawdeepwell = true,\n    }\n)",
+            parameters,
+            sizeof(parameters) / sizeof(parameters[0])
+        ));
+    }
+
+    /* layouthelpers.place_guardring_with_hole */
+    {
+        struct parameter parameters[] = {
+            { "cell",       OBJECT,     NULL, "cell to place guardring in" },
+            { "bl",         POINT,      NULL, "bottom-left boundary corner" },
+            { "tr",         POINT,      NULL, "top-right boundary corner" },
+            { "bl",         POINT,      NULL, "bottom-left hole boundary corner" },
+            { "tr",         POINT,      NULL, "top-right hole boundary corner" },
+            { "xspace",     INTEGER,    NULL, "space in x-direction between boundary and guardring" },
+            { "yspace",     INTEGER,    NULL, "space in y-direction between boundary and guardring" },
+            { "options",    TABLE,      NULL, "placement options" }
+        };
+        vector_append(entries, _make_api_entry(
+            "place_guardring_with_hole",
+            MODULE_LAYOUTHELPERS,
+            // help text
+            "place a guardring with a well hole in a cell with a defined boundary and spacing",
+            // example
+    "layouthelpers.place_guardring_with_hole(cell,\n    nmos:get_area_anchor(\"active\").bl,\n    pmos:get_area_anchor(\"active\").tr,\n    pmos:get_area_anchor(\"active\").bl,\n    pmos:get_area_anchor(\"active\").tr),\n    200, 200,\n    {\n        contype = \"n\",\n        ringwidth = 100,\n        drawdeepwell = true,\n    }\n)",
             parameters,
             sizeof(parameters) / sizeof(parameters[0])
         ));
