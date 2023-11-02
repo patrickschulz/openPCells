c823f530566b4e725c55c7ec67721302304dc821
Patrick Kurth <p.kurth@posteo.de>
2023-11-02 15:00:52 +0100

    added unequal rail width support

--- cells/passive/capacitor/mom.lua
+++ cells/passive/capacitor/mom.lua
@@ -6,6 +6,8 @@ function parameters()
         { "fheight(Finger Height)",  1000 },
         { "foffset(Finger Offset)",   100 },
         { "rwidth(Rail Width)",       100 },
+        { "urwidth",                  100, follow = "rwidth" },
+        { "lrwidth",                  100, follow = "rwidth" },
         { "rext(Rail Extension)",       0 },
         { "firstmetal(Start Metal)",    1 },
         { "lastmetal(End Metal)",       2 },
@@ -47,12 +49,12 @@ function layout(momcap, _P)
         geometry.rectanglebltr(
             momcap, generics.metal(m),
             point.create(-_P.rext, 0),
-            point.create(_P.fingers * _P.fwidth + (_P.fingers - 1) * _P.fspace + _P.rext, _P.rwidth)
+            point.create(_P.fingers * _P.fwidth + (_P.fingers - 1) * _P.fspace + _P.rext, _P.lrwidth)
         )
         geometry.rectanglebltr(
             momcap, generics.metal(m),
-            point.create(-_P.rext, _P.fheight + 2 * _P.foffset + _P.rwidth),
-            point.create(_P.fingers * _P.fwidth + (_P.fingers - 1) * _P.fspace + _P.rext, _P.fheight + 2 * _P.foffset + 2 * _P.rwidth)
+            point.create(-_P.rext, _P.fheight + 2 * _P.foffset + _P.lrwidth),
+            point.create(_P.fingers * _P.fwidth + (_P.fingers - 1) * _P.fspace + _P.rext, _P.fheight + 2 * _P.foffset + _P.lrwidth + _P.urwidth)
         )
         -- fingers
         for f = 1, _P.fingers do
@@ -64,8 +66,8 @@ function layout(momcap, _P)
             local yshift = (f % 2 == polarity) and 0 or _P.foffset
             geometry.rectanglebltr(
                 momcap, generics.metal(m),
-                point.create(xshift, _P.rwidth + yshift),
-                point.create(xshift + _P.fwidth, _P.rwidth + yshift + _P.fheight + _P.foffset)
+                point.create(xshift, _P.lrwidth + yshift),
+                point.create(xshift + _P.fwidth, _P.lrwidth + yshift + _P.fheight + _P.foffset)
             )
         end
     end
@@ -75,12 +77,12 @@ function layout(momcap, _P)
             viafunc(
                 momcap, firstmetal, lastmetal,
                 point.create(-_P.rext, 0),
-                point.create(_P.fingers * _P.fwidth + (_P.fingers - 1) * _P.fspace + _P.rext, _P.rwidth)
+                point.create(_P.fingers * _P.fwidth + (_P.fingers - 1) * _P.fspace + _P.rext, _P.lrwidth)
             )
             viafunc(
                 momcap, firstmetal, lastmetal,
-                point.create(-_P.rext, _P.fheight + 2 * _P.foffset + _P.rwidth),
-                point.create(_P.fingers * _P.fwidth + (_P.fingers - 1) * _P.fspace + _P.rext, _P.fheight + 2 * _P.foffset + 2 * _P.rwidth)
+                point.create(-_P.rext, _P.fheight + 2 * _P.foffset + _P.lrwidth),
+                point.create(_P.fingers * _P.fwidth + (_P.fingers - 1) * _P.fspace + _P.rext, _P.fheight + 2 * _P.foffset + _P.lrwidth + _P.urwidth)
             )
         end
     end
@@ -89,7 +91,7 @@ function layout(momcap, _P)
         local xpitch = _P.fillwidth + _P.fillxspace
         local ypitch = _P.fillheight + _P.fillyspace
         local totalwidth = 2 * _P.rext + _P.fingers * _P.fwidth + (_P.fingers - 1) * _P.fspace + 2 * _P.fillxextend
-        local totalheight = 2 * _P.rwidth + _P.fheight + 2 * _P.foffset + 2 * _P.fillyextend
+        local totalheight = _P.lrwidth + _P.urwidth + _P.fheight + 2 * _P.foffset + 2 * _P.fillyextend
         local xrep = (totalwidth + _P.fillxspace) // xpitch
         local yrep = (totalheight + _P.fillyspace) // ypitch
         local xshift = (totalwidth - xrep * _P.fillwidth - (xrep - 1) * _P.fillxspace) / 2
@@ -107,17 +109,17 @@ function layout(momcap, _P)
     end
 
     momcap:add_area_anchor_bltr("upperrail",
-        point.create(-_P.rext, _P.fheight + 2 * _P.foffset + _P.rwidth),
-        point.create(_P.fingers * _P.fwidth + (_P.fingers - 1) * _P.fspace + _P.rext, _P.fheight + 2 * _P.foffset + 2 * _P.rwidth)
+        point.create(-_P.rext, _P.fheight + 2 * _P.foffset + _P.lrwidth),
+        point.create(_P.fingers * _P.fwidth + (_P.fingers - 1) * _P.fspace + _P.rext, _P.fheight + 2 * _P.foffset + _P.lrwidth + _P.urwidth)
     )
     momcap:add_area_anchor_bltr("lowerrail",
         point.create(-_P.rext, 0),
-        point.create(_P.fingers * _P.fwidth + (_P.fingers - 1) * _P.fspace + _P.rext, _P.rwidth)
+        point.create(_P.fingers * _P.fwidth + (_P.fingers - 1) * _P.fspace + _P.rext, _P.lrwidth)
     )
     momcap:set_alignment_box(
         point.create(-_P.rext, 0),
-        point.create(_P.fingers * _P.fwidth + (_P.fingers - 1) * _P.fspace + _P.rext, _P.fheight + 2 * _P.foffset + 2 * _P.rwidth),
-        point.create(-_P.rext, _P.rwidth),
-        point.create(_P.fingers * _P.fwidth + (_P.fingers - 1) * _P.fspace + _P.rext, _P.fheight + 2 * _P.foffset + _P.rwidth)
+        point.create(_P.fingers * _P.fwidth + (_P.fingers - 1) * _P.fspace + _P.rext, _P.fheight + 2 * _P.foffset + _P.lrwidth + _P.urwidth),
+        point.create(-_P.rext, _P.lrwidth),
+        point.create(_P.fingers * _P.fwidth + (_P.fingers - 1) * _P.fspace + _P.rext, _P.fheight + 2 * _P.foffset + _P.lrwidth)
     )
 end
