8377da6d3f6d3826f3bb6e4a10674b0b7d86f2d9
Patrick Kurth <p.kurth@posteo.de>
2023-11-11 19:13:41 +0100

    fixed spelling

--- src/main.api_help.c
+++ src/main.api_help.c
@@ -1910,7 +1910,7 @@ static struct vector* _initialize_api_entries(void)
         vector_append(entries, _make_api_entry(
             "get_alignment_anchor",
             MODULE_OBJECT,
-            "Retrieve an alignemtn anchor from a cell. These anchors are the defining points of the alignment box. Valid anchor names are 'outerbl', 'outerbr', 'outertl', 'outertr', 'innerbl', 'innerbr', 'innertl' and 'innertr'. This function returns a point that contains the position of the specified anchor, corrected by the cell transformation. A non-existing anchor is an error",
+            "Retrieve an alignment anchor from a cell. These anchors are the defining points of the alignment box. Valid anchor names are 'outerbl', 'outerbr', 'outertl', 'outertr', 'innerbl', 'innerbr', 'innertl' and 'innertr'. This function returns a point that contains the position of the specified anchor, corrected by the cell transformation. A non-existing anchor is an error",
             "cell:get_alignment_anchor(\"outerbl\")",
             parameters,
             sizeof(parameters) / sizeof(parameters[0])
