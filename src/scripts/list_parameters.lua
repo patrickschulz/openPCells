d433cad56fdee0164e4e42cea54309fac9191884
Patrick Kurth <p.kurth@posteo.de>
2024-03-14 14:38:45 +0100

    updated default parameter listing format

--- src/scripts/list_parameters.lua
+++ src/scripts/list_parameters.lua
@@ -1,7 +1,7 @@
 -- FIXME: output cell parameters AFTER parameters have been processed in order to respect value changes in pfiles
 -- FIXME: is args.generictech currently used and supported?
 local params = pcell.parameters(args.cell, cellargs, args.generictech)
-local paramformat = args.parametersformat or "%n %v %i"
+local paramformat = args.parametersformat or "%n %v"
 for _, P in ipairs(params) do
     local doprint = true
     if args.parameternames then
