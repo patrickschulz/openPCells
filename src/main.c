96832f1b8e3518a70afb4772bf0dfa87961729a9
Patrick Kurth <p.kurth@posteo.de>
2025-01-12 18:17:47 +0100

    updated copyright year

--- src/main.c
+++ src/main.c
@@ -116,7 +116,7 @@ static int _load_config(struct hashmap* config)
 void _print_general_info(void)
 {
     printf("This is the openPCell layout generator (opc), version %u.%u.%u.\n", OPC_VERSION_MAJOR, OPC_VERSION_MINOR, OPC_VERSION_REVISION);
-    puts("Copyright 2020-2024 Patrick Kurth");
+    puts("Copyright 2020-2025 Patrick Kurth");
     puts("To generate a layout, you need to pass the technology, the export type and a cellname.");
     puts("Example:");
     puts("         opc --technology opc --export gds --cell stdcells/not_gate");
@@ -152,7 +152,7 @@ int main(int argc, const char* const * argv)
     if(cmdoptions_was_provided_long(cmdoptions, "version"))
     {
         printf("openPCells (opc) %u.%u.%u\n", OPC_VERSION_MAJOR, OPC_VERSION_MINOR, OPC_VERSION_REVISION);
-        puts("Copyright 2020-2024 Patrick Kurth");
+        puts("Copyright 2020-2025 Patrick Kurth");
         goto DESTROY_CMDOPTIONS;
     }
     FILE* pfd_f = NULL;
