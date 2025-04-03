9504ab9bc2601dddd36f304117afacdd22a2e62c
Patrick Kurth <p.kurth@posteo.de>
2025-04-03 19:46:53 +0200

    improved ldmos

--- cells/basic/ldmos.lua
+++ cells/basic/ldmos.lua
@@ -6,6 +6,7 @@ function parameters()
         { "gatelength",                     0 },
         { "gatestrapwidth",                 0 },
         { "gatestrapspace",                 0 },
+        { "gatemetal",                      1 },
         { "sourcemetal",                    1 },
         { "sourcewidth",                    0 },
         { "sourcespace",                    0 },
@@ -17,12 +18,16 @@ function parameters()
         { "gbotext",                        0 },
         { "subblocksourceextension",        0 },
         { "subblockdrainextension",         0 },
+        { "subblocktopextension",           0 },
+        { "subblockbottomextension",        0 },
         { "drawinnerguardring",             true },
         { "guardringwidth",                 0 },
         { "guardringtopsep",                0 },
         { "guardringbottomsep",             0 },
         { "guardringleftsep",               0 },
         { "guardringrightsep",              0 },
+        { "drainwellextendleftright",       0 },
+        { "drainwellextendtopbottom",       0 },
         { "extendall",                      0 },
         { "extendalltop",                   0, follow = "extendall" },
         { "extendallbottom",                0, follow = "extendall" },
@@ -36,10 +41,6 @@ function parameters()
         { "extendvthtypebottom",            0, follow = "extendallbottom" },
         { "extendvthtypeleft",              0, follow = "extendallleft" },
         { "extendvthtyperight",             0, follow = "extendallright" },
-        { "extendimplanttop",               0, follow = "extendalltop" },
-        { "extendimplantbottom",            0, follow = "extendallbottom" },
-        { "extendimplantleft",              0, follow = "extendallleft" },
-        { "extendimplantright",             0, follow = "extendallright" },
         { "extendwelltop",                  0, follow = "extendalltop" },
         { "extendwellbottom",               0, follow = "extendallbottom" },
         { "extendwellleft",                 0, follow = "extendallleft" },
@@ -60,18 +61,6 @@ function layout(ldmos, _P)
         point.create(
             (_P.fingers / 2 - 1) * xpitch + 2 * _P.sourcewidth + 2 * _P.sourcespace + 2 * _P.drainspace + _P.drainwidth + 2 * _P.gatelength,
             _P.fingerwidth
-        )
-    )
-
-    -- implant
-    geometry.rectanglebltr(ldmos, generics.implant(_P.channeltype),
-        point.create(
-            -_P.extendimplantleft,
-            -_P.gatestrapspace - _P.gatestrapwidth - _P.gbotext - _P.extendimplantbottom
-        ),
-        point.create(
-            (_P.fingers / 2 - 1) * xpitch + 2 * _P.sourcewidth + 2 * _P.sourcespace + 2 * _P.drainspace + _P.drainwidth + 2 * _P.gatelength + _P.extendallright,
-            _P.fingerwidth + _P.gatestrapspace + _P.gatestrapwidth + _P.gtopext + _P.extendimplanttop
         )
     )
 
@@ -139,6 +128,50 @@ function layout(ldmos, _P)
             point.create(
                 (i - 1) * xpitch + _P.sourcewidth + _P.sourcespace + _P.gatelength + 2 * _P.drainspace + _P.drainwidth + _P.gatelength,
                 _P.fingerwidth + _P.gatestrapspace + _P.gatestrapwidth
+            )
+        )
+    end
+
+    -- gate straps
+    geometry.rectanglebltr(ldmos, generics.metal(1),
+        point.create(
+            (1 - 1) * xpitch + _P.sourcewidth + _P.sourcespace,
+            _P.fingerwidth + _P.gatestrapspace
+        ),
+        point.create(
+            (_P.fingers / 2 - 1) * xpitch + _P.sourcewidth + _P.sourcespace + _P.gatelength + 2 * _P.drainspace + _P.drainwidth + _P.gatelength,
+            _P.fingerwidth + _P.gatestrapspace + _P.gatestrapwidth
+        )
+    )
+    geometry.rectanglebltr(ldmos, generics.metal(1),
+        point.create(
+            (1 - 1) * xpitch + _P.sourcewidth + _P.sourcespace,
+            - _P.gatestrapspace - _P.gatestrapwidth
+        ),
+        point.create(
+            (_P.fingers / 2 - 1) * xpitch + _P.sourcewidth + _P.sourcespace + _P.gatelength + 2 * _P.drainspace + _P.drainwidth + _P.gatelength,
+            - _P.gatestrapspace
+        )
+    )
+    if _P.gatemetal > 1 then
+        geometry.viabltr(ldmos, 1, _P.gatemetal,
+            point.create(
+                (1 - 1) * xpitch + _P.sourcewidth + _P.sourcespace,
+                _P.fingerwidth + _P.gatestrapspace
+            ),
+            point.create(
+                (_P.fingers / 2 - 1) * xpitch + _P.sourcewidth + _P.sourcespace + _P.gatelength + 2 * _P.drainspace + _P.drainwidth + _P.gatelength,
+                _P.fingerwidth + _P.gatestrapspace + _P.gatestrapwidth
+            )
+        )
+        geometry.viabltr(ldmos, 1, _P.gatemetal,
+            point.create(
+                (1 - 1) * xpitch + _P.sourcewidth + _P.sourcespace,
+                - _P.gatestrapspace - _P.gatestrapwidth
+            ),
+            point.create(
+                (_P.fingers / 2 - 1) * xpitch + _P.sourcewidth + _P.sourcespace + _P.gatelength + 2 * _P.drainspace + _P.drainwidth + _P.gatelength,
+                - _P.gatestrapspace
             )
         )
     end
@@ -215,6 +248,20 @@ function layout(ldmos, _P)
         end
     end
 
+    -- drain well
+    for i = 1, _P.fingers / 2 do
+        geometry.rectanglebltr(ldmos, generics.other("nwell"),
+            point.create(
+                (i - 1) * xpitch + _P.sourcewidth + _P.sourcespace + _P.gatelength + _P.drainspace - _P.drainwellextendleftright,
+                -_P.drainwellextendtopbottom
+            ),
+            point.create(
+                (i - 1) * xpitch + _P.sourcewidth + _P.sourcespace + _P.gatelength + _P.drainspace + _P.drainwidth + _P.drainwellextendleftright,
+                _P.fingerwidth + _P.drainwellextendtopbottom
+            )
+        )
+    end
+
     -- soi open
     if not _P.drawinnerguardring then
         geometry.rectanglebltr(ldmos, generics.other("soiopen"),
@@ -234,21 +281,21 @@ function layout(ldmos, _P)
         geometry.rectanglebltr(ldmos, generics.other("subblock"),
             point.create(
                 (i - 1) * xpitch + _P.sourcewidth + _P.sourcespace - _P.subblocksourceextension,
-                0
+                -_P.subblockbottomextension
             ),
             point.create(
                 (i - 1) * xpitch + _P.sourcewidth + _P.sourcespace + _P.gatelength + _P.subblockdrainextension,
-                _P.fingerwidth
+                _P.fingerwidth + _P.subblocktopextension
             )
         )
         geometry.rectanglebltr(ldmos, generics.other("subblock"),
             point.create(
-                (i - 1) * xpitch + _P.sourcewidth + _P.sourcespace - _P.subblocksourceextension + _P.gatelength + 2 * _P.drainspace + _P.drainwidth,
-                0
+                (i - 1) * xpitch + _P.sourcewidth + _P.sourcespace - _P.subblockdrainextension + _P.gatelength + 2 * _P.drainspace + _P.drainwidth,
+                -_P.subblockbottomextension
             ),
             point.create(
-                (i - 1) * xpitch + _P.sourcewidth + _P.sourcespace + _P.subblockdrainextension + _P.gatelength + 2 * _P.drainspace + _P.drainwidth + _P.gatelength,
-                _P.fingerwidth
+                (i - 1) * xpitch + _P.sourcewidth + _P.sourcespace + _P.subblocksourceextension + _P.gatelength + 2 * _P.drainspace + _P.drainwidth + _P.gatelength,
+                _P.fingerwidth + _P.subblocktopextension
             )
         )
     end
@@ -260,13 +307,10 @@ function layout(ldmos, _P)
             ringwidth = _P.guardringwidth,
             holewidth = (_P.fingers / 2 - 1) * xpitch + 2 * _P.sourcewidth + 2 * _P.sourcespace + 2 * _P.drainspace + _P.drainwidth + 2 * _P.gatelength  + _P.guardringleftsep + _P.guardringrightsep,
             holeheight = _P.fingerwidth + 2 * (_P.gatestrapwidth + _P.gatestrapspace) + _P.gtopext + _P.gbotext + _P.guardringtopsep + _P.guardringbottomsep,
-            fillwell = true,
+            fillwell = false,
             fillsoiopen = true,
-            --drawsegments = _P.guardringsegments,
-            --fillimplant = _P.guardringfillimplant,
-            --wellextension = _P.guardringwellextension,
-            --implantextension = _P.guardringimplantextension,
-            --soiopenextension = _P.guardringsoiopenextension,
+            fillinnerimplant = true,
+            innerimplantpolarity = _P.channeltype == "nmos" and "n" or "p",
         })
         guardring:move_point(guardring:get_area_anchor("innerboundary").bl, point.create(0, -_P.gatestrapspace - _P.gatestrapwidth - _P.gbotext))
         guardring:translate(-_P.guardringleftsep, -_P.guardringbottomsep)
