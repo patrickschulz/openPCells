4b94eedc37695b582399db39a862ff40a9bb3d98
Patrick Kurth <p.kurth@posteo.de>
2025-05-29 00:15:26 +0200

    added simple mosfet example

--- examples/cells/run.sh
+++ examples/cells/run.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 
+../../opc --technology opc --export gds --cellscript mosfet.lua --filename mosfet
 ../../opc --technology opc --export gds --cellscript ringoscillator.lua --filename ringoscillator
 ../../opc --technology opc --export gds --cellscript comparator.lua --filename comparator
