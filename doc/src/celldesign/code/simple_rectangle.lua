591933ef3be9a770b87ee3dcc6be5d1cb8d56633
Patrick Kurth <p.kurth@posteo.de>
2025-06-14 23:01:59 +0200

    working on doc update

--- doc/src/celldesign/code/simple_rectangle.lua
+++ doc/src/celldesign/code/simple_rectangle.lua
@@ -1,18 +1,17 @@
--- define parameters
-function parameters()
-    pcell.add_parameters(
-        { "width",  100 },
-        { "height", 100 }
-    )
-end
+-- create cell to collect all shapes
+local cell = object.create("toplevel")
 
--- define layout
-function layout(obj, _P)
-    -- create the shape and add it to the main object
-    geometry.rectanglebltr(
-        obj,
-        generics.metal(1),
-        point.create(0, 0),
-        point.create(_P.width, _P.height)
-    )
-end
+-- define parameters
+local width = 100
+local height = 100
+
+-- create rectangle
+geometry.rectanglebltr(
+    cell,
+    generics.metal(1),
+    point.create(0, 0),
+    point.create(width, height)
+)
+
+-- return created cell
+return cell
