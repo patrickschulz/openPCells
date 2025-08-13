716519fca2e8accf6583f43cce3afd194ea1c5fd
Patrick Kurth <p.kurth@posteo.de>
2025-08-13 08:15:55 +0200

    added stacked_ringoscillator example

--- examples/cells/run.sh
+++ examples/cells/run.sh
@@ -2,6 +2,7 @@
 
 ../../opc --technology opc --export gds --cellscript mosfet.lua --filename mosfet
 ../../opc --technology opc --export gds --cellscript ringoscillator.lua --filename ringoscillator
+../../opc --technology opc --export gds --cellscript stacked_ringoscillator.lua --filename stacked_ringoscillator --enable-fallback-vias
 ../../opc --technology opc --export gds --cellscript current_starved_ringoscillator.lua --filename current_starved_ringoscillator --enable-dprint
 ../../opc --technology opc --export gds --cellscript comparator.lua --filename comparator
 ../../opc --technology opc --export gds --cellscript LC_oscillator.lua --filename LC_oscillator --enable-fallback-vias
