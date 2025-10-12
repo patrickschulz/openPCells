cd2a1f87fb63931a48472acddbbde5e608f216c1
Patrick Kurth <p.kurth@posteo.de>
2025-10-12 23:33:46 +0200

    updated 'debug' export for testing

--- export/debug/init.lua
+++ export/debug/init.lua
@@ -7,42 +7,39 @@ function M.finalize()
 end
 
 function M.get_techexport()
-    return "raw"
+    return "debug"
 end
 
 function M.get_extension()
     return "debug"
 end
 
-function M.get_layer(S)
-    return S:get_lpp():str()
-end
-
 function M.write_rectangle(layer, bl, tr)
-    local xbot, ybot = bl:unwrap()
-    local xtop, ytop = tr:unwrap()
-    table.insert(__content, string.format("rect (%s): (%d, %d) (%d, %d)", layer, xbot, ybot, xtop, ytop))
+    table.insert(__content, string.format("rect (%s): (%d, %d) (%d, %d)", layer.layer, bl.x, bl.y, tr.x, tr.y))
 end
 
 function M.write_polygon(layer, pts)
     local t = {}
     for _, pt in ipairs(pts) do
-        local x, y = pt:unwrap()
-        table.insert(t, string.format("(%d, %d)", x, y))
+        table.insert(t, string.format("(%d, %d)", pt.x, pt.y))
     end
-    table.insert(__content, string.format("poly (%s): %s", layer, table.concat(t, " ")))
+    table.insert(__content, string.format("poly (%s): %s", layer.layer, table.concat(t, " ")))
 end
 
 function M.write_cell_reference(identifier, instname, origin)
-    table.insert(__content, string.format("ref  (%s): (%d, %d)", identifier, origin:getx(), origin:gety()))
+    table.insert(__content, string.format("cell reference  (%s): (%d, %d)", identifier, origin.x, origin.y))
+end
+
+function M.write_port(name, layer, where, sizehint)
+    table.insert(__content, string.format("port (%s): %s at (%d, %d)", layer.layer, name, where.x, where.y))
 end
 
 function M.at_begin_cell(cellname)
-    table.insert(__content, string.format("cell (%s) >", cellname))
+    table.insert(__content, string.format("cell definition begin (%s)", cellname))
 end
 
 function M.at_end_cell()
-    table.insert(__content, "<\n")
+    table.insert(__content, "cell definition end")
 end
 
 return M
