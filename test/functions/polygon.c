e813aca2a19407de4a85673f0b69b4c7b2f00920
Patrick Kurth <p.kurth@posteo.de>
2025-10-20 19:26:13 +0200

    added negative polygon intersection tests

--- test/functions/polygon.c
+++ test/functions/polygon.c
@@ -15,4 +15,16 @@ int main(void)
         check_boolean(intersect, "polygon intersection 02");
         simple_polygon_destroy(polygon);
     }
+    {
+        struct simple_polygon* polygon = simple_polygon_create_from_rectangle(-50, -50, 50, 50);
+        int intersect = simple_polygon_intersects_rectangle(polygon, 50, 50, 60, 60);
+        check_boolean(!intersect, "polygon intersection 03");
+        simple_polygon_destroy(polygon);
+    }
+    {
+        struct simple_polygon* polygon = simple_polygon_create_from_rectangle(-50, -50, 50, 50);
+        int intersect = simple_polygon_intersects_rectangle(polygon, 60, 60, 70, 70);
+        check_boolean(!intersect, "polygon intersection 03");
+        simple_polygon_destroy(polygon);
+    }
 }
