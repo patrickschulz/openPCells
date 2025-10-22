0420800b2aefff60025562293790706965b0e20c
Patrick Kurth <p.kurth@posteo.de>
2025-10-23 00:57:50 +0200

    updated cell examples

--- examples/cells/run.sh
+++ examples/cells/run.sh
@@ -4,6 +4,7 @@
 ../../opc --technology opc --export gds --cellscript ringoscillator.lua --filename ringoscillator
 ../../opc --technology opc --export gds --cellscript stacked_ringoscillator.lua --filename stacked_ringoscillator --enable-fallback-vias
 ../../opc --technology opc --export gds --cellscript current_starved_ringoscillator.lua --filename current_starved_ringoscillator --enable-dprint
-../../opc --technology opc --export gds --cellscript comparator.lua --filename comparator
-../../opc --technology opc --export gds --cellscript LC_oscillator.lua --filename LC_oscillator --enable-fallback-vias
+#../../opc --technology opc --export gds --cellscript comparator.lua --filename comparator
+#../../opc --technology opc --export gds --cellscript LC_oscillator.lua --filename LC_oscillator --enable-fallback-vias
 ../../opc --technology opc --export gds --cellscript ldmos.lua --filename ldmos
+../../opc --technology opc --export gds --cellscript RFmosfet.lua --filename RFmosfet
