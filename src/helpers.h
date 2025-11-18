9debcb69c019ebcde467788b73e4e29f756b8188
Patrick Kurth <p.kurth@posteo.de>
2025-11-18 07:05:54 +0100

    added SWAP macro

--- src/helpers.h
+++ src/helpers.h
@@ -9,4 +9,6 @@
 #define MAX4(a, b, c, d) MAX2(MAX2(a, b), MAX2(c, d))
 #define MIN4(a, b, c, d) MIN2(MIN2(a, b), MIN2(c, d))
 
+#define SWAP(a, b, type) do { type tmp = a; a = b; b = tmp; } while(0)
+
 #endif /* OPC_HELPERS_H */
