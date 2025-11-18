5c331250e99ae0cdc30b27e548d91e0ba2660ce7
Patrick Kurth <p.kurth@posteo.de>
2025-11-18 07:48:53 +0100

    test cell for nil parameters

---
+++ cells/__test/nilparam.lua
@@ -0,0 +1,14 @@
+function parameters()
+    pcell.add_parameters(
+        { "extralayers", nil },
+    )
+end
+
+function layout(cell, _P)
+    geometry.rectanglebltr(cell, generics.metal(1), point.create(0, 0), point.create(100, 100))
+    if _P.extralayers then
+        for _, layer in ipairs(_P.extralayers) do
+            geometry.rectanglebltr(cell, layer, point.create(0, 0), point.create(100, 100))
+        end
+    end
+end
