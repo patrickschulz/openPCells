eec62a00d98ca64142bcd1c6465c4c4f5310440d
Patrick Kurth <p.kurth@posteo.de>
2025-12-15 20:07:19 +0100

    adapted tests to new guardring stuff

--- test/full/run.sh
+++ test/full/run.sh
@@ -81,6 +81,8 @@ do_cell_test mosfet_10 basic/mosfet gds
 do_cell_test mosfet_11 basic/mosfet gds
 do_cell_test mosfet_12 basic/mosfet gds
 do_cell_test mosfet_13 basic/mosfet gds
+do_cell_test mosfet_14 basic/mosfet gds
+do_cell_test mosfet_15 basic/mosfet gds
 
 # polyresistor
 do_cell_test polyresistor_01 basic/polyresistor gds
