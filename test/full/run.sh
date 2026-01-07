d3e34c0fbd5a89edf3d9b4463f59016a4e109f23
Patrick Kurth <p.kurth@posteo.de>
2026-01-07 22:06:09 +0100

    added test for 'gate' placement with sourcestraps

--- test/full/run.sh
+++ test/full/run.sh
@@ -111,6 +111,7 @@ do_cell_test inverter_02 analog/inverter gds
 # common centroid
 do_cell_test common_centroid_01 analog/common_centroid gds
 do_cell_test common_centroid_02 analog/common_centroid gds
+do_cell_test common_centroid_03 analog/common_centroid gds
 do_cellscript_test cellscript_common_centroid_01 cellscript_common_centroid_01.lua gds
 do_cellscript_test cellscript_common_centroid_02 cellscript_common_centroid_02.lua gds
 do_cellscript_test cellscript_common_centroid_03 cellscript_common_centroid_03.lua gds
