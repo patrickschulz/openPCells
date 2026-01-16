8f84fac6e7e490513626721376a7e221c91bfc91
Patrick Kurth <p.kurth@posteo.de>
2026-01-16 08:16:38 +0100

    added simple minimum-via test

--- test/full/cellscript_tech_vias.lua
+++ test/full/cellscript_tech_vias.lua
@@ -1,27 +1,13 @@
 local cell = object.create("cell")
 
-geometry.viabltr(cell, 1, 2,
-    point.create(0, 0),
-    point.create(
-        technology.get_dimension("Minimum M1M2 Viawidth"),
-        technology.get_dimension("Minimum M1M2 Viawidth")
+for metal = 1, technology.resolve_metal(-2) do
+    geometry.viabltr(cell, metal, metal + 1,
+        point.create(0, 0),
+        point.create(
+            technology.get_dimension(string.format("Minimum M%dM%d Viawidth", metal, metal + 1)),
+            technology.get_dimension(string.format("Minimum M%dM%d Viawidth", metal, metal + 1))
+        )
     )
-)
-
-geometry.viabltr(cell, 2, 3,
-    point.create(0, 0),
-    point.create(
-        technology.get_dimension("Minimum M2M3 Viawidth"),
-        technology.get_dimension("Minimum M2M3 Viawidth")
-    )
-)
-
-geometry.viabltr(cell, 3, 4,
-    point.create(0, 0),
-    point.create(
-        technology.get_dimension("Minimum M3M4 Viawidth"),
-        technology.get_dimension("Minimum M3M4 Viawidth")
-    )
-)
+end
 
 return cell
