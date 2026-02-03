1c19f17fe03711a4445af01de23f3a44fc34b390
Patrick Kurth <p.kurth@posteo.de>
2026-02-03 20:12:19 +0100

    fixed some minor issues in routing/bus

--- cells/routing/bus.lua
+++ cells/routing/bus.lua
@@ -3,6 +3,10 @@ function parameters()
         { "numbits", 8 },
         { "numregs", 8 },
         { "nummerge", 1 },
+        { "usebitorder", false },
+        { "bitorder", false },
+        { "useregorder", false },
+        { "regorder", false },
         { "invert", false },
         { "expand_all", false },
         { "vregshift", 0 },
@@ -46,6 +50,11 @@ function process_parameters(_P)
     return t
 end
 
+function prepare(_P)
+    local state = {}
+end
+
+
 function check(_P)
     if not _P.shift_bus_for_vias and _P.vregshift == 0 then
         return false, "'shift_bus_for_vias == false' is currently only supported for a non-zero 'vregshift'. If 'vregshift' is not set high enough, unchecked shorts can occure."
@@ -78,25 +87,36 @@ function layout(bus, _P)
         end
         local mergeyshift = (merge - 1) * (_P.hwidth + _P.hspace)
         for reg = 1, _P.numregs do
+            local xreg
+            if _P.useregorder then
+                xreg = _P.regorder[reg]
+            else
+                xreg = reg
+            end
+            local yreg = reg
             local regxshift
             if _P.vregshift > 0 then
-                regxshift = (reg - 1) * _P.vregshift
+                regxshift = (xreg - 1) * _P.vregshift
             else
-                regxshift = (reg - 1) * _P.numbits * (_P.vwidth + _P.vspace)
+                regxshift = (xreg - 1) * _P.numbits * (_P.vwidth + _P.vspace)
             end
             local regyshift
             if _P.hregshift > 0 then
-                regyshift = (reg - 1) * _P.hregshift
+                regyshift = (yreg - 1) * _P.hregshift
             else
-                regyshift = (reg - 1) * _P.numbits * (_P.hwidth + _P.hspace)
+                regyshift = (yreg - 1) * _P.nummerge * _P.numbits * (_P.hwidth + _P.hspace)
             end
             for bit = 1, _P.numbits do
                 local xbit = bit
                 local ybit
-                if _P.invert then
-                    ybit = _P.numbits - bit + 1
+                if _P.usebitorder then
+                    ybit = _P.bitorder[bit]
                 else
-                    ybit = bit
+                    if _P.invert then
+                        ybit = _P.numbits - bit + 1
+                    else
+                        ybit = bit
+                    end
                 end
                 local bitxshift = (xbit - 1) * (_P.vwidth + _P.vspace)
                 local bityshift = _P.nummerge * (ybit - 1) * (_P.hwidth + _P.hspace)
@@ -128,25 +148,36 @@ function layout(bus, _P)
         end
         local mergeyshift = (merge - 1) * (_P.hwidth + _P.hspace)
         for reg = 1, _P.numregs do
+            local xreg
+            if _P.useregorder then
+                xreg = _P.regorder[reg]
+            else
+                xreg = reg
+            end
+            local yreg = reg
             local regxshift
             if _P.vregshift > 0 then
-                regxshift = (reg - 1) * _P.vregshift
+                regxshift = (xreg - 1) * _P.vregshift
             else
-                regxshift = (reg - 1) * _P.numbits * (_P.vwidth + _P.vspace)
+                regxshift = (xreg - 1) * _P.numbits * (_P.vwidth + _P.vspace)
             end
             local regyshift
             if _P.hregshift > 0 then
-                regyshift = (reg - 1) * _P.hregshift
+                regyshift = (yreg - 1) * _P.hregshift
             else
-                regyshift = (reg - 1) * _P.numbits * (_P.hwidth + _P.hspace)
+                regyshift = (yreg - 1) * _P.nummerge * _P.numbits * (_P.hwidth + _P.hspace)
             end
             for bit = 1, _P.numbits do
                 local xbit = bit
                 local ybit
-                if _P.invert then
-                    ybit = _P.numbits - bit + 1
+                if _P.usebitorder then
+                    ybit = _P.bitorder[bit]
                 else
-                    ybit = bit
+                    if _P.invert then
+                        ybit = _P.numbits - bit + 1
+                    else
+                        ybit = bit
+                    end
                 end
                 local bitxshift = (xbit - 1) * (_P.vwidth + _P.vspace)
                 local bityshift = _P.nummerge * (ybit - 1) * (_P.hwidth + _P.hspace)
@@ -168,60 +199,89 @@ function layout(bus, _P)
     end
 
     -- vias
-    for merge = 1, _P.nummerge do
-        local mergexshift = (merge - 1) * _P.numregs * _P.numbits * (_P.vwidth + _P.vspace)
-        if _P.shift_bus_for_vias then
-            mergexshift = mergexshift + (merge - 1) * 2 * _P.viaext
-        end
-        local mergeyshift = (merge - 1) * (_P.hwidth + _P.hspace)
-        for reg = 1, _P.numregs do
-            local regxshift
-            if _P.vregshift > 0 then
-                regxshift = (reg - 1) * _P.vregshift
-            else
-                regxshift = (reg - 1) * _P.numbits * (_P.vwidth + _P.vspace)
+    if _P.hmetal ~= _P.vmetal then
+        for merge = 1, _P.nummerge do
+            local mergexshift = (merge - 1) * _P.numregs * _P.numbits * (_P.vwidth + _P.vspace)
+            if _P.shift_bus_for_vias then
+                mergexshift = mergexshift + (merge - 1) * 2 * _P.viaext
             end
-            local regyshift
-            if _P.hregshift > 0 then
-                regyshift = (reg - 1) * _P.hregshift
-            else
-                regyshift = (reg - 1) * _P.numbits * (_P.hwidth + _P.hspace)
-            end
-            for bit = 1, _P.numbits do
-                local xbit = bit
-                local ybit
-                if _P.invert then
-                    ybit = _P.numbits - bit + 1
+            local mergeyshift = (merge - 1) * (_P.hwidth + _P.hspace)
+            for reg = 1, _P.numregs do
+                local xreg
+                if _P.useregorder then
+                    xreg = _P.regorder[reg]
                 else
-                    ybit = bit
+                    xreg = reg
                 end
-                local bitxshift = (xbit - 1) * (_P.vwidth + _P.vspace)
-                local bityshift = _P.nummerge * (ybit - 1) * (_P.hwidth + _P.hspace)
-                local xshift = mergexshift + regxshift + bitxshift
-                local yshift = mergeyshift + regyshift + bityshift
-                local viaxext = 0
-                local viaxshift = 0
-                local viayext = 0
-                local viayshift = 0
-                if _P.viamode == "horizontal" then
-                    viaxext = 2 * _P.viaext
-                    if _P.vposition == "bottom" then
-                        viaxshift = 0
-                    else
-                        viaxshift = 2 * _P.viaext
-                    end
+                local yreg = reg
+                local regxshift
+                if _P.vregshift > 0 then
+                    regxshift = (xreg - 1) * _P.vregshift
                 else
-                    viayext = 2 * _P.viaext
-                    if _P.hposition == "left" then
-                        viayshift = 0
-                    else
-                        viayshift = 2 * _P.viaext
-                    end
+                    regxshift = (xreg - 1) * _P.numbits * (_P.vwidth + _P.vspace)
                 end
-                geometry.viabltr(bus, _P.vmetal, _P.hmetal,
-                    point.create(xshift + viaxshift - viaxext, yshift + viayshift - viayext),
-                    point.create(xshift + viaxshift + _P.vwidth, yshift + viayshift + _P.hwidth)
-                )
+                local regyshift
+                if _P.hregshift > 0 then
+                    regyshift = (yreg - 1) * _P.hregshift
+                else
+                    regyshift = (yreg - 1) * _P.nummerge * _P.numbits * (_P.hwidth + _P.hspace)
+                end
+                for bit = 1, _P.numbits do
+                    local xbit = bit
+                    local ybit
+                    if _P.usebitorder then
+                        ybit = _P.bitorder[bit]
+                    else
+                        if _P.invert then
+                            ybit = _P.numbits - bit + 1
+                        else
+                            ybit = bit
+                        end
+                    end
+                    local bitxshift = (xbit - 1) * (_P.vwidth + _P.vspace)
+                    local bityshift = _P.nummerge * (ybit - 1) * (_P.hwidth + _P.hspace)
+                    local xshift = mergexshift + regxshift + bitxshift
+                    local yshift = mergeyshift + regyshift + bityshift
+                    local viaxext = 0
+                    local viaxshift = 0
+                    local viayext = 0
+                    local viayshift = 0
+                    if _P.viamode == "horizontal" then
+                        viaxext = 2 * _P.viaext
+                        if _P.vposition == "bottom" then
+                            if _P.invert then
+                                viaxshift = 2 * _P.viaext
+                            else
+                                if _P.invert then
+                                    viaxshift = 0
+                                else
+                                    viaxshift = 2 * _P.viaext
+                                end
+                            end
+                        else
+                            viaxshift = 2 * _P.viaext
+                        end
+                    else -- _P.viamode == "vertical"
+                        viayext = 2 * _P.viaext
+                        if _P.hposition == "left" then
+                            if _P.invert then
+                                viayshift = 2 * _P.viaext
+                            else
+                                viayshift = 0
+                            end
+                        else
+                            if _P.invert then
+                                viayshift = 0
+                            else
+                                viayshift = 2 * _P.viaext
+                            end
+                        end
+                    end
+                    geometry.viabltr(bus, _P.vmetal, _P.hmetal,
+                        point.create(xshift + viaxshift - viaxext, yshift + viayshift - viayext),
+                        point.create(xshift + viaxshift + _P.vwidth, yshift + viayshift + _P.hwidth)
+                    )
+                end
             end
         end
     end
